05783bd40e5a800865d2ce087e03a12c19fda55d
Pierre Bonneel <pierre.bonneel@gmail.com>
2019-05-03 14:55:09 +0200

    [NF] Adding prius IPMSM validation case

---
+++ Tests/Validation/Material/Magnet_prius.py
@@ -0,0 +1,15 @@
+from pyleecan.Classes.Material import Material
+from pyleecan.Classes.MatMagnet import MatMagnet
+
+
+Magnet_prius = Material(name="Magnet_prius")
+Magnet_prius.mag = MatMagnet()
+
+Magnet_prius.elec.rho = 1.6e-06
+
+Magnet_prius.mag.mur_lin = 1.05
+Magnet_prius.mag.Hc = 902181.163126629
+Magnet_prius.mag.alpha_Br = -0.001
+Magnet_prius.mag.Brm20 = 1.24
+
+Magnet_prius.struct.rho = 7500.0
