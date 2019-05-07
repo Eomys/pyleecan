d5997e043ab095ef898affe6692b184fc06caae8
Pierre Bonneel <pierre.bonneel@gmail.com>
2019-05-07 16:07:58 +0200

    [CO] Merge Winding DW1L and DW2L

--- Classes/WindingDW2L.py
+++ Classes/WindingDW2L.py
@@ -4,9 +4,8 @@
 from os import linesep
 from pyleecan.Classes.check import check_init_dict, check_var
 from pyleecan.Functions.save import save
-from pyleecan.Classes.Winding import Winding
+from pyleecan.Classes.WindingDW1L import WindingDW1L
 
-from pyleecan.Methods.Machine.WindingDW2L.comp_connection_mat import comp_connection_mat
 from pyleecan.Methods.Machine.WindingDW2L.get_dim_wind import get_dim_wind
 
 from pyleecan.Classes.check import InitUnKnowClassError
@@ -17,13 +16,11 @@ from pyleecan.Classes.CondType21 import CondType21
 from pyleecan.Classes.CondType22 import CondType22
 
 
-class WindingDW2L(Winding):
+class WindingDW2L(WindingDW1L):
     """double layer overlapping integral distributed winding, radial coil superposition """
 
     VERSION = 1
 
-    # cf Methods.Machine.WindingDW2L.comp_connection_mat
-    comp_connection_mat = comp_connection_mat
     # cf Methods.Machine.WindingDW2L.get_dim_wind
     get_dim_wind = get_dim_wind
     # save method is available in all object
@@ -92,9 +89,9 @@ class WindingDW2L(Winding):
             if "conductor" in list(init_dict.keys()):
                 conductor = init_dict["conductor"]
         # Initialisation by argument
-        self.coil_pitch = coil_pitch
-        # Call Winding init
+        # Call WindingDW1L init
         super(WindingDW2L, self).__init__(
+            coil_pitch=coil_pitch,
             is_reverse_wind=is_reverse_wind,
             Nslot_shift_wind=Nslot_shift_wind,
             qs=qs,
@@ -105,16 +102,15 @@ class WindingDW2L(Winding):
             Lewout=Lewout,
             conductor=conductor,
         )
-        # The class is frozen (in Winding init), for now it's impossible to
+        # The class is frozen (in WindingDW1L init), for now it's impossible to
         # add new properties
 
     def __str__(self):
         """Convert this objet in a readeable string (for print)"""
 
         WindingDW2L_str = ""
-        # Get the properties inherited from Winding
+        # Get the properties inherited from WindingDW1L
         WindingDW2L_str += super(WindingDW2L, self).__str__() + linesep
-        WindingDW2L_str += "coil_pitch = " + str(self.coil_pitch)
         return WindingDW2L_str
 
     def __eq__(self, other):
@@ -123,10 +119,8 @@ class WindingDW2L(Winding):
         if type(other) != type(self):
             return False
 
-        # Check the properties inherited from Winding
+        # Check the properties inherited from WindingDW1L
         if not super(WindingDW2L, self).__eq__(other):
-            return False
-        if other.coil_pitch != self.coil_pitch:
             return False
         return True
 
@@ -134,9 +128,8 @@ class WindingDW2L(Winding):
         """Convert this objet in a json seriable dict (can be use in __init__)
         """
 
-        # Get the properties inherited from Winding
+        # Get the properties inherited from WindingDW1L
         WindingDW2L_dict = super(WindingDW2L, self).as_dict()
-        WindingDW2L_dict["coil_pitch"] = self.coil_pitch
         # The class name is added to the dict fordeserialisation purpose
         # Overwrite the mother class name
         WindingDW2L_dict["__class__"] = "WindingDW2L"
@@ -145,23 +138,5 @@ class WindingDW2L(Winding):
     def _set_None(self):
         """Set all the properties to None (except pyleecan object)"""
 
-        self.coil_pitch = None
-        # Set to None the properties inherited from Winding
+        # Set to None the properties inherited from WindingDW1L
         super(WindingDW2L, self)._set_None()
-
-    def _get_coil_pitch(self):
-        """getter of coil_pitch"""
-        return self._coil_pitch
-
-    def _set_coil_pitch(self, value):
-        """setter of coil_pitch"""
-        check_var("coil_pitch", value, "int", Vmin=0, Vmax=1000)
-        self._coil_pitch = value
-
-    # winding coil pitch or coil span expressed in slots (coil_pitch1=Zs/(2p)->full-pitch distributed winding, coil_pitch1<Zs/(2p)->chorded/shorted-pitch distributed winding, coil_pitch1=1->tooth-winding). Coil pitch is sometimes written 1/9 means Input.Magnetics.coil_pitch1=9-1=8
-    # Type : int, min = 0, max = 1000
-    coil_pitch = property(
-        fget=_get_coil_pitch,
-        fset=_set_coil_pitch,
-        doc=u"""winding coil pitch or coil span expressed in slots (coil_pitch1=Zs/(2p)->full-pitch distributed winding, coil_pitch1<Zs/(2p)->chorded/shorted-pitch distributed winding, coil_pitch1=1->tooth-winding). Coil pitch is sometimes written 1/9 means Input.Magnetics.coil_pitch1=9-1=8""",
-    )
--- Methods/Machine/WindingDW1L/comp_connection_mat.py
+++ Methods/Machine/WindingDW1L/comp_connection_mat.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
-"""@package Methods.Machine.Winding._comp_wind_type_4
-Compute the Winding Matrix (for type 4) Method
-@date Created on Tue Dec 16 09:53:23 2014
-@copyright (C) 2014-2015 EOMYS ENGINEERING.
-@author pierre_b
-"""
-from numpy import mod, zeros
+
+
+from numpy import mod, zeros, arange, sign
 
 from pyleecan.Methods.Machine.Winding import WindingError
 from pyleecan.Functions.Winding.reverse_wind_mat import reverse_wind_mat
@@ -13,8 +9,8 @@ from pyleecan.Functions.Winding.shift_wind_mat import shift_wind_mat
 
 
 def comp_connection_mat(self, Zs):
-    """Compute the Winding Matrix (for winding type 4) (Nlay_rad=1,Nlay_tan=1)
-    type 4 : DISTRIBUTED SHORTED PITCH INTEGRAL WINDING
+    """Compute the Winding Matrix (for winding type 3 or 4) (Nlay_rad=1 or 2,Nlay_tan=1)
+    type 3 or 4 : DISTRIBUTED SHORTED PITCH INTEGRAL WINDING
 
     Parameters
     ----------
@@ -26,11 +22,11 @@ def comp_connection_mat(self, Zs):
     Returns
     -------
     wind_mat: numpy.ndarray
-        Winding Matrix (1, 1, Zs, qs)
+        Winding Matrix (1 or 2, 1, Zs, qs)
 
     Raises
     ------
-    WindingT4DefMsError
+    WindingDefMsError
         Zs/2/p/qs must be an integer
 
     """
@@ -38,29 +34,49 @@ def comp_connection_mat(self, Zs):
     assert Zs > 0, "Zs must be >0"
     assert Zs % 1 == 0, "Zs must be an integer"
 
+    coil_pitch = self.coil_pitch
     p = self.p
+    nlay = self.get_dim_wind()[0]
     qs = self.qs
     ms = Zs / 2.0 / float(p) / float(qs)
     tausp = Zs / 2.0 / float(p)
-    # nlay = 2
-    # Ncspc= Zs/(2.0*qs*self.Npcpp/nlay)  # number of coils in series per parallel circuit
-    # Ntspc = self.Ntcoil * Ncspc #Number of turns in series per phase
     Ntcoil = self.Ntcoil  # number of turns per coils
-    wind_mat = zeros((1, 1, Zs, qs))
+    wind_mat = zeros((nlay, 1, Zs, qs))
 
+    if qs == 3:
+        phase_order = [1, -3, 2]
+    else:
+        phase_order = range(1, qs + 1)
     if ms % 1 != 0:  # if ms isn't an integer
-        raise WindingT4DefMsError(
+        raise WindingDefMsError(
             "wrong winding definition, Zs/2/p/qs must " "be an integer !"
         )
     ms = int(ms)
     tausp = int(tausp)  # if ms is an integer, tausp is
 
     # shorted pitch Nlay-layered integral overlapping windings
-    for i in range(0, 2 * p):
-        for ph in range(0, qs):
-            for k in range(0, ms):  # cf Gieras p36
-                s = mod((i) * tausp + (ph) * ms + k + 1 - 1, Zs)
-                wind_mat[0, 0, s, ph] += ((-1) ** (i + ph)) * (Ntcoil)
+    for nl in range(0, nlay):
+        for i in range(0, p):
+            for k in range(0, qs):
+                ph = abs(phase_order[k])
+                # cf Gieras p36
+                z = (
+                    arange(1, ms + 1)
+                    + i * (Zs / p)
+                    + k * ms
+                    + nl * (coil_pitch - tausp)
+                )
+
+                sp = ((z - 1) % Zs).astype(int)  # positive pole
+                sm = ((z + Zs / 2 / p - 1) % Zs).astype(int)  # negative pole
+                for s in sp:
+                    wind_mat[nl, 0, s, ph - 1] = Ntcoil * sign(
+                        phase_order[k]
+                    )  # Accumulation for a single slot
+                for s in sm:
+                    wind_mat[nl, 0, s, ph - 1] = -Ntcoil * sign(
+                        phase_order[k]
+                    )  # Accumulation for a single slot
 
     # Apply the transformations
     if self.is_reverse_wind:
@@ -71,7 +87,7 @@ def comp_connection_mat(self, Zs):
     return wind_mat
 
 
-class WindingT4DefMsError(WindingError):
+class WindingDefMsError(WindingError):
     """ """
 
     pass
