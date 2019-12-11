934cf05c29384d82c692136d426f6f5d01d35932
RPile <raphael.pile@eomys.com>
2019-12-11 18:07:08 +0100

    [NF] Method to get element tags which contain a node tag

--- Tests/Methods/Mesh/test_get_node2element.py
+++ Tests/Methods/Mesh/test_get_node2element.py
@@ -2,7 +2,6 @@
 
 from unittest import TestCase
 from pyleecan.Classes.Mesh import Mesh
-from pyleecan.Classes.ElementDict import ElementDict
 from pyleecan.Classes.ElementMat import ElementMat
 import numpy as np
 
@@ -10,52 +9,18 @@ import numpy as np
 class unittest_getnode2element(TestCase):
     """unittest to get elements containing specific node(s)"""
 
-    def test_ElementDict(self):
-        # Init 1
-        mesh = Mesh()
-        mesh.element = ElementDict()
-        mesh.element.connectivity = {"Triangle": np.array([[0, 1, 2], [1, 2, 3]])}
-        mesh.element.tag = {"Triangle": np.array([1, 2])}
-        mesh.element.nb_elem = {"Triangle": 2}
-        mesh.element.nb_node_per_element = {"Triangle": 3}
-        # Method test 1
-        elem_tag = mesh.element.get_node2element(1)
-        # Check results
-        solution = np.array([1, 2])  # Warning, elements tags, not line position !
-        testA = np.sum(abs(solution - elem_tag))
-        msg = (
-            "Wrong projection: returned "
-            + str(elem_tag)
-            + ", expected: "
-            + str(solution)
-        )
-        DELTA = 1e-10
-        self.assertAlmostEqual(testA, 0, msg=msg, delta=DELTA)
-
-        # Method test 2
-        elem_tag = mesh.element.get_node2element(3)
-        solution = np.array([2])
-        testA = np.sum(abs(solution - elem_tag))
-        msg = (
-            "Wrong projection: returned "
-            + str(elem_tag)
-            + ", expected: "
-            + str(solution)
-        )
-        DELTA = 1e-10
-        self.assertAlmostEqual(testA, 0, msg=msg, delta=DELTA)
-
     def test_ElementMat(self):
         # Init 1
         mesh = Mesh()
-        mesh.element = ElementMat()
-        mesh.element.connectivity = np.array(
+        mesh.element["Triangle3"] = ElementMat()
+        mesh.element["Triangle3"].connectivity = np.array(
             [[11, 12, 13], [0, 1, 2], [1, 2, 3], [120, 11, 12]]
         )
-        mesh.element.nb_elem = 2
-        mesh.element.nb_node_per_element = 3
+        mesh.element["Triangle3"].nb_elem = 4
+        mesh.element["Triangle3"].nb_node_per_element = 3
+        mesh.element["Triangle3"].tag = np.array([0,1,2,3])
         # Method test 1
-        elem_tag = mesh.element.get_node2element(1)
+        elem_tag = mesh.element["Triangle3"].get_node2element(1)
         # Check results
         solution = np.array([1, 2])  # In this case, element tag = line position
         testA = np.sum(abs(solution - elem_tag))
