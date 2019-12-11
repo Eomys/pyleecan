3d51b9a3a6910a0a29e5fbbfc322ef18da56bbe7
RPile <raphael.pile@eomys.com>
2019-12-11 16:15:58 +0100

    [BC] vstack instead of concatenate when adding nodes coordinates, and empty tag vector case

--- Methods/Mesh/NodeMat/add_node.py
+++ Methods/Mesh/NodeMat/add_node.py
@@ -23,12 +23,15 @@ def add_node(self, coord):
         return None
     else:
         if self.coordinate.size > 0:
-            self.coordinate = np.concatenate((self.coordinate, coord))
+            self.coordinate = np.vstack((self.coordinate, coord))
             new_tag = max(self.tag) + 1
+            self.tag = np.concatenate((self.tag, np.array([new_tag], dtype=int)))
             self.nb_node = self.nb_node + 1
         else:
             self.coordinate = np.concatenate((self.coordinate, coord))
             new_tag = 0
-            self.tag = new_tag
+            self.tag = np.array([], dtype=int)
+            self.tag = np.concatenate((self.tag, np.array([new_tag], dtype=int)))
+            self.nb_node = self.nb_node + 1
 
     return new_tag
