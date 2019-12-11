99203a160000f1bbc24db2fb25683f80d3d7e3af
RPile <raphael.pile@eomys.com>
2019-12-11 16:16:48 +0100

    [BC] Taking into account special cases: empty node list, and only 1 node

--- Methods/Mesh/NodeMat/is_exist.py
+++ Methods/Mesh/NodeMat/is_exist.py
@@ -21,20 +21,29 @@ def is_exist(self, new_coord):
     # Check the existence of the element
     delta = self.delta
     nb_node = self.nb_node
-    point = np.tile(new_coord, (nb_node, 1))
     coords = self.coordinate
-    dist_node = np.reshape(
-        np.sqrt(
-            np.square(coords[:, 0] - point[:, 0])
-            + np.square(coords[:, 1] - point[:, 1])
-        ),
-        (nb_node, 1),
-    )
 
-    min_node_list = np.argsort(dist_node, axis=0)
-
-    if min_node_list[0] > delta:
+    if nb_node == 0:
         return False
+    if nb_node == 1:
+        dist = np.sqrt(np.sum(np.square(coords - new_coord)))
+        if dist > delta:
+            return False
+        else:
+            return True
     else:
-        return True
+        point = np.tile(new_coord, (nb_node, 1))
+        dist_node = np.reshape(
+            np.sqrt(
+                np.square(coords[:, 0] - point[:, 0])
+                + np.square(coords[:, 1] - point[:, 1])
+            ),
+            (nb_node, 1),
+        )
 
+        min_node_list = np.argsort(dist_node, axis=0)
+
+        if dist_node[min_node_list[0]] > delta:
+            return False
+        else:
+            return True
