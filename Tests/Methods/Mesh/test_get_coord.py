9d23aa741da97b450f1012a63cfc4db0edca7515
RPile <raphael.pile@eomys.com>
2019-12-12 16:06:24 +0100

    [CC] Using setUp for test_get_cooord

--- Tests/Methods/Mesh/test_get_coord.py
+++ Tests/Methods/Mesh/test_get_coord.py
@@ -7,60 +7,47 @@ from pyleecan.Classes.ElementMat import ElementMat
 import numpy as np
 
 
-class unittest_get_node(TestCase):
-    """unittest for nodes getter methods"""
+class unittest_get_coord(TestCase):
+    """unittest for NodeMat get_node methods"""
 
-    def test_NodeMat_ElementMat(self):
-        """unittest with ElementMat and NodeMat objects"""
-        # Init
-        mesh = Mesh()
-        mesh.element["Triangle3"] = ElementMat(nb_node_per_element=3)
-        mesh.node = NodeMat()
-        mesh.node.add_node(np.array([0, 0]))
-        mesh.node.add_node(np.array([1, 0]))
-        mesh.node.add_node(np.array([1, 2]))
-        mesh.node.add_node(np.array([2, 3]))
+    def setUp(self):
+        self.mesh = Mesh()
+        self.mesh.element["Triangle3"] = ElementMat(nb_node_per_element=3)
+        self.mesh.node = NodeMat()
+        self.mesh.node.add_node(np.array([0, 0]))
+        self.mesh.node.add_node(np.array([1, 0]))
+        self.mesh.node.add_node(np.array([1, 2]))
+        self.mesh.node.add_node(np.array([2, 3]))
 
-        mesh.add_element(np.array([0, 1, 2]), "Triangle3")
-        mesh.add_element(np.array([1, 2, 3]), "Triangle3")
+        self.mesh.add_element(np.array([0, 1, 2]), "Triangle3")
+        self.mesh.add_element(np.array([1, 2, 3]), "Triangle3")
 
-        node_tags = mesh.get_connectivity(1)
+    def test_NodeMat_1node(self):
+        """unittest for a node"""
+        node_tags = self.mesh.get_connectivity(1)
         # Method test 1
-        nodes_coord = mesh.node.get_coord(node_tags)
+        coord = self.mesh.node.get_coord(node_tags)
         # Check result
         solution = np.array([[1, 0], [1, 2], [2, 3]])
-        testA = np.sum(abs(solution - nodes_coord))
-        msg = (
-            "Wrong projection: returned "
-            + str(node_tags)
-            + ", expected: "
-            + str(solution)
-        )
+        testA = np.sum(abs(solution - coord))
+        msg = "Wrong output: returned " + str(coord) + ", expected: " + str(solution)
         DELTA = 1e-10
         self.assertAlmostEqual(testA, 0, msg=msg, delta=DELTA)
 
-        # Method test 2
-        nodes_coord = mesh.node.get_coord(-999)
-        # Check result
+    def test_NodeMat_false(self):
+        """unittest for a false node tag"""
+
+        coord = self.mesh.node.get_coord(-999)
         solution = None
-        testA = nodes_coord is None
-        msg = (
-            "Wrong projection: returned "
-            + str(nodes_coord)
-            + ", expected: "
-            + str(solution)
-        )
+        testA = coord is None
+        msg = "Wrong out: returned " + str(coord) + ", expected: " + str(solution)
         self.assertTrue(testA, msg=msg)
 
-        # Method test 3
-        nodes_coord = mesh.node.get_coord(None)
-        # Check result
+    def test_NodeMat_None(self):
+        """unittest for a None node tag"""
+
+        coord = self.mesh.node.get_coord(None)
         solution = None
-        testA = nodes_coord is None
-        msg = (
-            "Wrong projection: returned "
-            + str(nodes_coord)
-            + ", expected: "
-            + str(solution)
-        )
+        testA = coord is None
+        msg = "Wrong output: returned " + str(coord) + ", expected: " + str(solution)
         self.assertTrue(testA, msg=msg)
