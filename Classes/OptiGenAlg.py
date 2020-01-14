de84547adba3306f08b239dc911e32efb959de3b
Cedric Marinel <cedric.marinel@eomys.com>
2020-01-14 11:46:18 +0100

    [WP] Add the number of generation to OptiGenAlg

--- Classes/OptiGenAlg.py
+++ Classes/OptiGenAlg.py
@@ -17,14 +17,27 @@ from pyleecan.Classes.OptiProblem import OptiProblem
 
 
 class OptiGenAlg(FrozenClass):
-    """Multi-objectives optimization problem with some constraints"""
+    """Genetic algorithm class"""
 
     VERSION = 1
 
     # save method is available in all object
     save = save
 
-    def __init__(self, multi_output=-1, pop=[], selector=None, crossover=None, mutator=None, p_cross=0.9, p_mute=0.1, size_pop=50, problem=-1, init_dict=None):
+    def __init__(
+        self,
+        multi_output=-1,
+        pop=[],
+        selector=None,
+        crossover=None,
+        mutator=None,
+        p_cross=0.9,
+        p_mutate=0.1,
+        size_pop=50,
+        nb_gen=200,
+        problem=-1,
+        init_dict=None,
+    ):
         """Constructor of the class. Can be use in two ways :
         - __init__ (arg1 = 1, arg3 = 5) every parameters have name and default values
             for Matrix, None will initialise the property with an empty Matrix
@@ -39,7 +52,21 @@ class OptiGenAlg(FrozenClass):
         if problem == -1:
             problem = OptiProblem()
         if init_dict is not None:  # Initialisation by dict
-            check_init_dict(init_dict, ["multi_output", "pop", "selector", "crossover", "mutator", "p_cross", "p_mute", "size_pop", "problem"])
+            check_init_dict(
+                init_dict,
+                [
+                    "multi_output",
+                    "pop",
+                    "selector",
+                    "crossover",
+                    "mutator",
+                    "p_cross",
+                    "p_mutate",
+                    "size_pop",
+                    "nb_gen",
+                    "problem",
+                ],
+            )
             # Overwrite default value with init_dict content
             if "multi_output" in list(init_dict.keys()):
                 multi_output = init_dict["multi_output"]
@@ -53,10 +80,12 @@ class OptiGenAlg(FrozenClass):
                 mutator = init_dict["mutator"]
             if "p_cross" in list(init_dict.keys()):
                 p_cross = init_dict["p_cross"]
-            if "p_mute" in list(init_dict.keys()):
-                p_mute = init_dict["p_mute"]
+            if "p_mutate" in list(init_dict.keys()):
+                p_mutate = init_dict["p_mutate"]
             if "size_pop" in list(init_dict.keys()):
                 size_pop = init_dict["size_pop"]
+            if "nb_gen" in list(init_dict.keys()):
+                nb_gen = init_dict["nb_gen"]
             if "problem" in list(init_dict.keys()):
                 problem = init_dict["problem"]
         # Initialisation by argument
@@ -71,8 +100,9 @@ class OptiGenAlg(FrozenClass):
         self.crossover = crossover
         self.mutator = mutator
         self.p_cross = p_cross
-        self.p_mute = p_mute
+        self.p_mutate = p_mutate
         self.size_pop = size_pop
+        self.nb_gen = nb_gen
         # problem can be None, a OptiProblem object or a dict
         if isinstance(problem, dict):
             self.problem = OptiProblem(init_dict=problem)
@@ -91,27 +121,38 @@ class OptiGenAlg(FrozenClass):
         else:
             OptiGenAlg_str += "parent = " + str(type(self.parent)) + " object" + linesep
         if self.multi_output is not None:
-            OptiGenAlg_str += "multi_output = " + str(self.multi_output.as_dict()) + linesep + linesep
+            OptiGenAlg_str += (
+                "multi_output = " + str(self.multi_output.as_dict()) + linesep + linesep
+            )
         else:
             OptiGenAlg_str += "multi_output = None" + linesep + linesep
         OptiGenAlg_str += "pop = " + linesep + str(self.pop) + linesep
         if self._selector[1] is None:
             OptiGenAlg_str += "selector = " + str(self._selector[1])
         else:
-            OptiGenAlg_str += "selector = " + linesep + str(self._selector[1]) + linesep + linesep
+            OptiGenAlg_str += (
+                "selector = " + linesep + str(self._selector[1]) + linesep + linesep
+            )
         if self._crossover[1] is None:
             OptiGenAlg_str += "crossover = " + str(self._crossover[1])
         else:
-            OptiGenAlg_str += "crossover = " + linesep + str(self._crossover[1]) + linesep + linesep
+            OptiGenAlg_str += (
+                "crossover = " + linesep + str(self._crossover[1]) + linesep + linesep
+            )
         if self._mutator[1] is None:
             OptiGenAlg_str += "mutator = " + str(self._mutator[1])
         else:
-            OptiGenAlg_str += "mutator = " + linesep + str(self._mutator[1]) + linesep + linesep
+            OptiGenAlg_str += (
+                "mutator = " + linesep + str(self._mutator[1]) + linesep + linesep
+            )
         OptiGenAlg_str += "p_cross = " + str(self.p_cross) + linesep
-        OptiGenAlg_str += "p_mute = " + str(self.p_mute) + linesep
+        OptiGenAlg_str += "p_mutate = " + str(self.p_mutate) + linesep
         OptiGenAlg_str += "size_pop = " + str(self.size_pop) + linesep
+        OptiGenAlg_str += "nb_gen = " + str(self.nb_gen) + linesep
         if self.problem is not None:
-            OptiGenAlg_str += "problem = " + str(self.problem.as_dict()) + linesep + linesep
+            OptiGenAlg_str += (
+                "problem = " + str(self.problem.as_dict()) + linesep + linesep
+            )
         else:
             OptiGenAlg_str += "problem = None"
         return OptiGenAlg_str
@@ -133,9 +174,11 @@ class OptiGenAlg(FrozenClass):
             return False
         if other.p_cross != self.p_cross:
             return False
-        if other.p_mute != self.p_mute:
+        if other.p_mutate != self.p_mutate:
             return False
         if other.size_pop != self.size_pop:
+            return False
+        if other.nb_gen != self.nb_gen:
             return False
         if other.problem != self.problem:
             return False
@@ -154,18 +197,28 @@ class OptiGenAlg(FrozenClass):
         if self.selector is None:
             OptiGenAlg_dict["selector"] = None
         else:
-            OptiGenAlg_dict["selector"] = [dumps(self._selector[0]).decode('ISO-8859-2'), self._selector[1]]
+            OptiGenAlg_dict["selector"] = [
+                dumps(self._selector[0]).decode("ISO-8859-2"),
+                self._selector[1],
+            ]
         if self.crossover is None:
             OptiGenAlg_dict["crossover"] = None
         else:
-            OptiGenAlg_dict["crossover"] = [dumps(self._crossover[0]).decode('ISO-8859-2'), self._crossover[1]]
+            OptiGenAlg_dict["crossover"] = [
+                dumps(self._crossover[0]).decode("ISO-8859-2"),
+                self._crossover[1],
+            ]
         if self.mutator is None:
             OptiGenAlg_dict["mutator"] = None
         else:
-            OptiGenAlg_dict["mutator"] = [dumps(self._mutator[0]).decode('ISO-8859-2'), self._mutator[1]]
+            OptiGenAlg_dict["mutator"] = [
+                dumps(self._mutator[0]).decode("ISO-8859-2"),
+                self._mutator[1],
+            ]
         OptiGenAlg_dict["p_cross"] = self.p_cross
-        OptiGenAlg_dict["p_mute"] = self.p_mute
+        OptiGenAlg_dict["p_mutate"] = self.p_mutate
         OptiGenAlg_dict["size_pop"] = self.size_pop
+        OptiGenAlg_dict["nb_gen"] = self.nb_gen
         if self.problem is None:
             OptiGenAlg_dict["problem"] = None
         else:
@@ -184,8 +237,9 @@ class OptiGenAlg(FrozenClass):
         self.crossover = None
         self.mutator = None
         self.p_cross = None
-        self.p_mute = None
+        self.p_mutate = None
         self.size_pop = None
+        self.nb_gen = None
         if self.problem is not None:
             self.problem._set_None()
 
@@ -200,6 +254,7 @@ class OptiGenAlg(FrozenClass):
 
         if self._multi_output is not None:
             self._multi_output.parent = self
+
     # Optimization results containing every output
     # Type : OutputMultiOpti
     multi_output = property(
@@ -231,18 +286,23 @@ class OptiGenAlg(FrozenClass):
             check_var("selector", value, "list")
         except CheckTypeError:
             check_var("selector", value, "function")
-        if isinstance(value,list): # Load function from saved dict
-            self._selector = [loads(value[0].encode('ISO-8859-2')),value[1]]
+        if isinstance(value, list):  # Load function from saved dict
+            self._selector = [loads(value[0].encode("ISO-8859-2")), value[1]]
         elif value is None:
-            self._selector = [None,None]
+            self._selector = [None, None]
         elif callable(value):
-            self._selector = [value,getsource(value)]
+            self._selector = [value, getsource(value)]
         else:
-            raise TypeError('Expected function or list from a saved file, got: '+str(type(value))) 
+            raise TypeError(
+                "Expected function or list from a saved file, got: " + str(type(value))
+            )
+
     # Selector of the genetic algorithm
     # Type : function
     selector = property(
-        fget=_get_selector, fset=_set_selector, doc=u"""Selector of the genetic algorithm"""
+        fget=_get_selector,
+        fset=_set_selector,
+        doc=u"""Selector of the genetic algorithm""",
     )
 
     def _get_crossover(self):
@@ -255,18 +315,23 @@ class OptiGenAlg(FrozenClass):
             check_var("crossover", value, "list")
         except CheckTypeError:
             check_var("crossover", value, "function")
-        if isinstance(value,list): # Load function from saved dict
-            self._crossover = [loads(value[0].encode('ISO-8859-2')),value[1]]
+        if isinstance(value, list):  # Load function from saved dict
+            self._crossover = [loads(value[0].encode("ISO-8859-2")), value[1]]
         elif value is None:
-            self._crossover = [None,None]
+            self._crossover = [None, None]
         elif callable(value):
-            self._crossover = [value,getsource(value)]
+            self._crossover = [value, getsource(value)]
         else:
-            raise TypeError('Expected function or list from a saved file, got: '+str(type(value))) 
+            raise TypeError(
+                "Expected function or list from a saved file, got: " + str(type(value))
+            )
+
     # Crossover of the genetic algorithm
     # Type : function
     crossover = property(
-        fget=_get_crossover, fset=_set_crossover, doc=u"""Crossover of the genetic algorithm"""
+        fget=_get_crossover,
+        fset=_set_crossover,
+        doc=u"""Crossover of the genetic algorithm""",
     )
 
     def _get_mutator(self):
@@ -279,18 +344,23 @@ class OptiGenAlg(FrozenClass):
             check_var("mutator", value, "list")
         except CheckTypeError:
             check_var("mutator", value, "function")
-        if isinstance(value,list): # Load function from saved dict
-            self._mutator = [loads(value[0].encode('ISO-8859-2')),value[1]]
+        if isinstance(value, list):  # Load function from saved dict
+            self._mutator = [loads(value[0].encode("ISO-8859-2")), value[1]]
         elif value is None:
-            self._mutator = [None,None]
+            self._mutator = [None, None]
         elif callable(value):
-            self._mutator = [value,getsource(value)]
+            self._mutator = [value, getsource(value)]
         else:
-            raise TypeError('Expected function or list from a saved file, got: '+str(type(value))) 
+            raise TypeError(
+                "Expected function or list from a saved file, got: " + str(type(value))
+            )
+
     # Mutator of the genetic algorithm
     # Type : function
     mutator = property(
-        fget=_get_mutator, fset=_set_mutator, doc=u"""Mutator of the genetic algorithm"""
+        fget=_get_mutator,
+        fset=_set_mutator,
+        doc=u"""Mutator of the genetic algorithm""",
     )
 
     def _get_p_cross(self):
@@ -308,19 +378,19 @@ class OptiGenAlg(FrozenClass):
         fget=_get_p_cross, fset=_set_p_cross, doc=u"""Probability of crossover"""
     )
 
-    def _get_p_mute(self):
-        """getter of p_mute"""
-        return self._p_mute
+    def _get_p_mutate(self):
+        """getter of p_mutate"""
+        return self._p_mutate
 
-    def _set_p_mute(self, value):
-        """setter of p_mute"""
-        check_var("p_mute", value, "float", Vmin=0, Vmax=1)
-        self._p_mute = value
+    def _set_p_mutate(self, value):
+        """setter of p_mutate"""
+        check_var("p_mutate", value, "float", Vmin=0, Vmax=1)
+        self._p_mutate = value
 
-    # Probability of mutation 
+    # Probability of mutation
     # Type : float, min = 0, max = 1
-    p_mute = property(
-        fget=_get_p_mute, fset=_set_p_mute, doc=u"""Probability of mutation """
+    p_mutate = property(
+        fget=_get_p_mutate, fset=_set_p_mutate, doc=u"""Probability of mutation """
     )
 
     def _get_size_pop(self):
@@ -338,6 +408,21 @@ class OptiGenAlg(FrozenClass):
         fget=_get_size_pop, fset=_set_size_pop, doc=u"""Size of the population"""
     )
 
+    def _get_nb_gen(self):
+        """getter of nb_gen"""
+        return self._nb_gen
+
+    def _set_nb_gen(self, value):
+        """setter of nb_gen"""
+        check_var("nb_gen", value, "int", Vmin=1)
+        self._nb_gen = value
+
+    # Number of generations
+    # Type : int, min = 1
+    nb_gen = property(
+        fget=_get_nb_gen, fset=_set_nb_gen, doc=u"""Number of generations"""
+    )
+
     def _get_problem(self):
         """getter of problem"""
         return self._problem
@@ -349,6 +434,7 @@ class OptiGenAlg(FrozenClass):
 
         if self._problem is not None:
             self._problem.parent = self
+
     # Problem to solve
     # Type : OptiProblem
     problem = property(
