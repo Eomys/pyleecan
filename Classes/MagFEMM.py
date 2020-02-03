388053d853a4152481a22d73dc798c87d5f245ea
RPile <raphael.pile@eomys.com>
2020-02-03 14:35:22 +0100

    [WiP] Correcting class generator for merge

--- Classes/MagFEMM.py
+++ Classes/MagFEMM.py
@@ -297,9 +297,7 @@ class MagFEMM(Magnetics):
         MagFEMM_dict["Kgeo_fineness"] = self.Kgeo_fineness
         MagFEMM_dict["type_calc_leakage"] = self.type_calc_leakage
         MagFEMM_dict["file_name"] = self.file_name
-        MagFEMM_dict["FEMM_dict"] = dict()
-        for key, obj in self.FEMM_dict.items():
-            MagFEMM_dict["FEMM_dict"][key] = obj.as_dict()
+        MagFEMM_dict["FEMM_dict"] = self.FEMM_dict
         MagFEMM_dict["angle_stator"] = self.angle_stator
         MagFEMM_dict["is_get_mesh"] = self.is_get_mesh
         MagFEMM_dict["is_save_FEA"] = self.is_save_FEA
