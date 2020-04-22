abcace484f87ea255d2625fd16d09dbb12fd6f5a
CedMrnl <cedric.marinel@eomys.com>
2020-04-22 13:44:21 +0200

    [CC] Format with black

--- pyleecan/run_GUI.py
+++ pyleecan/run_GUI.py
@@ -7,7 +7,7 @@ from PyQt5.QtCore import QTranslator
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QIcon
 
-try: # Import if pyleecan is installed with pip 
+try:  # Import if pyleecan is installed with pip
     from .definitions import ROOT_DIR, DATA_DIR, MATLIB_DIR, PACKAGE_NAME
     from .GUI.Dialog.DMachineSetup.DMachineSetup import DMachineSetup
     from .GUI.Dialog.DMatLib.DMatLib import DMatLib
@@ -16,16 +16,26 @@ try: # Import if pyleecan is installed with pip
     from .GUI.Tools.MachinePlotWidget import MachinePlotWidget
     from .GUI.Tools.TreeView import TreeView
 
-except ImportError: # Import for dev version
+except ImportError:  # Import for dev version
     from definitions import PACKAGE_NAME, DATA_DIR, MATLIB_DIR, ROOT_DIR
 
-    exec("from " + PACKAGE_NAME + ".GUI.Dialog.DMachineSetup.DMachineSetup import DMachineSetup")
+    exec(
+        "from "
+        + PACKAGE_NAME
+        + ".GUI.Dialog.DMachineSetup.DMachineSetup import DMachineSetup"
+    )
     exec("from " + PACKAGE_NAME + ".GUI.Dialog.DMatLib.DMatLib import DMatLib")
-    exec("from " + PACKAGE_NAME + ".GUI.Dialog.DMatLib.WMatSelect.WMatSelect import WMatSelect")
+    exec(
+        "from "
+        + PACKAGE_NAME
+        + ".GUI.Dialog.DMatLib.WMatSelect.WMatSelect import WMatSelect"
+    )
     exec("from " + PACKAGE_NAME + ".GUI.Tools.SidebarWindow import SidebarWindow")
-    exec("from " + PACKAGE_NAME + ".GUI.Tools.MachinePlotWidget import MachinePlotWidget")
+    exec(
+        "from " + PACKAGE_NAME + ".GUI.Tools.MachinePlotWidget import MachinePlotWidget"
+    )
     exec("from " + PACKAGE_NAME + ".GUI.Tools.TreeView import TreeView")
-    
+
     sys.path.insert(0, ROOT_DIR)
 
 EXT_GUI = True
