28691f771a35f1a86c2bd5a36776067fc967e497
CedMrnl <cedric.marinel@eomys.com>
2020-04-29 11:38:52 +0200

    [NF] Create a user folder that will contains all the Data :     - Windows : %APPDATA%/Pyleecan/Data     - Linux and MacOS : $HOME/.local/share/Pyleecan/Data

--- pyleecan/__init__.py
+++ pyleecan/__init__.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 from .loggers import LOGGING_CONFIG_CONSOLE, LOGGING_CONFIG_FILE
 from logging.config import dictConfig
-
+from .definitions import PYLEECAN_USER_DIR
+from os.path import isdir
 dictConfig(LOGGING_CONFIG_CONSOLE)  ##
+
+if not isdir(PYLEECAN_USER_DIR): # Create Pyleecan user folder 
+    from .definitions import create_folder
+    create_folder()
--- pyleecan/definitions.py
+++ pyleecan/definitions.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 import sys
-from os.path import dirname, abspath, normpath, join, realpath
+from os.path import dirname, abspath, normpath, join, realpath, isdir
+import os
+import platform
+import shutil
+from logging import getLogger
 
 ROOT_DIR = normpath(abspath(join(dirname(__file__), "..")))
 MAIN_DIR = dirname(realpath(__file__))
@@ -19,3 +23,28 @@ DATA_DIR = join(MAIN_DIR, "Data")  # Absolute path to default data dir
 MATLIB_DIR = join(DATA_DIR, "Material")
 
 TEST_DIR = join(MAIN_DIR, "../Tests")
+
+# Pyleecan user folder
+if platform.system() == 'Windows':
+    PYLEECAN_USER_DIR = os.environ['APPDATA']+"/Pyleecan"
+    PYLEECAN_USER_DIR = PYLEECAN_USER_DIR.replace('\\','/')
+else: 
+    PYLEECAN_USER_DIR = os.environ['HOME']+"/.local/share/Pyleecan"
+
+def create_folder():
+    """
+    Create Pyleecan folder to copy Data into it.
+    
+    Windows : %APPDATA%/Pyleecan
+    Linux and MacOS : $HOME/.local/share/Pyleecan
+    """
+    logger = getLogger("Pyleecan")
+    
+    if not isdir(PYLEECAN_USER_DIR):
+        # Create Pyleecan folder and copy Data folder
+        logger.debug('Copying Data folder in '+PYLEECAN_USER_DIR+"/Data")
+        shutil.copytree(__file__[:max(__file__.rfind('/'),__file__.rfind('\\'))] + '/Data', PYLEECAN_USER_DIR + "/Data")
+    
+        
+    
+   
