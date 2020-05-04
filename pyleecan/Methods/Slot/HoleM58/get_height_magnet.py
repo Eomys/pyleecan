04350d8e7f78037210ea15cc7b57c404c10412c9
Pierre Bonneel <pierre.bonneel@gmail.com>
2020-05-04 14:47:52 +0200

    [NF] Adding HoleM58 (missing GUI part)

---
+++ pyleecan/Methods/Slot/HoleM58/get_height_magnet.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+def get_height_magnet(self):
+    """get the height of the hole magnets
+
+    Parameters
+    ----------
+    self : HoleM58
+        A HoleM58 object
+
+    Returns
+    -------
+    Hmag: float
+        height of the Magnet [m]
+
+    """
+
+    return self.H2
---
+++ pyleecan/Methods/Slot/HoleM58/remove_magnet.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+def remove_magnet(self):
+    """Remove the magnet (set to None) of the Hole
+
+    Parameters
+    ----------
+    self : HoleM58
+        a HoleM58 object
+    """
+
+    self.magnet_0 = None
