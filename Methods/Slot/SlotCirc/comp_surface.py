63e30d89b65d5764b540964b20b0e44144359ce4
Pierre Bonneel <pierre.bonneel@gmail.com>
2020-05-29 11:50:56 +0200

    [NF] New SlotCirc for notches

---
+++ Methods/Slot/SlotCirc/comp_surface.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from numpy import sin, tan
+
+
+def comp_surface(self):
+    """Compute the Slot total surface (by analytical computation).
+    Caution, the bottom of the Slot is an Arc
+
+    Parameters
+    ----------
+    self : SlotCirc
+        A SlotCirc object
+
+    Returns
+    -------
+    S: float
+        Slot total surface [m**2]
+
+    """
+
+    return self.comp_surface_wind()
