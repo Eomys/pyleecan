1e2c61ae0ee911555e9d6a5c9357ba679e52c252
helene-t <helene.toubin@eomys.com>
2020-06-03 09:42:10 +0200

    [WP] Electrical module test

--- Tests/Validation/Simulation/test_E_IPMSM_FL_002.py
+++ Tests/Validation/Simulation/test_E_IPMSM_FL_002.py
@@ -1,4 +1,4 @@
-from numpy import pi, ones, zeros
+from numpy import zeros, ones, pi, array
 from os.path import join
 import matplotlib.pyplot as plt
 from Tests import save_validation_path as save_path
@@ -8,40 +8,39 @@ from Tests.Validation.Machine.IPMSM_A import IPMSM_A
 
 from pyleecan.Classes.InputCurrent import InputCurrent
 from pyleecan.Classes.ImportGenVectLin import ImportGenVectLin
-from pyleecan.Classes.ImportGenVectSin import ImportGenVectSin
-from pyleecan.Classes.ImportGenMatrixSin import ImportGenMatrixSin
 from pyleecan.Classes.ImportMatrixVal import ImportMatrixVal
 
-from pyleecan.Classes.EEC_PMSM import EEC_PMSM
-from pyleecan.Classes.FluxLinkFEMM import FluxLinkFEMM
-from pyleecan.Classes.IndMagFEMM import IndMagFEMM
-from pyleecan.Classes.DriveWave import DriveWave
+from pyleecan.Classes.MagFEMM import MagFEMM
+from pyleecan.Classes.ForceMT import ForceMT
 from pyleecan.Classes.Output import Output
 import pytest
 
 
+@pytest.mark.long
 @pytest.mark.validation
 @pytest.mark.FEMM
-def test_E_IPMSM_FL_002():
-    """Validation of the PMSM Electrical Equivalent Circuit with the Prius machine
+def test_EM_IPMSM_FL_002():
+    """Validation of the TOYOTA Prius 2004 interior magnet (V shape) with distributed winding
+    50 kW peak, 400 Nm peak at 1500 rpm from publication
+    from publication
+    Z. Yang, M. Krishnamurthy and I. P. Brown,
+    "Electromagnetic and vibrational characteristic of IPM over full torque-speed range,"
+    Electric Machines & Drives Conference (IEMDC), 2013 IEEE International, Chicago, IL, 2013, pp. 295-302.
+    Test compute the Flux in FEMM, with and without symmetry
     """
-    simu = Simu1(name="E_IPMSM_FL_002", machine=IPMSM_A)
+    simu = Simu1(name="EM_IPMSM_FL_002", machine=IPMSM_A)
 
     # Definition of the enforced output of the electrical module
-    Nr = ImportMatrixVal(value=ones(2048) * 2504)
-    time = ImportGenVectLin(start=0, stop=1, num=2048, endpoint=False)
-    angle = ImportGenVectLin(start=0, stop=2 * pi, num=2048, endpoint=False)
-    Is_mat = zeros((2048, 3))
+    Nr = ImportMatrixVal(value=ones(1) * 2504)
+    Is_mat = zeros((1, 3))
+    Is_mat[0, :] = array([0, 12.2474, -12.2474])
     Is = ImportMatrixVal(value=Is_mat)
-
-    driveA = ImportGenVectSin(f=50, A=220, Phi=0, N=2048, Tf=1)
-    driveB = ImportGenVectSin(f=50, A=220, Phi=-2 * pi / 3, N=2048, Tf=1)
-    driveC = ImportGenVectSin(f=50, A=220, Phi=2 * pi / 3, N=2048, Tf=1)
-    drive = ImportGenMatrixSin(sin_list=[driveA, driveB, driveC])
+    time = ImportGenVectLin(start=0, stop=0, num=1, endpoint=False)
+    angle = ImportGenVectLin(start=0, stop=2 * pi, num=2048, endpoint=False)
 
     simu.input = InputCurrent(
         Is=Is,
-        Ir=None,
+        Ir=None,  # No winding on the rotor
         Nr=Nr,
         angle_rotor=None,  # Will be computed
         time=time,
@@ -49,25 +48,44 @@ def test_E_IPMSM_FL_002():
         angle_rotor_initial=0.86,
     )
 
-    # Definition of the electrical simulation (FEMM)
-    simu.elec.eec = EEC_PMSM(
-        freq0=50,
-        indmag=IndMagFEMM(is_symmetry_a=True, sym_a=4, is_antiper_a=True, Nt_tot=3),
-        fluxlink=FluxLinkFEMM(is_symmetry_a=True, sym_a=4, is_antiper_a=True, Nt_tot=3),
-        drive=DriveWave(wave=drive),
+    # Definition of the magnetic simulation (no symmetry)
+    simu.mag = MagFEMM(
+        type_BH_stator=0,
+        type_BH_rotor=0,
+        is_symmetry_a=False,
+        is_antiper_a=True,
+        Kgeo_fineness=0.75,
     )
-
-    simu.mag = None
     simu.force = None
     simu.struct = None
+    # simu.struct.force = ForceMT()
+    # Copy the simu and activate the symmetry
+    simu_sym = Simu1(init_dict=simu.as_dict())
+    simu_sym.mag.is_symmetry_a = True
+    simu_sym.mag.sym_a = 4
+    simu_sym.mag.is_antiper_a = True
+    simu_sym.struct = None
 
     out = Output(simu=simu)
+    out.post.legend_name = "No symmetry"
     simu.run()
+
+    out2 = Output(simu=simu_sym)
+    out2.post.legend_name = "1/2 symmetry"
+    out2.post.line_color = "r--"
+    simu_sym.run()
 
     # Plot the result by comparing the two simulation
     plt.close("all")
-    out.plot_A_time("elec.Currents", index_list=[0, 1, 2])
+    out.plot_B_space(out_list=[out2])
 
     fig = plt.gcf()
-    fig.savefig(join(save_path, "test_E_IPMSM_FL_002_currents.png"))
+    fig.savefig(join(save_path, "test_EM_IPMSM_FL_002_sym.png"))
+
+    # # Plot the surface magnetic forces
+    # plt.close("all")
+    # out.plot_force_space(j_t0=0, is_deg=False, out_list=[])
+
+    # fig = plt.gcf()
+    # fig.savefig(join(save_path, "test_EM_IPMSM_FL_002_force.png"))
 
