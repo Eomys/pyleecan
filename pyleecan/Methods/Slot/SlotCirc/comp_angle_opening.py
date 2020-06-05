a37472c5cb11aafbcd0c0f320ec5b5bb0d4c2cd5
Pierre Bonneel <pierre.bonneel@gmail.com>
2020-06-05 17:33:07 +0200

    [CC] Moving method folder after merge

---
+++ pyleecan/Methods/Slot/SlotCirc/comp_angle_opening.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from numpy import arcsin
+
+
+def comp_angle_opening(self):
+    """Compute the average opening angle of the Slot
+
+    Parameters
+    ----------
+    self : SlotCirc
+        A SlotCirc object
+
+    Returns
+    -------
+    alpha: float
+        Average opening angle of the slot [rad]
+    """
+
+    Rbo = self.get_Rbo()
+
+    return float(2 * arcsin(self.W0 / (2 * Rbo)))
