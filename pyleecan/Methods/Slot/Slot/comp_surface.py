427093c22d0ea1cd7f805ca753c6d0e586ca4dd4
Pierre Bonneel <pierre.bonneel@gmail.com>
2020-06-08 10:16:20 +0200

    [NF] Adding possibility to change discretization for numerical slot computation

--- pyleecan/Methods/Slot/Slot/comp_surface.py
+++ pyleecan/Methods/Slot/Slot/comp_surface.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-def comp_surface(self):
+def comp_surface(self, Ndisc=200):
     """Compute the Slot total surface (by numerical computation).
     Caution, the bottom of the Slot is an Arc
 
@@ -9,13 +9,14 @@ def comp_surface(self):
     ----------
     self : Slot
         A Slot object
+    Ndisc : int
+        Number of point to discretize the lines
 
     Returns
     -------
     S: float
         Slot total surface [m**2]
-
     """
 
     surf = self.get_surface()
-    return surf.comp_surface()
+    return surf.comp_surface(Ndisc=Ndisc)
