f17f612adb135d1e59b931b51abd26e3fba29094
RPile <raphael.pile@eomys.com>
2020-06-23 19:08:50 +0200

    [NF] Update get_vertice and corresponding unittest

--- pyleecan/Methods/Mesh/MeshMat/get_point.py
+++ pyleecan/Methods/Mesh/MeshMat/get_point.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
 
-def get_point(self, indice=None, is_indice=False):
+def get_point(self, indices=None, is_indice=False):
     """Return a matrix of points coordinates.
 
     Parameters
     ----------
     self : Mesh
         an Mesh object
-    indice : list
+    indices : list
         Indices of the targeted points. If None, return all.
     is_indice: bool
         Option to return the points indices (useful for unsorted
@@ -21,10 +21,13 @@ def get_point(self, indice=None, is_indice=False):
         points indices
 
     """
-    if indice is None:
+    if indices is None:
         if is_indice:
             return self.point.coordinate, self.point.indice
         else:
             return self.point.coordinate
     else:
-        return self.point.coordinate[indice, :]
+        if indices.size == 0:
+            return indices
+        else:
+            return self.point.coordinate[indices, :]
--- pyleecan/Methods/Mesh/MeshMat/get_vertice.py
+++ pyleecan/Methods/Mesh/MeshMat/get_vertice.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 
-def get_vertice(self, elem_type=None):
+def get_vertice(self, indices=None):
     """Return a connectivity matrix where the points indices are replaced by their coordinates.
 
     Parameters
@@ -22,14 +22,18 @@ def get_vertice(self, elem_type=None):
 
     """
 
-    cells = self.get_cell()
+    cells = self.get_cell(indices=indices)
+    vertices = dict()
+    for key in cells:
+        vertices[key] = self.get_point(cells[key])
 
-    if nb_elem == 1:
-        vertices = np.zeros((nb_node_per_elem, 2))
-        vertices = self.node.get_coord(connect_select)
-    else:
-        vertices = np.zeros((nb_elem, nb_node_per_elem, 2))
-        for ie in range(nb_elem):
-            vertices[ie, :, :] = self.node.get_coord(connect_select[ie, :])
-
-    return vertices, nb_elem
+    return vertices
+    # if nb_elem == 1:
+    #     vertices = np.zeros((nb_node_per_elem, 2))
+    #     vertices = self.node.get_coord(connect_select)
+    # else:
+    #     vertices = np.zeros((nb_elem, nb_node_per_elem, 2))
+    #     for ie in range(nb_elem):
+    #         vertices[ie, :, :] = self.node.get_coord(connect_select[ie, :])
+    #
+    # return vertices, nb_elem
