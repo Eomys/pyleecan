5cb0d3afd89f0245458c76f9980dfbff05696f19
helene-t <helene.toubin@eomys.com>
2020-07-20 10:32:54 +0200

    [WP] import vector fields

--- pyleecan/Methods/Simulation/InputFlux/gen_input.py
+++ pyleecan/Methods/Simulation/InputFlux/gen_input.py
@@ -2,7 +2,6 @@
 
 from ....Classes.OutMag import OutMag
 from ....Methods.Simulation.Input import InputError
-from SciDataTool import DataLinspace, DataTime, VectorField
 from numpy import ndarray
 
 
@@ -28,7 +27,6 @@ def gen_input(self):
             + str(output.time.shape)
             + " shape found"
         )
-    Nt_tot = len(output.time)
 
     # Load and check angle
     if self.angle is None:
@@ -41,65 +39,15 @@ def gen_input(self):
             + str(output.angle.shape)
             + " shape found"
         )
-    Na_tot = len(output.angle)
 
-    if self.Br is None:
-        raise InputError("ERROR: InFlux.Br missing")
-    Br = self.Br.get_data()
-    if not isinstance(Br, ndarray) or Br.shape != (Nt_tot, Na_tot):
-        raise InputError(
-            "ERROR: InFlux.Br must be a matrix with the shape "
-            + str((Nt_tot, Na_tot))
-            + " (len(time), stator phase number), "
-            + str(Br.shape)
-            + " returned"
-        )
-    Time = DataLinspace(
-        name="time",
-        unit="s",
-        symmetries={},
-        initial=output.time[0],
-        final=output.time[-1],
-        number=Nt_tot,
-    )
-    Angle = DataLinspace(
-        name="angle",
-        unit="rad",
-        symmetries={},
-        initial=output.angle[0],
-        final=output.angle[-1],
-        number=Na_tot,
-    )
-    Br_data = DataTime(
-        name="Airgap radial flux density",
-        unit="T",
-        symbol="B_r",
-        axes=[Time, Angle],
-        values=Br,
-    )
-    output.B = VectorField(
-        name="Airgap flux density",
-        components={"radial": Br_data}
-    )
-
-    if self.Bt is not None:
-        Bt = self.Bt.get_data()
-        if not isinstance(Bt, ndarray) or Bt.shape != (Nt_tot, Na_tot):
-            raise InputError(
-                "ERROR: InFlux.Bt must be a matrix with the shape "
-                + str((Nt_tot, Na_tot))
-                + " (len(time), rotor phase number), "
-                + str(Bt.shape)
-                + " returned"
-            )
-        Bt_data = DataTime(
-            name="Airgap tangential flux density",
-            unit="T",
-            symbol="B_t",
-            axes=[Time, Angle],
-            values=Bt,
-        )
-        output.B.components["tangential"] = Bt_data
+    if self.B is None:
+        raise InputError("ERROR: InFlux.B missing")
+    if self.B.name is None:
+        self.B.name = "Airgap flux density"
+    if self.B.symbol is None:
+        self.B.symbol = "B"
+    B = self.B.get_data()
+    output.B = B
 
     if self.parent.parent is None:
         raise InputError(
--- pyleecan/Methods/Simulation/InputForce/gen_input.py
+++ pyleecan/Methods/Simulation/InputForce/gen_input.py
@@ -2,7 +2,6 @@
 
 from ....Classes.OutStruct import OutStruct
 from ....Methods.Simulation.Input import InputError
-from numpy import ndarray
 
 
 def gen_input(self):
@@ -15,60 +14,15 @@ def gen_input(self):
     """
 
     output = OutStruct()
-    # Load and check time
-    if self.time is None:
-        raise InputError("ERROR: InputForce.time missing")
-    output.time = self.time.get_data()
 
-    if not isinstance(output.time, ndarray) or len(output.time.shape) != 1:
-        # time should be a vector
-        raise InputError(
-            "ERROR: InputForce.time should be a vector, "
-            + str(output.time.shape)
-            + " shape found"
-        )
-    Nt_tot = len(output.time)
-
-    # Load and check angle
-    if self.angle is None:
-        raise InputError("ERROR: InputForce.angle missing")
-    output.angle = self.angle.get_data()
-    if not isinstance(output.angle, ndarray) or len(output.angle.shape) != 1:
-        # angle should be a vector
-        raise InputError(
-            "ERROR: InputForce.angle should be a vector, "
-            + str(output.angle.shape)
-            + " shape found"
-        )
-    Na_tot = len(output.angle)
-
-    if self.Prad is None:
-        raise InputError("ERROR: InputForce.Prad missing")
-    output.Prad = self.Prad.get_data()
-    if not isinstance(output.Prad, ndarray) or output.Prad.shape != (Nt_tot, Na_tot):
-        raise InputError(
-            "ERROR: InputForce.Prad must be a matrix with the shape "
-            + str((Nt_tot, Na_tot))
-            + " (len(time), stator phase number), "
-            + str(output.Prad.shape)
-            + " returned"
-        )
-
-    if self.Ptan is not None:
-        output.Ptan = self.Ptan.get_data()
-        if not isinstance(output.Ptan, ndarray) or output.Ptan.shape != (
-            Nt_tot,
-            Na_tot,
-        ):
-            raise InputError(
-                "ERROR: InputForce.Ptan must be a matrix with the shape "
-                + str((Nt_tot, Na_tot))
-                + " (len(time), rotor phase number), "
-                + str(output.Ptan.shape)
-                + " returned"
-            )
-    else:
-        output.Ptan = None
+    if self.P is None:
+        raise InputError("ERROR: InForce.P missing")
+    if self.P.name is None:
+        self.P.name = "Magnetic airgap surface force"
+    if self.P.symbol is None:
+        self.P.symbol = "P"
+    P = self.P.get_data()
+    output.P = P
 
     if self.parent.parent is None:
         raise InputError(
