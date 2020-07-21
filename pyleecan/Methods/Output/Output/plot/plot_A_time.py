d76fc53308c808583f12920d37b127be46663e89
helene-t <helene.toubin@eomys.com>
2020-07-21 15:40:47 +0200

    [WP] plot commands for VectorField objects

--- pyleecan/Methods/Output/Output/plot/plot_A_time.py
+++ pyleecan/Methods/Output/Output/plot/plot_A_time.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from .....Functions.Plot.plot_A_time import plot_A_time as plot_A_time_fct
-
+from .....Functions.init_fig import init_fig
+from SciDataTool import VectorField
 
 def plot_A_time(
     self,
@@ -15,6 +16,7 @@ def plot_A_time(
     is_norm=False,
     unit="SI",
     data_list=[],
+    component_list=None,
     legend_list=[],
     color_list=[],
     save_path=None,
@@ -49,6 +51,8 @@ def plot_A_time(
         unit in which to plot the field
     data_list : list
         list of Data objects to compare
+    component_list : list
+        list of component names to plot in separate figures
     legend_list : list
         list of legends to use for each Data object (including reference one) instead of data.name
     color_list : list
@@ -69,23 +73,52 @@ def plot_A_time(
     phys = getattr(self, Data_str.split(".")[0])
     data = getattr(phys, Data_str.split(".")[1])
 
+    (fig, axes, patch_leg, label_leg) = init_fig(fig, shape="rectangle")
+    
     # Call the plot function
-    plot_A_time_fct(
-        data,
-        index_list=index_list,
-        alpha=alpha,
-        alpha_index=alpha_index,
-        is_fft=is_fft,
-        is_elecorder=is_elecorder,
-        freq_max=freq_max,
-        is_norm=is_norm,
-        unit=unit,
-        data_list=data_list,
-        legend_list=legend_list,
-        color_list=color_list,
-        save_path=save_path,
-        y_min=y_min,
-        y_max=y_max,
-        mag_max=mag_max,
-        is_auto_ticks=is_auto_ticks,
-    )
+    if isinstance(data, VectorField):
+        if component_list is None: # default: extract all components
+            component_list = data.components.keys()
+        for comp in component_list:
+            plot_A_time_fct(
+                data.components[comp],
+                index_list=index_list,
+                alpha=alpha,
+                alpha_index=alpha_index,
+                is_fft=is_fft,
+                is_elecorder=is_elecorder,
+                freq_max=freq_max,
+                is_norm=is_norm,
+                unit=unit,
+                data_list=data_list,
+                legend_list=legend_list,
+                color_list=color_list,
+                save_path=save_path,
+                y_min=y_min,
+                y_max=y_max,
+                mag_max=mag_max,
+                is_auto_ticks=is_auto_ticks,
+                fig=fig,
+            )
+    
+    else:
+        # Call the plot function
+        plot_A_time_fct(
+            data,
+            index_list=index_list,
+            alpha=alpha,
+            alpha_index=alpha_index,
+            is_fft=is_fft,
+            is_elecorder=is_elecorder,
+            freq_max=freq_max,
+            is_norm=is_norm,
+            unit=unit,
+            data_list=data_list,
+            legend_list=legend_list,
+            color_list=color_list,
+            save_path=save_path,
+            y_min=y_min,
+            y_max=y_max,
+            mag_max=mag_max,
+            is_auto_ticks=is_auto_ticks,
+        )
