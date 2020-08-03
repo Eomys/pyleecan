543ae14168d093746b24c1d2af4140620044812b
CedMrnl <cedric.marinel@eomys.com>
2020-08-03 17:03:25 +0200

    [WP] Add a test to execute the tutorials

---
+++ Tests/Tutorials/test_tutorial.py
@@ -0,0 +1,30 @@
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+import pytest
+from Tests import TEST_DIR
+from os.path import join, abspath, isfile
+
+TUTO_DIR = join(join(TEST_DIR, ".."), "Tutorials")
+
+
+@pytest.mark.tutorial
+@pytest.mark.parametrize(
+    "tuto_name",
+    [
+        "tuto_Machine",
+        "tuto_Simulation_FEMM",
+        "tuto_Force",
+        "tuto_Plots",
+        "tuto_Optimization",
+    ],
+)
+def test_tutorial(tuto_name):
+    """Execute the tutorial"""
+    # Read the notebook
+    with open(abspath(join(TUTO_DIR, tuto_name + ".ipynb"))) as f:
+        nb = nbformat.read(f, as_version=4)
+
+    # Execute it
+    ep = ExecutePreprocessor(timeout=-1, kernel_name="python3")
+    ep.preprocess(nb, {"metadata": {"path": TUTO_DIR}})
+
