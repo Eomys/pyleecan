5f90c5d2195942f5e033e17cfa57fa210ad3f9c6
RPile <raphael.pile@eomys.com>
2020-08-11 18:10:17 +0200

    [BC] Bad indent

--- pyleecan/Methods/Mesh/MeshSolution/plot_glyph.py
+++ pyleecan/Methods/Mesh/MeshSolution/plot_glyph.py
@@ -88,54 +88,47 @@ def plot_glyph(
         else:
             mesh_pv = mesh.get_mesh(indices=indices)
 
-    # Get the mesh
-    mesh = self.get_mesh(label=label, index=index)
-    if isinstance(mesh, MeshMat):
-        mesh_pv = mesh.get_mesh_pv(indices=indices)
-    else:
-        mesh_pv = mesh.get_mesh(indices=indices)
+        # Get the vector field
+        vect_field = real(self.get_field(label=label, index=index, indices=indices))
+        if len(vect_field.shape) == 3:
+            # Third dimension is frequencies
+            vect_field = vect_field[:, :, ifreq]
 
-    # Get the vector field
-    vect_field = real(self.get_field(label=label, index=index, indices=indices))
-    if len(vect_field.shape) == 3:
-        # Third dimension is frequencies
-        vect_field = vect_field[:, :, ifreq]
+        # Compute factor
+        if factor is None:
+            factor = 1 / (100 * np_max(vect_field))
 
-    # Compute factor
-    if factor is None:
-        factor = 1 / (100 * np_max(vect_field))
+        if self.dimension == 2:
+            vect_field = np.hstack((vect_field, np.zeros((vect_field.shape[0], 1))))
 
-    if self.dimension == 2:
-        vect_field = np.hstack((vect_field, np.zeros((vect_field.shape[0], 1))))
+        # Compute factor
+        if factor is None:
+            factor = 1 / (100 * np_max(vect_field))
 
-    # Compute factor
-    if factor is None:
-        factor = 1 / (100 * np_max(vect_field))
+        # Add field to mesh
+        if is_point_arrow:
+            mesh_pv.vectors = vect_field * factor
+            arrows_plt = mesh_pv.arrows
+        else:
+            mesh_pv["field"] = vect_field
+            mesh_cell = mesh_pv.point_data_to_cell_data()
+            surf = mesh_cell.extract_geometry()
+            centers2 = surf.cell_centers()
+            centers2.vectors = surf["field"] * factor
+            arrows_plt = centers2.arrows
 
-    # Add field to mesh
-    if is_point_arrow:
-        mesh_pv.vectors = vect_field * factor
-        arrows_plt = mesh_pv.arrows
-    else:
-        mesh_pv["field"] = vect_field
-        mesh_cell = mesh_pv.point_data_to_cell_data()
-        surf = mesh_cell.extract_geometry()
-        centers2 = surf.cell_centers()
-        centers2.vectors = surf["field"] * factor
-        arrows_plt = centers2.arrows
-
-    # Configure plot
-    if is_pyvistaqt:
-        p = pv.BackgroundPlotter()
-        p.set_background("white")
-    else:
-        pv.set_plot_theme("document")
-        p = pv.Plotter(notebook=False)
-    p.add_mesh(mesh_pv, color="grey", opacity=0.7, show_edges=True, edge_color="white")
-    p.add_mesh(arrows_plt, color="red")
-    if self.dimension:
-        p.view_xy()
-    if save_path is None:
-        p.show()
-    else:
-        p.show(interactive=False, screenshot=save_path)
+        # Configure plot
+        if is_pyvistaqt:
+            p = pv.BackgroundPlotter()
+            p.set_background("white")
+        else:
+            pv.set_plot_theme("document")
+            p = pv.Plotter(notebook=False)
+        p.add_mesh(mesh_pv, color="grey", opacity=0.7, show_edges=True, edge_color="white")
+        p.add_mesh(arrows_plt, color="red")
+        if self.dimension:
+            p.view_xy()
+        if save_path is None:
+            p.show()
+        else:
+            p.show(interactive=False, screenshot=save_path)
