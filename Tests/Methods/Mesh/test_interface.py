f27af51820382f902318c4e9f99a43a9085af1bd
RPile <raphael.pile@eomys.com>
2020-08-17 17:18:29 +0200

    [NF] Update interface and test

--- Tests/Methods/Mesh/test_interface.py
+++ Tests/Methods/Mesh/test_interface.py
@@ -7,28 +7,28 @@ from pyleecan.Classes.PointMat import PointMat
 from pyleecan.Classes.CellMat import CellMat
 import numpy as np
 
+
 @pytest.mark.MeshSol
-@pytest.mark.skip
 class unittest_interface(TestCase):
     """unittest for elements and nodes getter methods"""
 
     @classmethod
     def setUp(self):
         self.mesh = MeshMat()
-        self.mesh.cell["triangle3"] = CellMat(nb_pt_per_cell=3)
-        self.mesh.cell["segment2"] = CellMat(nb_pt_per_cell=2)
+        self.mesh.cell["triangle"] = CellMat(nb_pt_per_cell=3)
+        self.mesh.cell["segment"] = CellMat(nb_pt_per_cell=2)
         self.mesh.point = PointMat()
 
         self.other_mesh = MeshMat()
-        self.other_mesh.cell["triangle3"] = CellMat(nb_pt_per_cell=3)
-        self.other_mesh.cell["segment2"] = CellMat(nb_pt_per_cell=2)
+        self.other_mesh.cell["triangle"] = CellMat(nb_pt_per_cell=3)
+        self.other_mesh.cell["segment"] = CellMat(nb_pt_per_cell=2)
         self.other_mesh.point = self.mesh.point
 
     def test_MeshMat_flat(self):
         """unittest with ElementDict and PointMat objects"""
 
-        self.mesh.add_cell([0, 1, 2], "triangle3")
-        self.mesh.add_cell([2, 3, 4], "triangle3")
+        self.mesh.add_cell([0, 1, 2], "triangle")
+        self.mesh.add_cell([2, 3, 4], "triangle")
 
         self.mesh.point.add_point([0, 0])
         self.mesh.point.add_point([0.5, 1])
@@ -38,12 +38,12 @@ class unittest_interface(TestCase):
         self.mesh.point.add_point([0.5, -1])
         self.mesh.point.add_point([1.5, -1])
 
-        self.other_mesh.add_cell([0, 5, 2], "triangle3")
-        self.other_mesh.add_cell([4, 6, 2], "triangle3")
+        self.other_mesh.add_cell([0, 5, 2], "triangle")
+        self.other_mesh.add_cell([4, 6, 2], "triangle")
 
         new_seg_mesh = self.mesh.interface(self.other_mesh)
         solution = np.array([[0, 2], [2, 4]])
-        resultat = new_seg_mesh.cell["segment2"].connectivity
+        resultat = new_seg_mesh.cell["segment"].connectivity
         testA = np.sum(abs(resultat - solution))
         msg = (
             "Wrong projection: returned "
@@ -56,25 +56,25 @@ class unittest_interface(TestCase):
 
     def test_CellMat_PointMat_corner_ext(self):
         """unittest with CellMat and PointMat objects, extract interface from the external mesh point of view"""
-        self.mesh.add_cell([0, 1, 2], "triangle3")
-        self.mesh.add_cell([1, 2, 3], "triangle3")
-        self.mesh.add_cell([1, 5, 4], "triangle3")
+        self.mesh.add_cell([0, 1, 2], "triangle")
+        self.mesh.add_cell([1, 2, 3], "triangle")
+        self.mesh.add_cell([1, 5, 4], "triangle")
 
-        self.mesh.point.add_node([2, 0])
-        self.mesh.point.add_node([3, 0])
-        self.mesh.point.add_node([2.5, 1])
-        self.mesh.point.add_node([4, 0])
-        self.mesh.point.add_node([3.5, 1])
-        self.mesh.point.add_node([3, -1])
+        self.mesh.point.add_point([2, 0])
+        self.mesh.point.add_point([3, 0])
+        self.mesh.point.add_point([2.5, 1])
+        self.mesh.point.add_point([4, 0])
+        self.mesh.point.add_point([3.5, 1])
+        self.mesh.point.add_point([3, -1])
 
-        self.other_mesh.add_cell([0, 1, 5], "triangle3")
+        self.other_mesh.add_cell([0, 1, 5], "triangle")
 
         # Method test 1
         new_seg_mesh = self.mesh.interface(self.other_mesh)
 
         # Check result
         solution = np.array([[0, 1], [1, 5]])
-        result = new_seg_mesh.cell["Segment2"].connectivity
+        result = new_seg_mesh.cell["segment"].connectivity
         testA = np.sum(abs(result - solution))
         msg = "Wrong result: returned " + str(result) + ", expected: " + str(solution)
         DELTA = 1e-10
@@ -82,25 +82,25 @@ class unittest_interface(TestCase):
 
     def test_CellMat_PointMat_corner_int(self):
         """unittest with CellMat and PointMat objects, extract interface from the internal mesh point of view"""
-        self.mesh.add_cell([0, 1, 2], "triangle3")
-        self.mesh.add_cell([1, 2, 3], "triangle3")
-        self.mesh.add_cell([1, 5, 4], "triangle3")
+        self.mesh.add_cell([0, 1, 2], "triangle")
+        self.mesh.add_cell([1, 2, 3], "triangle")
+        self.mesh.add_cell([1, 5, 4], "triangle")
 
-        self.mesh.point.add_node([2, 0])
-        self.mesh.point.add_node([3, 0])
-        self.mesh.point.add_node([2.5, 1])
-        self.mesh.point.add_node([4, 0])
-        self.mesh.point.add_node([3.5, 1])
-        self.mesh.point.add_node([3, -1])
+        self.mesh.point.add_point([2, 0])
+        self.mesh.point.add_point([3, 0])
+        self.mesh.point.add_point([2.5, 1])
+        self.mesh.point.add_point([4, 0])
+        self.mesh.point.add_point([3.5, 1])
+        self.mesh.point.add_point([3, -1])
 
-        self.other_mesh.add_cell([0, 1, 5], "triangle3")
+        self.other_mesh.add_cell([0, 1, 5], "triangle")
 
         # Method test 1
         new_seg_mesh = self.other_mesh.interface(self.mesh)
 
         # Check result
         solution = np.array([[0, 1], [1, 5]])
-        result = new_seg_mesh.cell["segment2"].connectivity
+        result = new_seg_mesh.cell["segment"].connectivity
         testA = np.sum(abs(result - solution))
         msg = "Wrong result: returned " + str(result) + ", expected: " + str(solution)
         DELTA = 1e-10
@@ -108,10 +108,10 @@ class unittest_interface(TestCase):
 
     def test_CellMat_PointMat_self(self):
         """unittest with CellMat and PointMat objects, extract interface on itself"""
-        self.mesh.add_cell([0, 1, 2], "triangle3")
-        self.mesh.add_cell([0, 2, 3], "triangle3")
-        self.mesh.add_cell([0, 3, 4], "triangle3")
-        self.mesh.add_cell([0, 4, 1], "triangle3")
+        self.mesh.add_cell([0, 1, 2], "triangle")
+        self.mesh.add_cell([0, 2, 3], "triangle")
+        self.mesh.add_cell([0, 3, 4], "triangle")
+        self.mesh.add_cell([0, 4, 1], "triangle")
 
         self.mesh.point.add_point([0, 0])
         self.mesh.point.add_point([0, 1])
@@ -124,7 +124,7 @@ class unittest_interface(TestCase):
 
         # Check result
         solution = np.array([])
-        result = new_seg_mesh.cell["segment2"].connectivity
+        result = new_seg_mesh.cell["segment"].connectivity
         testA = np.sum(abs(result - solution))
         msg = "Wrong result: returned " + str(result) + ", expected: " + str(solution)
         DELTA = 1e-10
--- pyleecan/Methods/Mesh/MeshMat/interface.py
+++ pyleecan/Methods/Mesh/MeshMat/interface.py
@@ -7,12 +7,11 @@ import numpy as np
 
 
 def interface(self, other_mesh):
-    """Define an Mesh object corresponding to the exact intersection between two Mesh (nodes must be in both meshes,
-    the nodes tags must be identically defined).
+    """Define a MeshMat object corresponding to the exact intersection between two meshes (points must be in both meshes).
 
     Parameters
     ----------
-    self : Mesh
+    self : MeshMat
         a Mesh object
     other_mesh : Mesh
         an other Mesh object
@@ -22,120 +21,127 @@ def interface(self, other_mesh):
     """
 
     # Dynamic import
-    module = __import__(PACKAGE_NAME + ".Classes." + "Mesh", fromlist=["Mesh"])
-    new_mesh = getattr(module, "Mesh")()
-    new_mesh.node = PointMat()
-    new_mesh.element["Segment2"] = CellMat(nb_node_per_element=2)
+    new_mesh = self.copy()
+    new_mesh.point = PointMat()
+    new_mesh.cell = dict()
+    new_mesh.cell["segment"] = CellMat(nb_pt_per_cell=2)
 
-    for key in self.element:
-        nodes_tags = self.element[key].get_all_node_tags()
-        other_nodes_tags = other_mesh.element[key].get_all_node_tags()
+    for key in self.cell:
 
-        # Find the nodes on the interface (they are in both in and out)
-        interface_nodes_tags = np.intersect1d(nodes_tags, other_nodes_tags)
-        nb_interf_nodes = len(interface_nodes_tags)
+        # Developer info: IDK if this code works with other than triangle cells. To be checked.
+        if self.cell[key].nb_pt_per_cell == 3:  # Triangle case
+            points_tags = np.unique(self.cell[key].get_connectivity())
+            other_points_tags = np.unique(other_mesh.cell[key].get_connectivity())
 
-        tmp_element_tags = np.array([], dtype=int)
-        tmp_element_tags_other = np.array([], dtype=int)
-        node2elem_dict = dict()
-        node2elem_other_dict = dict()
-        # Find the elements in contact with the interface (they contain the interface nodes)
-        for ind in range(nb_interf_nodes):
-            tmp_tag = self.element[key].get_node2element(interface_nodes_tags[ind])
-            node2elem_dict[interface_nodes_tags[ind]] = tmp_tag
-            tmp_element_tags = np.concatenate((tmp_element_tags, tmp_tag))
+            # Find the points on the interface (they are in both in and out)
+            interface_points_tags = np.intersect1d(points_tags, other_points_tags)
+            nb_interf_points = len(interface_points_tags)
 
-            tmp_tag = other_mesh.element[key].get_node2element(
-                interface_nodes_tags[ind]
-            )
-            node2elem_other_dict[interface_nodes_tags[ind]] = tmp_tag
-            tmp_element_tags_other = np.concatenate((tmp_element_tags_other, tmp_tag))
+            tmp_cell_tags = np.array([], dtype=int)
+            tmp_cell_tags_other = np.array([], dtype=int)
+            point2elem_dict = dict()
+            point2elem_other_dict = dict()
+            # Find the cells in contact with the interface (they contain the interface points)
+            for ind in range(nb_interf_points):
+                tmp_tag = self.cell[key].get_point2cell(interface_points_tags[ind])
+                point2elem_dict[interface_points_tags[ind]] = tmp_tag
+                tmp_cell_tags = np.concatenate((tmp_cell_tags, tmp_tag))
 
-        # Find element tags in contact and number of nodes in contact for each element
-        tmp_element_tags_unique = np.unique(
-            tmp_element_tags
-        )  # List of element tag which are in contact with the interface
-        nb_elem_contact = len(tmp_element_tags_unique)
-        nb_element_tags_unique = np.zeros((nb_elem_contact, 1), dtype=int)
-        elem2node_dict = dict()
-        for ind in range(nb_elem_contact):
-            # Number of node on the interface for each element from tmp_element_tags_unique
-            Ipos = np.where(tmp_element_tags_unique[ind] == tmp_element_tags)[0]
-            nb_element_tags_unique[ind] = len(Ipos)
-            # Which nodes exactly are concerned; store them in elem2node_dict
-            nodes_tmp = self.get_node_tags(tmp_element_tags_unique[ind])
-            nodes_tmp_interf = np.array([], dtype=int)
-            for ipos in range(len(nodes_tmp)):
-                if nodes_tmp[ipos] in interface_nodes_tags:
-                    nodes_tmp_interf = np.concatenate(
-                        (nodes_tmp_interf, np.array([nodes_tmp[ipos]], dtype=int))
-                    )
-            elem2node_dict[tmp_element_tags_unique[ind]] = nodes_tmp_interf
+                tmp_tag = other_mesh.cell[key].get_point2cell(
+                    interface_points_tags[ind]
+                )
+                point2elem_other_dict[interface_points_tags[ind]] = tmp_tag
+                tmp_cell_tags_other = np.concatenate((tmp_cell_tags_other, tmp_tag))
 
-        # Build element
-        seg_elem_pos = np.where(nb_element_tags_unique == 2)[
-            0
-        ]  # Position in the vector tmp_element_tags_unique
-        seg_elem_tag = tmp_element_tags_unique[
-            seg_elem_pos
-        ]  # Vector of element tags with only 2 nodes on the interface
-        nb_elem_segm = len(seg_elem_tag)
-        for i_seg in range(nb_elem_segm):
-            tag_two_nodes = elem2node_dict[seg_elem_tag[i_seg]]
-            new_tag = new_mesh.get_new_tag()
-            new_mesh.element["Segment2"].add_element(tag_two_nodes, new_tag)
+            # Find cell tags in contact and number of points in contact for each cell
+            tmp_cell_tags_unique = np.unique(
+                tmp_cell_tags
+            )  # List of cell tag which are in contact with the interface
+            nb_elem_contact = len(tmp_cell_tags_unique)
+            nb_cell_tags_unique = np.zeros((nb_elem_contact, 1), dtype=int)
+            elem2point_dict = dict()
+            for ind in range(nb_elem_contact):
+                # Number of point on the interface for each cell from tmp_cell_tags_unique
+                Ipos = np.where(tmp_cell_tags_unique[ind] == tmp_cell_tags)[0]
+                nb_cell_tags_unique[ind] = len(Ipos)
+                # Which points exactly are concerned; store them in elem2point_dict
+                cells_tmp = self.get_cell(tmp_cell_tags_unique[ind])[0]
+                # for key in cells_tmp:
+                points_tmp = np.unique(cells_tmp[key])
+                points_tmp_interf = np.array([], dtype=int)
+                for ipos in range(len(points_tmp)):
+                    if points_tmp[ipos] in interface_points_tags:
+                        points_tmp_interf = np.concatenate(
+                            (points_tmp_interf, np.array([points_tmp[ipos]], dtype=int))
+                        )
+                elem2point_dict[tmp_cell_tags_unique[ind]] = points_tmp_interf
 
-        # The same operation is applied in the other mesh because in the corners, 1 element will contain 3 nodes,
-        # and it will not be detected by seg_elem_pos. Applying the same process to the other mesh solve the issue
-        # if add_element ignore the already defined elements.
-        tmp_element_tags_other_unique = np.unique(tmp_element_tags_other)
-        nb_node_other_contact = len(tmp_element_tags_other_unique)
-        nb_element_tags_other_unique = np.zeros((nb_node_other_contact, 1), dtype=int)
-        elem2node_other_dict = dict()
-        for ind in range(nb_node_other_contact):
-            Ipos = np.where(
-                tmp_element_tags_other_unique[ind] == tmp_element_tags_other
-            )[0]
-            nb_element_tags_other_unique[ind] = len(Ipos)
-            nodes_tmp = other_mesh.get_node_tags(tmp_element_tags_other_unique[ind])
-            nodes_tmp_interf = np.array([], dtype=int)
-            for ipos in range(len(nodes_tmp)):
-                if nodes_tmp[ipos] in interface_nodes_tags:
-                    nodes_tmp_interf = np.concatenate(
-                        (nodes_tmp_interf, np.array([nodes_tmp[ipos]], dtype=int))
-                    )
-            elem2node_other_dict[tmp_element_tags_other_unique[ind]] = nodes_tmp_interf
+            # Build cell
+            seg_elem_pos = np.where(nb_cell_tags_unique == 2)[
+                0
+            ]  # Position in the vector tmp_cell_tags_unique
+            seg_elem_tag = tmp_cell_tags_unique[
+                seg_elem_pos
+            ]  # Vector of cell tags with only 2 points on the interface
+            nb_elem_segm = len(seg_elem_tag)
+            for i_seg in range(nb_elem_segm):
+                tag_two_points = elem2point_dict[seg_elem_tag[i_seg]]
+                new_mesh.add_cell(tag_two_points, "segment")
 
-        # Build segment elements in other mesh
-        seg_elem_pos = np.where(nb_element_tags_other_unique == 2)[0]
-        seg_elem_tag = tmp_element_tags_other_unique[seg_elem_pos]
-        nb_elem_segm = len(seg_elem_tag)
-        for i_seg in range(nb_elem_segm):
-            tag_two_nodes = elem2node_other_dict[seg_elem_tag[i_seg]]
-            # It is not really added if it already exist
-            new_tag = new_mesh.get_new_tag()
-            new_mesh.element["Segment2"].add_element(tag_two_nodes, new_tag)
+            # The same operation is applied in the other mesh because in the corners, 1 cell will contain 3 points,
+            # and it will not be detected by seg_elem_pos. Applying the same process to the other mesh solve the issue
+            # if add_cell ignore the already defined cells.
+            tmp_cell_tags_other_unique = np.unique(tmp_cell_tags_other)
+            nb_point_other_contact = len(tmp_cell_tags_other_unique)
+            nb_cell_tags_other_unique = np.zeros((nb_point_other_contact, 1), dtype=int)
+            elem2point_other_dict = dict()
+            for ind in range(nb_point_other_contact):
+                Ipos = np.where(tmp_cell_tags_other_unique[ind] == tmp_cell_tags_other)[
+                    0
+                ]
+                nb_cell_tags_other_unique[ind] = len(Ipos)
+                cells_tmp = other_mesh.get_cell(tmp_cell_tags_other_unique[ind])[0]
+                points_tmp = np.unique(cells_tmp[key])
+                points_tmp_interf = np.array([], dtype=int)
+                for ipos in range(len(points_tmp)):
+                    if points_tmp[ipos] in interface_points_tags:
+                        points_tmp_interf = np.concatenate(
+                            (points_tmp_interf, np.array([points_tmp[ipos]], dtype=int))
+                        )
+                elem2point_other_dict[
+                    tmp_cell_tags_other_unique[ind]
+                ] = points_tmp_interf
+
+            # Build segment cells in other mesh
+            seg_elem_pos = np.where(nb_cell_tags_other_unique == 2)[0]
+            seg_elem_tag = tmp_cell_tags_other_unique[seg_elem_pos]
+            nb_elem_segm = len(seg_elem_tag)
+            for i_seg in range(nb_elem_segm):
+                tag_two_points = elem2point_other_dict[seg_elem_tag[i_seg]]
+                # It is not really added if it already exist
+                # new_tag = new_mesh.get_new_tag()
+                new_mesh.add_cell(tag_two_points, "segment")
 
     return new_mesh
-    # TODO : Extend the code to higher dimension (3 nodes triangles for tetrahedra interfaces ...)
+    # TODO : Extend the code to higher dimension (3 points triangles for tetrahedra interfaces ...)
 
     # import matplotlib.pyplot as plt
     # fig, ax = plt.subplots()
-    # x = mesh_group.node[:,0]
-    # y = mesh_group.node[:,1]
+    # x = mesh_group.point[:,0]
+    # y = mesh_group.point[:,1]
     # ax.scatter(x, y)
     # for ieleme in range(mesh_group.nb_elem):
-    #     x = mesh_group.node[interface_elem[ieleme,:],0]
-    #     y = mesh_group.node[interface_elem[ieleme,:],1]
+    #     x = mesh_group.point[interface_elem[ieleme,:],0]
+    #     y = mesh_group.point[interface_elem[ieleme,:],1]
     #     ax.plot(x, y)
     #
     # fig, ax = plt.subplots()
-    # x = nodes_in[:, 0]
-    # y = nodes_in[:, 1]
+    # x = points_in[:, 0]
+    # y = points_in[:, 1]
     # ax.scatter(x, y)
-    # x = nodes_out[:, 0]
-    # y = nodes_out[:, 1]
+    # x = points_out[:, 0]
+    # y = points_out[:, 1]
     # ax.scatter(x, y)
-    # x = nodes_parent[interface_nodes_id, 0]
-    # y = nodes_parent[interface_nodes_id, 1]
+    # x = points_parent[interface_points_id, 0]
+    # y = points_parent[interface_points_id, 1]
     # ax.scatter(x, y, marker='x')
