e80269f1157ae47c788ebdfc13696e6aaaac126a
Helene Toubin <helene.toubin@eomys.com>
2020-08-18 10:05:23 +0200

    [BC] remove transpose in Is matrix

--- pyleecan/Methods/Simulation/InputCurrent/gen_input.py
+++ pyleecan/Methods/Simulation/InputCurrent/gen_input.py
@@ -86,11 +86,11 @@ def gen_input(self):
                 output.Is = None
         else:
             Is = self.Is.get_data()
-            if not isinstance(Is, ndarray) or Is.shape != (qs, self.Nt_tot):
+            if not isinstance(Is, ndarray) or Is.shape != (self.Nt_tot, qs):
                 raise InputError(
                     "ERROR: InputCurrent.Is must be a matrix with the shape "
-                    + str((qs, self.Nt_tot))
-                    + " (stator phase number, len(time)), "
+                    + str((self.Nt_tot, qs))
+                    + " (len(time), stator phase number), "
                     + str(Is.shape)
                     + " returned"
                 )
@@ -107,7 +107,7 @@ def gen_input(self):
                 unit="A",
                 symbol="Is",
                 axes=[Phase, Time],
-                values=Is,
+                values=transpose(Is),
             )
             # Compute corresponding Id/Iq reference
             Idq = n2dq(transpose(output.Is.values), 2 * pi * output.felec * output.time)
@@ -120,11 +120,11 @@ def gen_input(self):
             raise InputError("ERROR: InputCurrent.Ir missing")
         else:
             Ir = self.Ir.get_data()
-            if not isinstance(Ir, ndarray) or Ir.shape != (qr, self.Nt_tot):
+            if not isinstance(Ir, ndarray) or Ir.shape != (self.Nt_tot, qr):
                 raise InputError(
                     "ERROR: InputCurrent.Ir must be a matrix with the shape "
-                    + str((qr, self.Nt_tot))
-                    + " (rotor phase number, len(time)), "
+                    + str((self.Nt_tot, qr))
+                    + " (len(time), rotor phase number), "
                     + str(Ir.shape)
                     + " returned"
                 )
@@ -141,7 +141,7 @@ def gen_input(self):
                 unit="A",
                 symbol="Ir",
                 axes=[Phase, Time],
-                values=Ir,
+                values=transpose(Ir),
             )
 
     # Load and check alpha_rotor and N0
