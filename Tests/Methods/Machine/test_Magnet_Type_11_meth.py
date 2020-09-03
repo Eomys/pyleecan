62ef6655b317079d0bf143f70d0cdb8e1d8880cd
BenjaminGabet <benjamin.gabetpro@gmail.com>
2020-09-03 16:55:30 +0200

    Refactoring test_Magnet_Type_11_meth.py

--- Tests/Methods/Machine/test_Magnet_Type_11_meth.py
+++ Tests/Methods/Machine/test_Magnet_Type_11_meth.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from unittest import TestCase
-
-from ddt import ddt, data
+import pytest
 
 from pyleecan.Classes.Arc1 import Arc1
 from pyleecan.Classes.Segment import Segment
@@ -37,62 +35,62 @@ Mag11_test.append({"test_obj": lam, "S_exp": 2.09439e-6, "Ao": pi / 12, "H_exp":
 DELTA = 1e-4
 
 
-@ddt
-class test_Magnet_Type_11_meth(TestCase):
+@pytest.mark.METHODS
+class Test_Magnet_Type_11_meth(object):
     """unittest for MagnetType11 methods
-    """
-
-    @data(*Mag11_test)
-    def test_comp_surface(self, test_dict):
-        """Check that the computation of the surface is correct
         """
+
+    @pytest.mark.parametrize("test_dict", Mag11_test)
+    def test_comp_surface(self,test_dict):
+        """Check that the computation of the surface is correct
+            """
         test_obj = test_dict["test_obj"]
         result = test_obj.slot.magnet[0].comp_surface()
 
         a = result
         b = test_dict["S_exp"]
         msg = "Return " + str(a) + " expected " + str(b)
-        self.assertAlmostEqual((a - b) / a, 0, delta=DELTA, msg=msg)
+        assert abs((a - b) / a-0) < DELTA, msg
 
         # Compare numerical and analytical results
         b = comp_surface(test_obj.slot.magnet[0])
         msg = "Analytical: " + str(a) + " Numerical " + str(b)
-        self.assertAlmostEqual((a - b) / a, 0, delta=DELTA, msg=msg)
+        assert abs((a - b) / a-0) < DELTA, msg
 
-    @data(*Mag11_test)
-    def test_comp_height(self, test_dict):
+    @pytest.mark.parametrize("test_dict", Mag11_test)
+    def test_comp_height(self,test_dict):
         """Check that the computation of the height is correct
-        """
+            """
         test_obj = test_dict["test_obj"]
         result = test_obj.slot.magnet[0].comp_height()
 
         a = result
         b = test_dict["H_exp"]
         msg = "Return " + str(a) + " expected " + str(b)
-        self.assertAlmostEqual((a - b) / a, 0, delta=DELTA, msg=msg)
+        assert abs((a - b) / a-0) < DELTA, msg
 
-    @data(*Mag11_test)
-    def test_comp_angle_op(self, test_dict):
+    @pytest.mark.parametrize("test_dict", Mag11_test)
+    def test_comp_angle_op(self,test_dict):
         """Check that the computation of the opening angle is correct
-        """
+            """
         test_obj = test_dict["test_obj"]
         result = test_obj.slot.magnet[0].comp_angle_opening()
 
         a = result
         b = test_dict["Ao"]
         msg = "Return " + str(a) + " expected " + str(b)
-        self.assertAlmostEqual((a - b) / a, 0, delta=DELTA, msg=msg)
+        assert abs((a - b) / a-0) < DELTA, msg
 
     def test_build_geometry_out(self):
         """check that curve_list is correct (outwards magnet)"""
-        lam = LamSlotMag(
-            Rint=40e-3,
-            Rext=90e-3,
-            is_internal=False,
-            is_stator=False,
-            L1=0.45,
-            Nrvd=1,
-            Wrvd=0.05,
+        lam = LamSlotMag(    
+            Rint=40e-3,    
+            Rext=90e-3,    
+            is_internal=False,    
+            is_stator=False,    
+            L1=0.45,    
+            Nrvd=1,    
+            Wrvd=0.05,    
         )
         magnet = [MagnetType11(Wmag=pi / 10, Hmag=0.2)]
         lam.slot = SlotMPolar(Zs=8, W0=pi / 10, H0=0.2, magnet=magnet)
@@ -117,22 +115,22 @@ class test_Magnet_Type_11_meth(TestCase):
         for i in range(0, len(result)):
             a = result[i].begin
             b = curve_list[i].begin
-            self.assertAlmostEqual((a - b) / a, 0, delta=DELTA)
+            assert abs((a - b) / a-0) < DELTA
 
             a = result[i].end
             b = curve_list[i].end
-            self.assertAlmostEqual((a - b) / a, 0, delta=DELTA)
+            assert abs((a - b) / a-0) < DELTA
 
     def test_build_geometry_in(self):
         """check that curve_list is correct (inwards magnet)"""
-        lam = LamSlotMag(
-            Rint=40e-1,
-            Rext=90e-1,
-            is_internal=True,
-            is_stator=False,
-            L1=0.45,
-            Nrvd=1,
-            Wrvd=0.05,
+        lam = LamSlotMag(    
+            Rint=40e-1,    
+            Rext=90e-1,    
+            is_internal=True,   
+            is_stator=False,    
+            L1=0.45,    
+            Nrvd=1,    
+            Wrvd=0.05,    
         )
         magnet = [MagnetType11(Wmag=pi / 10, Hmag=0.2)]
         lam.slot = SlotMPolar(Zs=8, W0=pi / 10, H0=0.2, magnet=magnet)
@@ -157,8 +155,8 @@ class test_Magnet_Type_11_meth(TestCase):
         for i in range(0, len(result)):
             a = result[i].begin
             b = curve_list[i].begin
-            self.assertAlmostEqual((a - b) / a, 0, delta=DELTA)
+            assert abs((a - b) / a-0) < DELTA
 
             a = result[i].end
             b = curve_list[i].end
-            self.assertAlmostEqual((a - b) / a, 0, delta=DELTA)
+            assert abs((a - b) / a-0) < DELTA
