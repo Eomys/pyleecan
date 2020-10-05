59508f61a4a0b3066491c438b5241ea9d26eb543
SebGue <sebgue@gmx.net>
2020-10-05 08:19:30 +0200

    [WiP] solve_EEC

--- pyleecan/Methods/Simulation/EEC_SCIM/solve_EEC.py
+++ pyleecan/Methods/Simulation/EEC_SCIM/solve_EEC.py
@@ -17,8 +17,8 @@ def solve_EEC(self, output):
     |                     |   |                       |
      ---------Is---------- --- ---------Ir------------
 
-             --->                               
-              Us                                
+             --->
+              Us
 
     Parameters
     ----------
@@ -27,9 +27,23 @@ def solve_EEC(self, output):
     output : Output
         an Output object
     """
+    Rs = self.parameters["Rs"]
+    Rr = self.parameters["Rr"]
+    Rfe = self.parameters["Rfe"]
+    Ls = self.parameters["Ls"]
+    Lr = self.parameters["Lr"]
+    Lm = self.parameters["Lm"]
+
+    s = self.parameter["s"]
 
     felec = output.elec.felec
     ws = 2 * pi * felec
+
+    Xs = ws * Ls
+    Xm = ws * Lm
+    Xr = ws * Lr
+
+    Rr_s = Rr / s if s != 0 else 1e16  # TODO modify system instead
 
     # Prepare linear system
 
@@ -37,12 +51,11 @@ def solve_EEC(self, output):
     if "Us" in self.parameters:
         Us = self.parameters["Us"]
         # input vector
-        b = array([
-            real(Us), imag(Us),  0, 0, 0, 0, 0, 0, 0, 0
-        ])
+        b = array([real(Us), imag(Us), 0, 0, 0, 0, 0, 0, 0, 0])
         # system matrix (unknowns order: Um, Is, Im, Ir', Ife each real and imagine parts)
         # TODO simplify system for less unknows (only calculate them afterwards, e.g. Um, Im, Ife)
-        A = array([
+        # fmt: off
+        A = array([ 
             # sum of (real and imagine) voltages equals the input voltage Us
             [1, 0, Rs, -Xs, 0, 0, 0, 0, 0, 0, 0, 0], 
             [0, 1, Xs, +Rs, 0, 0, 0, 0, 0, 0, 0, 0], 
@@ -53,23 +66,25 @@ def solve_EEC(self, output):
             [-1, 0, 0, 0, 0, -Xm, 0, 0, 0, 0,], 
             [0, -1, 0, 0, +Xm, 0, 0, 0, 0, 0],
             # (Rr'/s + j*Xr')*Ir' = Um
-            [-1, 0, 0, 0, 0, 0, Rr_, -Xr_, 0, 0], 
-            [0, -1, 0, 0, 0, 0, +Xm, +Rr_, 0, 0],
+            [-1, 0, 0, 0, 0, 0, Rr_s, -Xr, 0, 0], 
+            [0, -1, 0, 0, 0, 0, +Xm, Rr_s, 0, 0],
             # Rfe*Ife = Um
             [-1, 0, 0, 0, 0, 0, 0, 0, Rfe, 0], 
-            [0, +1, 0, 0, 0, 0, 0, 0, 0, Ref],
-        ])
+            [0, +1, 0, 0, 0, 0, 0, 0, 0, Rfe],
+        ]) 
+        # fmt: on
         # TODO delete last row and column if Rfe is None
 
         X = solve(A, b)
-        
-        Um = X[0] + j*X[1]
-        Is = X[2] + j*X[3]
-        Ir_ = X[6] + j*X[7]
-        Ife = X[8] + j*x[9]
 
-        #output.elec.Id_ref = XI[0]
-        #output.elec.Iq_ref = XI[1]
+        Um = X[0] + j * X[1]
+        Is = X[2] + j * X[3]
+        Ir_ = X[6] + j * X[7]
+        Ife = X[8] + j * x[9]
+        # TODO use logger for output of some quantities
+
+        output.elec.Id_ref = real(Is)  # use Id_ref / Iq_ref for now
+        output.elec.Iq_ref = imag(Is)
     else:
         pass
         # TODO
