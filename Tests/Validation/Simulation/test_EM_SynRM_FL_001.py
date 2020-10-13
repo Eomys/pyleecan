fd5798c193388c9ad4fe118be8957e065f1fb1a3
Pierre Bonneel <pierre.bonneel@gmail.com>
2020-10-13 22:57:34 +0200

    [CO] Reorganizing the test according to latest object

--- Tests/Validation/Simulation/test_EM_SynRM_FL_001.py
+++ Tests/Validation/Simulation/test_EM_SynRM_FL_001.py
@@ -1,23 +1,17 @@
-from numpy import ones, pi, array, zeros, linspace, cos
 from os.path import join
 import matplotlib.pyplot as plt
-from Tests import save_validation_path as save_path
-
-from pyleecan.Classes.Simu1 import Simu1
-
-from pyleecan.Classes.InputCurrent import InputCurrent
-from pyleecan.Classes.InputFlux import InputFlux
-from pyleecan.Classes.ImportGenVectLin import ImportGenVectLin
-from pyleecan.Classes.ImportMatrixVal import ImportMatrixVal
-from pyleecan.Classes.ImportMatlab import ImportMatlab
-
-from pyleecan.Classes.MagFEMM import MagFEMM
-from pyleecan.Classes.Output import Output
-from Tests import TEST_DATA_DIR
 import pytest
-
-from pyleecan.Functions.load import load
+from numpy import array, cos, linspace, ones, pi, zeros, sqrt
+from pyleecan.Classes.InputCurrent import InputCurrent
+from pyleecan.Classes.MagFEMM import MagFEMM
+from pyleecan.Classes.Simu1 import Simu1
+from pyleecan.Classes.VarLoadCurrent import VarLoadCurrent
 from pyleecan.definitions import DATA_DIR
+from pyleecan.Functions.load import load
+from pyleecan.Functions.Plot.plot_A_2D import plot_A_2D
+from Tests import TEST_DATA_DIR
+from Tests import save_validation_path as save_path
+from pyleecan.definitions import config_dict
 
 
 @pytest.mark.long
@@ -32,14 +26,13 @@ def test_Magnetic_Phi0():
     # The aim of this validation test is to compute the torque as a function of Phi0
     # As (for now) there is no electrical model, we will compute the current for each Phi0 here
     SynRM_001 = load(join(DATA_DIR, "Machine", "SynRM_001.json"))
-    freq0 = 50  # supply frequency [Hz]
-    qs = 3  # Number of phases
-    p = 2  # Number of pole pairs
+    felec = 50  # supply frequency [Hz]
     Nt_tot = 3  # Number of time step for each current angle Phi0
-    Imax = 28.6878  # Nominal stator current magnitude [A]
-    # to have one torque ripple period since torque ripple appears at multiple of 6*freq0
+    Imax = 28.6878 / sqrt(2)  # RMS stator current magnitude [A]
+    # to have one torque ripple period since torque ripple appears at multiple of 6*felec
     Nrev = 1 / 6
-    time = linspace(0, Nrev * p / freq0 * (1 - 1 / Nt_tot), Nt_tot)
+    p = SynRM_001.stator.get_pole_pair_number()
+    N0 = 60 * felec / p
 
     Phi0 = (
         array(
@@ -65,38 +58,6 @@ def test_Magnetic_Phi0():
         * pi
         / 180
     )
-    Is_list = list()
-    for Phi in Phi0:
-        Is = zeros((Nt_tot, qs))
-        for q in range(qs):
-            Is[:, q] = Imax * cos(2 * pi * freq0 * time - q * 2 * pi / qs + Phi)
-        Is_list.append(Is)
-
-    # Definition of the main simulation
-    simu = Simu1(name="EM_SynRM_FL_001", machine=SynRM_001)
-    time_obj = ImportMatrixVal(value=time)
-    Na_tot = 2016
-    alpha_rotor = ImportMatrixVal(value=array([0, pi / 9, 2 * pi / 9]) + 2.6180)
-
-    simu.input = InputCurrent(
-        Is=None,
-        Ir=None,  # No winding on the rotor
-        N0=None,
-        angle_rotor=alpha_rotor,
-        time=time_obj,
-        Na_tot=Na_tot,
-        angle_rotor_initial=0,
-    )
-
-    # Definition of the magnetic simulation (1/2 symmetry)
-    assert SynRM_001.comp_periodicity() == (2, True, 2, True)
-    simu.mag = MagFEMM(
-        type_BH_stator=0,
-        type_BH_rotor=0,
-        is_periodicity_a=True,
-    )
-    simu.force = None
-    simu.struct = None
     # Expected results
     Tem = [
         0.08,
@@ -116,19 +77,59 @@ def test_Magnetic_Phi0():
         -7.5445,
         0.08,
     ]
-    Tem_FEMM = zeros(Phi0.shape)
-    for ii, I in enumerate(Is_list):
-        simu.input.Is = ImportMatrixVal(value=I)
-        out = Output(simu=simu)
-        simu.run()
-        print("test_EM_SynRM_FL_001: " + str(ii + 1) + "/16")
-        Tem_FEMM[ii] = out.mag.Tem_av
 
-    fig, ax = plt.subplots()
-    ax.set_title("Torque as a function of Phi0")
-    ax.set_xlabel("Phi0 [rad]")
-    ax.set_ylabel("Torque [N.m]")
-    ax.plot(Phi0, Tem_FEMM, "r", label="FEMM")
-    ax.plot(Phi0, Tem, "b--", label="Syr-e")
+    # Definition of the main simulation
+    simu = Simu1(name="EM_SynRM_FL_001", machine=SynRM_001)
+    Na_tot = 2016
 
-    fig.savefig(join(save_path, "test_EM_SynRM_FL_001.png"))
+    simu.input = InputCurrent(
+        Is=None,
+        Ir=None,  # No winding on the rotor
+        N0=N0,
+        Nt_tot=Nt_tot,
+        Nrev=1 / 6,
+        Na_tot=Na_tot,
+        felec=felec,
+    )
+
+    varload = VarLoadCurrent(is_reuse_femm_file=True, ref_simu_index=0, is_torque=True)
+    varload.type_OP_matrix = 0  # Matrix N0, I0, Phi0, Tem_ref
+
+    N_simu = Phi0.size
+    OP_matrix = zeros((N_simu, 4))
+    OP_matrix[:, 0] = ones(N_simu) * N0
+    OP_matrix[:, 1] = ones(N_simu) * Imax
+    OP_matrix[:, 2] = Phi0
+    OP_matrix[:, 3] = Tem
+    varload.OP_matrix = OP_matrix
+    simu.var_simu = varload
+
+    # Definition of the magnetic simulation (1/2 symmetry)
+    assert SynRM_001.comp_periodicity() == (2, True, 2, True)
+    simu.mag = MagFEMM(
+        type_BH_stator=0,
+        type_BH_rotor=0,
+        is_periodicity_a=True,
+        is_periodicity_t=False,
+    )
+    simu.force = None
+    simu.struct = None
+
+    Xout = simu.run()
+
+    curve_colors = config_dict["PLOT"]["COLOR_DICT"]["CURVE_COLORS"]
+    plot_A_2D(
+        array([x * 180 / pi for x in Xout.xoutput_dict["Phi0"].result]),
+        [Xout.xoutput_dict["Tem_av"].result, Xout.xoutput_dict["Tem_av_ref"].result],
+        color_list=curve_colors,
+        legend_list=["Pyleecan", "Syr-e r29"],
+        xlabel="Current angle [°]",
+        ylabel="Electrical torque [N.m]",
+        title="Electrical torque vs current angle",
+    )
+    fig = plt.gcf()
+    fig.savefig(join(save_path, "test_SynRM_Syr-e.png"))
+
+
+if __name__ == "__main__":
+    test_Magnetic_Phi0()
