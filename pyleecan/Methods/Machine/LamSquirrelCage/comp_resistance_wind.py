3a0839febf3190b1080acb56c06f49c736a6c9e9
MVreemann <71326146+MVreemann@users.noreply.github.com>
2020-10-14 14:02:54 +0200

    rotor resistance calculations
    [NF] New LamSquirrelCage functions for rotor resistance calculation

---
+++ pyleecan/Methods/Machine/LamSquirrelCage/comp_resistance_wind.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from numpy import pi
+from math import sin
+
+
+def comp_resistance_wind(self):
+    """Computation of the rotor resistance
+
+    Parameters
+    ----------
+    self : LamSquirrelCage
+        A LamSquirrelCage object
+
+    Returns
+    -------
+    Rrot: float
+         resistance of the rotor[Ohm]
+
+    """
+    # calculate resistance ring
+    Sring = self.comp_surface_ring()
+    lring = self.comp_length_ring()
+    rho = self.ring_mat.struc.rho
+    Zs = self.get_Zs(self)
+    P = self.get_pole_pair_number(self)
+
+    Rring = rho * lring / (2 * Sring * sin(pi * P / Zs) ** 2)
+
+    # calculate resistance rod
+    Srod = self.slot.comp_surface_wind()
+    lrod = self.comp_length()
+    Rrod = rho * lrod / Srod
+
+    Rtot = Rring + Rrod
+
+    return Rtot
