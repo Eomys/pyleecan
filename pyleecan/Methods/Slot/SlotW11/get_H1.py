3a1e9b74c9bb4bbb63072bc15aeb2e6f13623400
Pierre Bonneel <pierre.bonneel@gmail.com>
2020-10-19 18:08:00 +0200

    [BC] Missing return

--- pyleecan/Methods/Slot/SlotW11/get_H1.py
+++ pyleecan/Methods/Slot/SlotW11/get_H1.py
@@ -19,6 +19,6 @@ def get_H1(self):
     """
 
     if self.H1_is_rad:  # H1 in rad
-        H1 = (self.W1 - self.W0) / 2.0 * tan(self.H1)  # convertion to m
+        return (self.W1 - self.W0) / 2.0 * tan(self.H1)  # convertion to m
     else:  # H1 in m
-        H1 = self.H1
+        return self.H1
