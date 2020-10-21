a58e35c116a92a1568a7b78058f5949d2d60f9e0
SebGue <43440731+SebGue@users.noreply.github.com>
2020-10-21 18:07:24 +0200

    [CO] smaller changes to comp_resistance_wind

--- pyleecan/Methods/Machine/LamSquirrelCage/comp_resistance_wind.py
+++ pyleecan/Methods/Machine/LamSquirrelCage/comp_resistance_wind.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from numpy import pi
-from math import sin
+from numpy import pi, sin
 
 
-def comp_resistance_wind(self):
-    """Computation of the rotor resistance
+def comp_resistance_wind(self, qs=3):
+    """Computation of the equivalent rotor resistance of a cage winding with 'qs' number of phases
 
     Parameters
     ----------
     self : LamSquirrelCage
         A LamSquirrelCage object
+    qs : int
+        number of the equivalent rotor winding phases, default value: 3 phases
 
     Returns
     -------
@@ -22,16 +23,16 @@ def comp_resistance_wind(self):
     Sring = self.comp_surface_ring()
     lring = self.comp_length_ring()
     rho = self.ring_mat.elec.rho
-    Zs = self.get_Zs(self)
-    P = self.get_pole_pair_number(self)
+    Zs = self.get_Zs()
+    P = self.get_pole_pair_number()
 
-    Rring = rho * lring / (2 * Sring * sin(pi * P / Zs) ** 2)
+    Rring = rho * lring / Sring
 
     # calculate resistance rod
     Srod = self.slot.comp_surface_wind()
-    lrod = self.comp_length()
+    lrod = self.comp_length() + 2 * self.winding.Lewout
     Rrod = rho * lrod / Srod
 
-    Rtot = Rring + Rrod
+    Rtot = Zs / qs * (Rrod + Rring / (2 * sin(pi * P / Zs) ** 2))
 
     return Rtot
