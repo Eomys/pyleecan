8eebd2d0f44db03fe9d6729f09cf6f7231de1839
Pierre Bonneel <pierre.bonneel@gmail.com>
2020-10-22 00:11:01 +0200

    [NF] Define HoleUD from DXF in matplotlib viewer

--- pyleecan/GUI/Dxf/DXF.py
+++ pyleecan/GUI/Dxf/DXF.py
@@ -1,43 +1,44 @@
-from .Ui_DXF import Ui_DXF
-from PySide2.QtWidgets import (
-    QWidget,
-    QGraphicsScene,
-    QTableWidgetItem,
-    QComboBox,
-    QPushButton,
-    QLineEdit,
-)
-from PySide2.QtCore import Signal, QSize
-import PySide2.QtCore as QtCore
+from os.path import dirname, isfile
+
+from ezdxf import readfile
+from numpy import angle as np_angle
+from numpy import array
+from pyleecan.GUI.Dxf.dxf_to_pyleecan_list import dxf_to_pyleecan_list
+from pyleecan.GUI.Resources import pixmap_dict
+from PySide2.QtCore import QSize
 from PySide2.QtGui import QIcon, QPixmap
-from ...definitions import GUI_DIR
+from PySide2.QtWidgets import QComboBox, QFileDialog, QPushButton, QWidget
 
 from ...Classes.HoleUD import HoleUD
 from ...Classes.Magnet import Magnet
-from math import pi
-from cmath import phase
+from ...Classes.SurfLine import SurfLine
 from ...GUI.Tools.MPLCanvas import MPLCanvas2
-from numpy import array
-from matplotlib.patches import Patch
-from matplotlib.pyplot import axis, legend
+from .Ui_DXF import Ui_DXF
 
-from ...Functions.init_fig import init_fig
-from ...definitions import config_dict
-from ezdxf import readfile
-from pyleecan.GUI.Dxf.dxf_to_pyleecan_list import dxf_to_pyleecan_list
-from pyleecan.GUI.Resources import pixmap_dict
-
-MAGNET_COLOR = config_dict["PLOT"]["COLOR_DICT"]["MAGNET_COLOR"]
-
-LINE_LIST = list()
-SELECTED_LIST = list()
+# Column index for table
+TYPE_COL = 0
+DEL_COL = 1
+HL_COL = 2
 
 
 class DXF(Ui_DXF, QWidget):
+    """Dialog to create HoleUD objects from DXF files"""
+
     def __init__(self, dxf_path=None):
+        """Initialize the Dialog
+
+        Parameters
+        ----------
+        self : DXF
+            a DXF object
+        dxf_path : str
+            Path to a dxf file to read
+        """
+        # Widget setup
         QWidget.__init__(self)
         self.setupUi(self)
 
+        # Icon preparation
         self.delete_icon = QPixmap(pixmap_dict["delete_36"])
 
         # Initialize the graph
@@ -46,10 +47,18 @@ class DXF(Ui_DXF, QWidget):
         self.w_viewer.draw()
         self.main_layout.removeWidget(self.w_viewer)
         self.main_layout.insertWidget(0, self.w_viewer)
+        self.init_graph()
+
+        # Init properties
+        self.line_list = list()  # List of line from DXF
+        self.selected_list = list()  # List of currently selected lines
+        self.surf_list = list()  # List of defined surfaces
+        self.lf_center_x.setValue(0)
+        self.lf_center_y.setValue(0)
 
         # Load the DXF file if provided
         self.dxf_path = dxf_path
-        if dxf_path is not None:
+        if dxf_path is not None and isfile(dxf_path):
             self.open_document()
 
         # Setup Path selector for DXF files
@@ -64,68 +73,77 @@ class DXF(Ui_DXF, QWidget):
         self.w_surface_list.horizontalHeader().hide()
         self.w_surface_list.verticalHeader().hide()
         self.w_surface_list.setColumnWidth(2, 24)
-        self.nrows = 0
 
         # Connect signals to slot
         self.w_path_selector.pathChanged.connect(self.open_document)
         self.b_save.pressed.connect(self.save)
         self.b_plot.pressed.connect(self.plot)
-        self.w_surface_list.currentCellChanged.connect(self.highlight_surface)
 
         # Display the GUI
         self.show()
 
     def open_document(self):
-        """Open a new dxf in the viewer"""
+        """Open a new dxf in the viewer
 
+        Parameters
+        ----------
+        self : DXF
+            a DXF object
+        """
+
+        # Read the DXF file
         document = readfile(self.dxf_path)
-        # Model Space
         modelspace = document.modelspace()
-        # Create pyleecan objects
-        LINE_LIST = dxf_to_pyleecan_list(modelspace)
-        print(len(LINE_LIST))
-        SELECTED_LIST = [False for line in LINE_LIST]
-
+        # Convert DXF to pyleecan objects
+        self.line_list = dxf_to_pyleecan_list(modelspace)
+        # Display
+        self.selected_list = [False for line in self.line_list]
         self.update_graph()
 
-    def update_graph(self):
-        # self.w_viewer.refresh_fig()
-        print("Bla " + str(len(LINE_LIST)))
+    def init_graph(self):
+        """Initialize the viewer
+
+        Parameters
+        ----------
+        self : DXF
+            a DXF object
+        """
         fig, axes = self.w_viewer.fig, self.w_viewer.axes
         axes.set_axis_off()
 
-        # Draw the lines in the correct color
-        for ii, line in enumerate(LINE_LIST):
-            point_list = array(line.discretize(20))
-            if SELECTED_LIST[ii]:
-                color = "r"
-            else:
-                color = "k"
-            axes.plot(point_list.real, point_list.imag, color)
-
         # Setup interaction with graph
-        def set_cursor(event):
+        def select_line(event):
+            """Function to select/unselect the closest line from click"""
             X = event.xdata  # X position of the click
             Y = event.ydata  # Y position of the click
             # Get closer pyleecan object
             Z = X + 1j * Y
             min_dist = float("inf")
             closest_id = -1
-            for ii, line in enumerate(LINE_LIST):
+            for ii, line in enumerate(self.line_list):
                 line_dist = line.comp_distance(Z)
-
                 if line_dist < min_dist:
                     closest_id = ii
                     min_dist = line_dist
-            print("Debug")
-            print(len(SELECTED_LIST))
-            print(len(LINE_LIST))
-            print(closest_id)
-            SELECTED_LIST[closest_id] = not SELECTED_LIST[closest_id]
-            self.update_graph()
+            # Select/unselect line
+            self.selected_list[closest_id] = not self.selected_list[closest_id]
+            # Change line color
+            point_list = array(self.line_list[closest_id].discretize(20))
+            if self.selected_list[closest_id]:
+                color = "r"
+            else:
+                color = "k"
+            axes.plot(point_list.real, point_list.imag, color, zorder=2)
+            self.w_viewer.draw()
 
-        def zoom_fun(event):
-            base_scale = 0.3
+            # Check if the surface is complete
+            if self.check_selection():
+                self.add_surface()
+
+        def zoom(event):
+            """Function to zoom/unzoom according the mouse wheel"""
+
+            base_scale = 0.3  # Scaling factor
             # get the current x and y limits
             ax = self.w_viewer.axes
             cur_xlim = ax.get_xlim()
@@ -152,186 +170,236 @@ class DXF(Ui_DXF, QWidget):
             )
             self.w_viewer.draw()  # force re-draw
 
-        # Connect the cursor fonction to click on graph
-        self.w_viewer.mpl_connect("button_press_event", set_cursor)
+        # Connect the function
+        self.w_viewer.mpl_connect("button_press_event", select_line)
+        self.w_viewer.mpl_connect("scroll_event", zoom)
 
-        # attach the call back
-        self.w_viewer.mpl_connect("scroll_event", zoom_fun)
-
+        # Axis cleanup
         axes.axis("equal")
+        axes.set_axis_off()
+
+    def update_graph(self):
+        """Clean and redraw all the lines in viewer
+
+        Parameters
+        ----------
+        self : DXF
+            a DXF object
+        """
+        fig, axes = self.w_viewer.fig, self.w_viewer.axes
+        axes.clear()
+        axes.set_axis_off()
+
+        # Draw the lines in the correct color
+        for ii, line in enumerate(self.line_list):
+            point_list = array(line.discretize(20))
+            if self.selected_list[ii]:
+                color = "r"
+            else:
+                color = "k"
+            axes.plot(point_list.real, point_list.imag, color, zorder=1)
+
         self.w_viewer.draw()
 
-    def new_surface(self, element):
-        """Add the new surface in the QTableWidget"""
+    def check_selection(self):
+        """Check if every line in the selection form a surface
+
+        Parameters
+        ----------
+        self : DXF
+            a DXF object
+
+        Returns
+        -------
+        is_surf : bool
+            True if it forms a surface
+        """
+
+        # Create list of begin and end point for all lines
+        point_list = list()
+        for ii, line in enumerate(self.line_list):
+            if self.selected_list[ii]:
+                point_list.append(line.get_begin())
+                point_list.append(line.get_end())
+
+        # Check with a tolerance if every point is twice in the list
+        if len(point_list) == 0:
+            return False
+
+        for p1 in point_list:
+            count = 0
+            for p2 in point_list:
+                if abs(p1 - p2) < 1e-9:
+                    count += 1
+            if count != 2:
+                return False
+
+        return True
+
+    def add_surface(self):
+        """Validate the selection and create a surface object
+
+        Parameters
+        ----------
+        self : DXF
+            a DXF object
+        """
+
+        # Get all the selected lines
+        line_list = list()
+        for ii, line in enumerate(self.line_list):
+            if self.selected_list[ii]:
+                line_list.append(line.copy())
+        # Sort the lines (begin = end)
+        curve_list = list()
+        curve_list.append(line_list.pop())
+        while len(line_list) > 0:
+            end = curve_list[-1].get_end()
+            for ii in range(len(line_list)):
+                if abs(line_list[ii].get_begin() - end) < 1e-9:
+                    break
+                if abs(line_list[ii].get_end() - end) < 1e-9:
+                    line_list[ii].reverse()
+                    break
+            curve_list.append(line_list.pop(ii))
+        # Create the Surface object
+        self.surf_list.append(SurfLine(line_list=curve_list))
+        self.surf_list[-1].comp_point_ref(is_set=True)
+
+        # Update the GUI
         nrows = self.w_surface_list.rowCount()
-
-        # Surface label
+        # Adding Surface Type combobox
         self.w_surface_list.setRowCount(nrows + 1)
-        w_name = QLineEdit(element["name"])
-        self.w_surface_list.setCellWidget(nrows, 0, w_name)
-        w_name.textChanged.connect(self.edit_surface_name)
-
-        # Surface type
         combobox = QComboBox()
         combobox.addItems(["Hole", "Magnet"])
         self.w_surface_list.setCellWidget(
-            nrows, 1, combobox,
+            nrows,
+            TYPE_COL,
+            combobox,
         )
-
-        # Delete button
-        new_button = QPushButton()
-        new_button.setIcon(QIcon(self.delete_icon))
-        new_button.setIconSize(QSize(24, 24))
-        new_button.pressed.connect(self.delete_surface)
-
+        # Adding Delete button
+        del_button = QPushButton("Delete")
+        del_button.setIcon(QIcon(self.delete_icon))
+        del_button.setIconSize(QSize(24, 24))
+        del_button.pressed.connect(self.delete_surface)
         self.w_surface_list.setCellWidget(
-            nrows, 2, new_button,
+            nrows,
+            DEL_COL,
+            del_button,
         )
 
-        self.w_surface_list.setCurrentCell(nrows, 0)
+        # Adding Highlight button
+        HL_button = QPushButton("Highlight")
+        # HL_button.setIcon(QIcon(self.delete_icon))
+        HL_button.setIconSize(QSize(24, 24))
+        HL_button.pressed.connect(self.highlight_surface)
+        self.w_surface_list.setCellWidget(
+            nrows,
+            HL_COL,
+            HL_button,
+        )
 
-    def edit_surface_name(self, name):
-        """Edit a surface name from the QTableWidget"""
-        # Get current row
-        nrow = self.w_surface_list.currentRow()
-        # Edit corresponding surface
-        self.viewer.surface_list[nrow]["name"] = name
+        # Remove selection
+        self.selected_list = [False for line in self.line_list]
+        self.update_graph()
+
+    def get_hole(self):
+        """Generate the HoleUD object corresponding to the selected surfaces
+
+        Parameters
+        ----------
+        self : DXF
+            a DXF object
+
+        Returns
+        -------
+        hole : HoleUD
+            User defined hole according to selected surfaces
+        """
+
+        hole = HoleUD(surf_list=self.surf_list)
+        # Set labels
+        Nmag = 0
+        for ii in range(self.w_surface_list.rowCount()):
+            if self.w_surface_list.cellWidget(ii, TYPE_COL).currentIndex() == 0:
+                hole.surf_list[ii].label = "Hole"
+            else:
+                hole.surf_list[ii].label = "HoleMagnet"
+                Nmag += 1
+        # Create magnet objects
+        hole.magnet_dict = dict()
+        for ii in range(Nmag):
+            hole.magnet_dict["magnet_" + str(ii)] = Magnet()
+
+        # Sort the surfaces
+        angles = [np_angle(surf.point_ref) for surf in hole.surf_list]
+        idx = sorted(range(len(angles)), key=lambda k: angles[k])
+        surf_list_sorted = [hole.surf_list[ii] for ii in idx]
+        hole.surf_list = surf_list_sorted
+
+        # Rotation
+        Zref = sum([surf.point_ref for surf in hole.surf_list])
+        for surf in hole.surf_list:
+            surf.rotate(-1 * np_angle(Zref))
+
+        # Set metadata
+        hole.Zh = self.si_Zh.value()
+
+        return hole
 
     def plot(self):
-        """Plot every selected surfaces"""
-        # TODO plot with pyleecan graphic chart hole and magnet
-        Zh = self.si_zh.value()
-        angle = float(self.lf_axe_angle.value())
-        center = float(self.lf_center_x.value()) + float(self.lf_center_y.value()) * 1j
-        surf_list = [e["surface"].copy() for e in self.viewer.surface_list]
-        for i, surf in enumerate(surf_list):
-            # Translate and rotate
-            surf.translate(-center)
-            surf.rotate(-angle)
-            if self.w_surface_list.cellWidget(i, 1).currentText() == "Magnet":
-                surf.label = "HoleMagnet"
-            else:
-                surf.label = "Hole"
+        """Plot the current state of the hole
 
-        # TODO sort the surf_list according
-        angles = [phase(surf.point_ref) for surf in surf_list]
-        idx = sorted(range(len(angles)), key=lambda k: angles[k])
-        surf_list_sorted = [surf_list[i] for i in idx]
-
-        mag_list = [surf for surf in surf_list_sorted if surf.label == "HoleMagnet"]
-
-        mag_dict = {}
-        for i in range(len(mag_list)):
-            mag_dict["magnet_{}".format(i)] = Magnet(type_magnetization=0, Lmag=1)
-
-        plot_without_lam(HoleUD(Zh=8, surf_list=surf_list_sorted, magnet_dict=mag_dict))
-
-    def highlight_surface(self, crow, ccol, prow, pcol):
-        """Highlight a surface if a the user clic in the """
-        # Return if the row has not changed or the surface is deleted
-        if crow == prow or ccol == 2:  # or len(self.viewer.lines_selection) != 0:
-            return
-
-        if crow == -1:
-            self.viewer.remove_highlight_surface()
-            return
-
-        surf_name = self.w_surface_list.cellWidget(crow, 0).text()
-        self.viewer.highlight_surface(surf_name)
+        Parameters
+        ----------
+        self : DXF
+            a DXF object
+        """
+        hole = self.get_hole()
+        hole.plot()
 
     def delete_surface(self):
-        """Delete a surface """
+        """Delete a selected surface
+
+        Parameters
+        ----------
+        self : DXF
+            a DXF object
+        """
         nrow = self.w_surface_list.currentRow()
-        surf_name = self.w_surface_list.cellWidget(nrow, 0).text()
-        for element in self.viewer.surface_list:
-            if element["name"] == surf_name:
-                self.viewer.surface_list.remove(element)
-                break
+        self.surf_list.pop(nrow)
         self.w_surface_list.removeRow(nrow)
 
+    def highlight_surface(self):
+        """Highlight a surface to find it on the viewer
+
+        Parameters
+        ----------
+        self : DXF
+            a DXF object
+        """
+        self.selected_list = [False for line in self.line_list]
+        surf = self.surf_list[self.w_surface_list.currentRow()]
+        # Find the index of the surface line in self.line_list
+        for surf_line in surf.line_list:
+            mid = surf_line.get_middle()
+            for ii, line in enumerate(self.line_list):
+                if abs(mid - line.get_middle()) < 1e-6:
+                    self.selected_list[ii] = True
+        self.update_graph()
+
     def save(self):
-        """Save the corresponding surfaces in a HoleUD"""
-        # Zh = 8
-        # angle = pi / 8
-        # center = 0 + 0j
-        Zh = self.si_zh.value()
-        angle = float(self.lf_axe_angle.value())
-        center = float(self.lf_center_x.value()) + float(self.lf_center_y.value()) * 1j
-        surf_list = [e["surface"].copy() for e in self.viewer.surface_list]
-        for i, surf in enumerate(surf_list):
-            # Translate and rotate
-            surf.translate(-center)
-            surf.rotate(-angle)
-            if self.w_surface_list.cellWidget(i, 1).currentText() == "Magnet":
-                surf.label = "HoleMagnet"
-            else:
-                surf.label = "Hole"
+        """Save the HoleUD object in a json file
 
-        # TODO sort the surf_list according
-        angles = [phase(surf.point_ref) for surf in surf_list]
-        idx = sorted(range(len(angles)), key=lambda k: angles[k])
-        surf_list_sorted = [surf_list[i] for i in idx]
+        Parameters
+        ----------
+        self : DXF
+            a DXF object
+        """
 
-        mag_list = [surf for surf in surf_list_sorted if surf.label == "HoleMagnet"]
+        hole = self.get_hole()
 
-        mag_dict = {}
-        for i in range(len(mag_list)):
-            mag_dict["magnet_{}".format(i)] = Magnet(type_magnetization=0, Lmag=1)
-
-        HoleUD(Zh=8, surf_list=surf_list_sorted, magnet_dict=mag_dict).save(
-            "Scripts/DXF/test_holeud.pkl"
-        )
-        print("Saving")
-
-
-def plot_without_lam(hole):
-    """Plot the Hole in a matplotlib fig
-
-    Parameters
-    ----------
-    self : Hole
-        A Hole object
-    fig :
-        if None, open a new fig and plot, else add to the current
-        one (Default value = None)
-    display_magnet : bool
-        if True, plot the magnet inside the hole, if there is any (Default value = True)
-
-    Returns
-    -------
-    None
-    """
-    fig = None
-    display_magnet = True
-    display = fig is None
-    if display:
-        color = "k"
-    else:
-        color = "w"
-
-    surf_hole = hole.build_geometry()
-    patches = list()
-    for surf in surf_hole:
-        if "Magnet" in surf.label and display_magnet:
-            patches.extend(surf.get_patches(color=MAGNET_COLOR))
-        else:
-            patches.extend(surf.get_patches(color=color))
-
-    # Display the result
-    (fig, axes, patch_leg, label_leg) = init_fig(fig)
-    axes.set_xlabel("(m)")
-    axes.set_ylabel("(m)")
-    axes.set_title("Hole")
-
-    # Add all the hole (and magnet) to fig
-    for patch in patches:
-        axes.add_patch(patch)
-
-    # Axis Setup
-    axis("equal")
-
-    if display_magnet and "Magnet" in [surf.label for surf in surf_hole]:
-        patch_leg.append(Patch(color=MAGNET_COLOR))
-        label_leg.append("Magnet")
-        legend(patch_leg, label_leg)
-    fig.show()
+        save_file_path = QFileDialog.getSaveFileName(
+            self, self.tr("Save file"), dirname(self.dxf_path), "Json (*.json)"
+        )[0]
+        hole.save(save_file_path)
