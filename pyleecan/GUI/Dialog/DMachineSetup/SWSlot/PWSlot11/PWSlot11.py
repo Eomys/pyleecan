547425f34c553ef93d5ed436b2550fd1114e67f4
BenjaminGabet <benjamin.gabetpro@gmail.com>
2020-10-23 17:01:14 +0200

    [CC] Changing the error message

--- pyleecan/GUI/Dialog/DMachineSetup/SWSlot/PWSlot11/PWSlot11.py
+++ pyleecan/GUI/Dialog/DMachineSetup/SWSlot/PWSlot11/PWSlot11.py
@@ -222,21 +222,21 @@ class PWSlot11(Gen_PWSlot11, QWidget):
 
         # Check that everything is set
         if lam.slot.W0 is None:
-            return translate("You must set W0 !", "PWSlot11")
+            return translate("You must set W0 !", "PWSlot11 check")
         elif lam.slot.W1 is None:
-            return translate("You must set W1 !", "PWSlot11")
+            return translate("You must set W1 !", "PWSlot11 check")
         elif lam.slot.W2 is None:
-            return translate("You must set W2 !", "PWSlot11")
+            return translate("You must set W2 !", "PWSlot11 check")
         elif lam.slot.H0 is None:
-            return translate("You must set H0 !", "PWSlot11")
+            return translate("You must set H0 !", "PWSlot11 check")
         elif lam.slot.H1 is None:
-            return translate("You must set H1 !", "PWSlot11")
+            return translate("You must set H1 !", "PWSlot11 check")
         elif lam.slot.H2 is None:
-            return translate("You must set H2 !", "PWSlot11")
+            return translate("You must set H2 !", "PWSlot11 check")
         elif lam.slot.R1 is None:
-            return translate("You must set R1 !", "PWSlot11")
+            return translate("You must set R1 !", "PWSlot11 check")
         elif lam.slot.H1 >= pi / 2:
-            return translate("You must have H1 < 90°", "PWSlot11")
+            return translate("You must have H1 < 90°", "PWSlot11 check")
 
         # Constraints
         try:
--- pyleecan/GUI/Dialog/DMachineSetup/SWSlot/PWSlot14/PWSlot14.py
+++ pyleecan/GUI/Dialog/DMachineSetup/SWSlot/PWSlot14/PWSlot14.py
@@ -156,15 +156,15 @@ class PWSlot14(Gen_PWSlot14, QWidget):
 
         # Check that everything is set
         if lam.slot.W0 is None:
-            return translate("You must set W0 !", "PWSlot14")
+            return translate("You must set W0 !", "PWSlot14 check")
         elif lam.slot.W3 is None:
-            return translate("You must set W3 !", "PWSlot14")
+            return translate("You must set W3 !", "PWSlot14 check")
         elif lam.slot.H0 is None:
-            return translate("You must set H0 !", "PWSlot14")
+            return translate("You must set H0 !", "PWSlot14 check")
         elif lam.slot.H1 is None:
-            return translate("You must set H1 !", "PWSlot14")
+            return translate("You must set H1 !", "PWSlot14 check")
         elif lam.slot.H3 is None:
-            return translate("You must set H3 !", "PWSlot14")
+            return translate("You must set H3 !", "PWSlot14 check")
 
         # Check that everything is set right
         # Constraints
