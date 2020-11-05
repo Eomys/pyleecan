06655d63f2b8f4104a2577cb669422373caa1982
SebGue <sebgue@gmx.net>
2020-11-05 17:03:18 +0100

    [WiP] update elec.comp_joule_losses for rotor loss

--- pyleecan/Methods/Simulation/EEC_SCIM/comp_joule_losses.py
+++ pyleecan/Methods/Simulation/EEC_SCIM/comp_joule_losses.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from numpy import mean, zeros
+from ....Functions.Electrical.coordinate_transformation import n2ab
 
 
 def comp_joule_losses(self, output):
@@ -11,14 +13,33 @@ def comp_joule_losses(self, output):
     output : Output
         an Output object
     """
-    # TODO include rotor losses
-    # TODO use of output Id Iq may be wrong ???
-
+    # TODO utilize loss models instead here
+    # compute stator joule losses
     qs = output.simu.machine.stator.winding.qs
     Id = output.elec.Id_ref
     Iq = output.elec.Iq_ref
-    R = self.parameters["Rs"]
+    Rs = self.parameters["Rs"]
 
-    Pj_losses = qs * R * (Id ** 2 + Iq ** 2) / 2
+    P_joule_s = qs * Rs * (Id ** 2 + Iq ** 2)
 
-    output.elec.Pj_losses = Pj_losses
+    # compute rotor joule losses
+    qr = output.simu.machine.rotor.winding.qs
+    sym = output.simu.machine.comp_periodicity()[0]
+    Rr = self.parameters["Rr_norm"] / self.parameters["norm"] ** 2
+
+    # get the bar currents
+    Ir = output.elec.Ir.get_along("time", "phase")["Ir"].T
+
+    # transform rotor current to 2 phase equivalent
+    qr_eff = qr // sym
+    Ir_2ph = zeros([Ir.shape[0], 2])
+    for ii in range(sym):
+        id0 = qr_eff * ii
+        id1 = qr_eff * (ii + 1)
+        Ir_2ph += n2ab(Ir[:, id0:id1], n=qr_eff) / sym
+
+    Ir_mag = abs(Ir_2ph[:, 0] + 1j * Ir_2ph[:, 1])
+
+    P_joule_r = 3 * Rr * mean(Ir_mag ** 2) / 2
+
+    output.elec.Pj_losses = P_joule_s + P_joule_r
