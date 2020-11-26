e072a9b1c0f975c19f6034adfe5eb0396ad91c59
Pierre Bonneel <pierre.bonneel@gmail.com>
2020-11-26 15:42:02 +0100

    [BC] meshsol.group is None

--- Tests/Methods/Mesh/test_get_group.py
+++ Tests/Methods/Mesh/test_get_group.py
@@ -7,24 +7,7 @@ from pyleecan.Classes.MeshSolution import MeshSolution
 from pyleecan.Classes.PointMat import PointMat
 import numpy as np
 
-mesh = MeshMat()
-mesh.cell["triangle"] = CellMat(nb_pt_per_cell=3)
-mesh.point = PointMat()
-mesh.point.add_point(np.array([0, 0]))
-mesh.point.add_point(np.array([1, 0]))
-mesh.point.add_point(np.array([1, 2]))
-mesh.point.add_point(np.array([2, 3]))
-mesh.point.add_point(np.array([3, 3]))
 
-mesh.add_cell(np.array([0, 1, 2]), "triangle")
-mesh.add_cell(np.array([1, 2, 3]), "triangle")
-mesh.add_cell(np.array([4, 2, 3]), "triangle")
-
-meshsol = MeshSolution()
-meshsol.mesh = [mesh]
-# self.meshsol.group = dict()
-meshsol.group["stator"] = np.array([0, 1])
-meshsol.group["rotor"] = np.array([2])
 DELTA = 1e-10
 
 
@@ -32,6 +15,25 @@ DELTA = 1e-10
 @pytest.mark.MeshSol
 def test_MeshMat_1group():
     """unittest for 1 group"""
+
+    mesh = MeshMat()
+    mesh.cell["triangle"] = CellMat(nb_pt_per_cell=3)
+    mesh.point = PointMat()
+    mesh.point.add_point(np.array([0, 0]))
+    mesh.point.add_point(np.array([1, 0]))
+    mesh.point.add_point(np.array([1, 2]))
+    mesh.point.add_point(np.array([2, 3]))
+    mesh.point.add_point(np.array([3, 3]))
+
+    mesh.add_cell(np.array([0, 1, 2]), "triangle")
+    mesh.add_cell(np.array([1, 2, 3]), "triangle")
+    mesh.add_cell(np.array([4, 2, 3]), "triangle")
+
+    meshsol = MeshSolution()
+    meshsol.mesh = [mesh]
+    meshsol.group = dict()
+    meshsol.group["stator"] = np.array([0, 1])
+    meshsol.group["rotor"] = np.array([2])
 
     MS_grp = meshsol.get_group("stator")
     cells_grp, nb_cell, indices = MS_grp.get_mesh().get_cell()
