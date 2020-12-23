484e398963d20e5ef60b33343f070786dba9c063
SebGue <sebgue@gmx.net>
2020-12-23 13:18:09 +0100

    [NF] add validation

--- Tests/Validation/Simulation/test_FEMM_Loss.py
+++ Tests/Validation/Simulation/test_FEMM_Loss.py
@@ -14,7 +14,7 @@ from pyleecan.Classes.LossModelBertotti import LossModelBertotti
 from pyleecan.Classes.LossModelWinding import LossModelWinding
 from pyleecan.Classes.ImportMatrixXls import ImportMatrixXls
 
-from numpy import linspace
+from numpy import linspace, pi
 from pyleecan.Functions.load import load
 from pyleecan.definitions import DATA_DIR
 
@@ -22,27 +22,42 @@ from pyleecan.definitions import DATA_DIR
 @pytest.mark.long
 @pytest.mark.validation
 @pytest.mark.FEMM
-# @pytest.mark.DEV
+@pytest.mark.DEV
 def test_FEMM_Loss():
-    """Validation of the Loss implementaiton using FEMM.
-    Only test for simulation running without errors for now."""
+    """Validation of the Loss implementaiton using MagFEMM and compare to
+    FEMM example - https://www.femm.info/wiki/SPMLoss -
+    """
+    # TODO stacking factor is disregarded for now but should be included
 
-    machine = load(join(DATA_DIR, "Machine", "IPMSM_A.json"))
-    pp = machine.rotor.get_pole_pair_number()
+    # Reference values:
+    rotor_speed = 4000  # RPM
+    mechanical_power = 62.2952  # W
+    rotor_core_loss = 0.0574995  # W
+    stator_core_loss = 3.40587  # W
+    prox_loss = 0.0585815  # W
+    i_sqr_R_loss = 4.37018  # W
+    magnet_loss = 1.38116  # W
+    total_electromagnetic_losses = 9.27329  # W
+
+    Id_ref = 0
+    Iq_ref = 2 ** (1 / 2)
+
+    n_step = 180
+    Nrev = 1 / 2
+
+    # readability
+    machine = load(join(DATA_DIR, "Machine", "SPMSM_020.json"))
+    machine.stator.winding.is_reverse_wind = True
     simu = Simu1(name="FEMM_IronLoss", machine=machine)
 
-    rotor_speed = 2000
-
     # Definition of the enforced output of the electrical module
-    simu.input = InputCurrent(Id_ref=0, Iq_ref=0, Na_tot=2048, N0=rotor_speed)
+    simu.input = InputCurrent(Id_ref=Id_ref, Iq_ref=Iq_ref, Na_tot=2048, N0=rotor_speed)
 
     # time discretization [s]
     # TODO without explicit time def. there is an error
-    n_step = 16
-
     simu.input.time = ImportMatrixVal()
     simu.input.time.value = linspace(
-        start=0, stop=60 / rotor_speed / pp, num=n_step, endpoint=False
+        start=0, stop=60 / rotor_speed * Nrev, num=n_step, endpoint=False
     )  # n_step timesteps
 
     # Definition of the magnetic simulation: with periodicity
@@ -52,7 +67,7 @@ def test_FEMM_Loss():
 
     simu.mag.is_get_mesh = True  # To get FEA mesh for latter post-procesing
 
-    # --- Setup the Loss Model ----------------------------------------------------------- #
+    # --- Setup the Loss Model ------------------------------------------------------- #
     simu.loss = Loss()
 
     myIronLoss = LossModelBertotti()
@@ -61,51 +76,71 @@ def test_FEMM_Loss():
     simu.loss.iron["Stator"] = [myIronLoss]
     simu.loss.winding["Stator"] = [myWindingLoss]
 
+    # FEMM ex. Ch = 143 W / m³ / T² / Hz --> k_hy = 2.089 W / kg @ F_REF, B_REF
+    #          Ce = 0.53 W / m³ / T² / Hz² --> k_ed = 0.387 W / kg @ F_REF, B_REF
+
+    # stator
     myIronLoss.name = "Stator Iron Losses"
-    myIronLoss.k_hy = None
+    myIronLoss.k_hy = 2.089  # None
     myIronLoss.alpha_hy = 2
-    myIronLoss.k_ed = None
+    myIronLoss.k_ed = 0.387  # None
     myIronLoss.alpha_ed = 2
     myIronLoss.k_ex = 0
     myIronLoss.alpha_ex = 1.5
     myIronLoss.group = "stator core"  # this is the FEMM group name
 
+    # rotor
+    simu.loss.iron["Rotor"] = [myIronLoss.copy()]
+    simu.loss.iron["Rotor"][0].name = "Rotor Iron Losses"
+    simu.loss.iron["Rotor"][0].group = "rotor core"
+
     # TODO load loss data with BH curve by default
-    LossData = ImportMatrixXls()
-    # LossData.file_path = "pyleecan\\pyleecan\\Data\\Material\\M400-50A.xlsx"
-    LossData.file_path = join(DATA_DIR, "Material", "M400-50A.xlsx")
-    LossData.is_transpose = False
-    LossData.sheet = "LossData"
-    LossData.skiprows = 2
-    LossData.usecols = None
+    # TODO add M19 loss data to compare parameter estimates
+    # LossData = ImportMatrixXls()
+    # LossData.file_path = join(DATA_DIR, "Material", "M400-50A.xlsx")
+    # LossData.is_transpose = False
+    # LossData.sheet = "LossData"
+    # LossData.skiprows = 2
+    # LossData.usecols = None
 
-    machine.stator.mat_type.mag.LossData = LossData
+    # machine.stator.mat_type.mag.LossData = LossData
 
-    # --- Run the Loss Simulation -------------------------------------------------------- #
+    # --- Run the Loss Simulation ---------------------------------------------------- #
     out = Output(simu=simu)
     simu.run()
 
     loss = out.loss
     mshsol = loss.get_loss_dist(loss_type="Iron", label="Stator", index=0)
-    assert mshsol is not None
 
     # mshsol.plot_contour(label="LossDens", itime=7)
     # mshsol.plot_contour(label="LossDensSum", itime=0)
 
+    P_mech = 2 * pi * rotor_speed / 60 * out.mag.Tem_av
+
     loss_stator_iron = loss.get_loss(loss_type="Iron", label="Stator", index=0)
+    loss_rotor_iron = loss.get_loss(loss_type="Iron", label="Rotor", index=0)
     loss_stator_wind = loss.get_loss(loss_type="Winding", label="Stator", index=0)
 
-    print(f"stator iron loss = {loss_stator_iron.get_field([]).mean()} W")
+    loss_stator_iron = loss_stator_iron.get_field([]).mean()
+    loss_rotor_iron = loss_rotor_iron.get_field([]).mean()
+
+    print(f"mechanical power = {P_mech} W")
+    print(f"stator iron loss = {loss_stator_iron} W")
+    print(f"rotor iron loss = {loss_rotor_iron} W")
     print(f"stator winding loss = {loss_stator_wind.get_field([]).mean()} W")
 
+    delta = 5 / 100  # arbitary allowed relative difference
+
     assert mshsol is not None
-    assert loss_stator_iron is not None
+    assert (abs(loss_stator_iron - stator_core_loss) / stator_core_loss) <= delta
+    # rotor loss is disregarded since absolute value seems to be too small
+    # assert abs(loss_rotor_iron - rotor_core_loss)/rotor_core_loss <= delta
     assert loss_stator_wind is not None
+    assert (abs(mechanical_power - P_mech) / mechanical_power) <= delta
 
     return out
 
 
 # To run it without pytest
 if __name__ == "__main__":
-
     out = test_FEMM_Loss()
