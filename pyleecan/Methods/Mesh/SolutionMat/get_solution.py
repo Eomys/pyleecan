78aaf7aef76b1bf47f0dcc4f4fb4eeae4fccef3f
Sebastian Günther <sebgue@gmx.net>
2021-01-06 14:53:52 +0100

    [CC] fixed bug + 'random' test indices

--- pyleecan/Methods/Mesh/SolutionMat/get_solution.py
+++ pyleecan/Methods/Mesh/SolutionMat/get_solution.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from numpy import take
+from numpy import take, where, array
 
 
 def get_solution(self, indice=None):
@@ -33,17 +33,22 @@ def get_solution(self, indice=None):
     # check input indices
     if not indice:
         indice = s_indice
-    else:
-        max_indice = max(s_indice)
-        if max_indice < max(indice):
-            logger.warning(
-                "Given indices exceed Solution indices. Indices will be truncated."
-            )
-        indice = [i for i in indice if i <= max_indice]
+
+    if set(indice) - set(s_indice):
+        logger.warning(
+            "At least one input indice is not part of the solution. "
+            + "Respective indice will be skipped."
+        )
+
+    # skip indice that are not part of the solution
+    new_indice = [ii for ii in indice if ii in s_indice]
+
+    # get array index of new_indice
+    array_indice = [where(array(s_indice) == ii)[0][0] for ii in new_indice]
 
     # setup requested solution
-    axis_size[Iindice] = len(indice)
-    new_field_sol = take(field_sol, indice, axis=Iindice)
+    axis_size[Iindice] = len(new_indice)
+    new_field_sol = take(field_sol, array_indice, axis=Iindice)
 
     solution = type(self)(
         label=self.label,
