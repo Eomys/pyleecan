4baa086dba37c2c1765786be82b7af29a71b8126
Pierre Bonneel <pierre.bonneel@gmail.com>
2021-01-08 17:43:08 +0100

    [CO] New definition with dict

--- pyleecan/Methods/Slot/SlotW10/build_geometry_active.py
+++ pyleecan/Methods/Slot/SlotW10/build_geometry_active.py
@@ -33,7 +33,12 @@ def build_geometry_active(self, Nrad, Ntan, is_simplified=False, alpha=0, delta=
     # get the name of the lamination
     st = self.get_name_lam()
 
-    [Z10, Z9, Z8, Z7, Z6, Z5, Z4, Z3, Z2, Z1] = self._comp_point_coordinate()
+    point_dict = self._comp_point_coordinate()
+    Z4 = point_dict["Z4"]
+    Z5 = point_dict["Z5"]
+    Z6 = point_dict["Z6"]
+    Z7 = point_dict["Z7"]
+
     X = linspace(Z7, Z6, Nrad + 1)
     # Nrad+1 and Ntan+1 because 3 points => 2 zones
     Z = zeros((Nrad + 1, Ntan + 1), dtype=complex)
