0bbdfdab11d7b43ee41047941f3335e3ac4cb4bc
Pierre Bonneel <pierre.bonneel@gmail.com>
2021-01-08 17:45:46 +0100

    [NF] plot schematics for SlotW10

--- pyleecan/Functions/Plot/__init__.py
+++ pyleecan/Functions/Plot/__init__.py
@@ -30,3 +30,23 @@ ifft_dict = {
     "freqs": "time",
     "wavenumber": "angle",
 }
+
+## Paramater for Schematics plot
+P_FONT_SIZE = 12  # Point Font size
+SC_FONT_SIZE = 12  # Schematics Font size
+TEXT_BOX = dict(  # Parameter of the text box
+    boxstyle="round",
+    ec=(0.0, 0.0, 0.0),
+    fc=(1.0, 1.0, 1.0),
+)
+# Arrow parameters
+ARROW_WIDTH = 2
+ARROW_COLOR = "black"
+# Schematics lines
+SC_LINE_COLOR = "black"
+SC_LINE_STYLE = "dotted"
+SC_LINE_WIDTH = 1
+# Main lines
+MAIN_LINE_COLOR = "0.5"  # Gray
+MAIN_LINE_STYLE = "dotted"
+MAIN_LINE_WIDTH = 1
