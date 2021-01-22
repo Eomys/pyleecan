4f7bed292eb435cf93d3ee59ffd69e98659e33c2
Pierre Bonneel <pierre.bonneel@gmail.com>
2021-01-23 00:06:28 +0100

    [NF] Adding SlotW22 schematics

--- pyleecan/Classes/SlotW22.py
+++ pyleecan/Classes/SlotW22.py
@@ -67,6 +67,11 @@ try:
 except ImportError as error:
     get_surface_active = error
 
+try:
+    from ..Methods.Slot.SlotW22.plot_schematics import plot_schematics
+except ImportError as error:
+    plot_schematics = error
+
 
 from ._check import InitUnKnowClassError
 
@@ -190,6 +195,17 @@ class SlotW22(Slot):
         )
     else:
         get_surface_active = get_surface_active
+    # cf Methods.Slot.SlotW22.plot_schematics
+    if isinstance(plot_schematics, ImportError):
+        plot_schematics = property(
+            fget=lambda x: raise_(
+                ImportError(
+                    "Can't use SlotW22 method plot_schematics: " + str(plot_schematics)
+                )
+            )
+        )
+    else:
+        plot_schematics = plot_schematics
     # save and copy methods are available in all object
     save = save
     copy = copy
