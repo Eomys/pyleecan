0e342458998882d7a2d5ff55fb5e6ba915aa144e
SebGue <sebgue@gmx.net>
2021-02-02 22:30:39 +0100

    [NF] allow retrocompatibility for all pyleecan typ

--- pyleecan/Functions/Load/retrocompatibility.py
+++ pyleecan/Functions/Load/retrocompatibility.py
@@ -1,22 +1,50 @@
+def _search_(init_dict, convert_list):
+    # walk through the dict
+    for value in init_dict.values():
+        if isinstance(value, dict):
+            # check if the sub dict is of pyleecan type
+            if "__class__" in value.keys():
+                if (
+                    value["__class__"] == "MachineSIPMSM"
+                    and "magnet" not in value["rotor"]
+                ):
+                    # add to list for later conversion
+                    convert_list.append(value)
+            # recursively search the dict
+            _search_(value, convert_list)
+        elif isinstance(value, list):
+            for item in value:
+                if isinstance(item, dict):
+                    # recursively search the dict
+                    _search_(item, convert_list)
+
+
 def convert_init_dict(init_dict):
     """Convert an init_dict from an old version of pyleecan to the current one"""
+    convert_list = []
+    _search_(init_dict, convert_list)
 
     # V 1.0.4 => 1.1.0: New definition for LamSlotMag + SlotMag
-    if init_dict["__class__"] == "MachineSIPMSM" and "magnet" not in init_dict["rotor"]:
+    for machine_dict in convert_list:
         print("Old machine version detected, Updating the LamSlotMag object")
-        # Moving the magnet (assume only one magnet)
-        assert (
-            len(init_dict["rotor"]["slot"]["magnet"]) == 1
-        ), "LamSlotMag with more than one magnet per pole is no longer available (for now)"
-        init_dict["rotor"]["magnet"] = init_dict["rotor"]["slot"]["magnet"][0]
-        init_dict["rotor"]["slot"].pop("magnet")
+        # readability
+        rotor = machine_dict["rotor"]
+        slot = machine_dict["rotor"]["slot"]
+
+        # Moving the magnet (use only one magnet)
+        if len(slot["magnet"]) > 1:
+            print(
+                "LamSlotMag with more than one magnet per pole "
+                + "is not available for now. Only keeping first magnet."
+            )
+        rotor["magnet"] = slot["magnet"][0]
+        slot.pop("magnet")
+
         # Update the slot with the magnet parameters
-        init_dict["rotor"]["slot"]["__class__"] = (
-            "SlotM" + init_dict["rotor"]["magnet"]["__class__"][-2:]
-        )
-        init_dict["rotor"]["slot"]["Wmag"] = init_dict["rotor"]["magnet"]["Wmag"]
-        init_dict["rotor"]["slot"]["Hmag"] = init_dict["rotor"]["magnet"]["Hmag"]
-        if "Rtop" in init_dict["rotor"]["magnet"]:
-            init_dict["rotor"]["slot"]["Rtopm"] = init_dict["rotor"]["magnet"]["Rtop"]
-        init_dict["rotor"]["magnet"]["__class__"] = "Magnet"
-    return init_dict
+        if rotor['magnet'] is not None:
+            slot["__class__"] = "SlotM" + rotor["magnet"]["__class__"][-2:]
+            slot["Wmag"] = rotor["magnet"]["Wmag"]
+            slot["Hmag"] = rotor["magnet"]["Hmag"]
+            if "Rtop" in rotor["magnet"]:
+                slot["Rtopm"] = rotor["magnet"]["Rtop"]
+            rotor["magnet"]["__class__"] = "Magnet"
