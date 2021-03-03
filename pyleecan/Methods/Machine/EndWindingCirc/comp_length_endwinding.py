c18a5207779e0c882ed992a565016b293e0fdb17
SebGue <sebgue@gmx.net>
2021-03-03 13:29:34 +0100

    [BC] end winding length calc.

--- pyleecan/Methods/Machine/EndWindingCirc/comp_length_endwinding.py
+++ pyleecan/Methods/Machine/EndWindingCirc/comp_length_endwinding.py
@@ -50,6 +50,6 @@ def comp_length_endwinding(self):
                 coil_pitch = Zs / p / 2
 
     # calculate the length as a half circle
-    end_wind_length = pi * Rmid * coil_pitch / Zs
+    end_wind_length = pi * Rmid * coil_pitch / Zs / 2
 
     return end_wind_length
