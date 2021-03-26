fab89b6bd7f019720916eca36f9eb9fe2082bf08
Pierre Bonneel <pierre.bonneel@gmail.com>
2021-03-26 11:08:00 +0100

    [BC] type check in setter

--- pyleecan/Methods/Simulation/DataKeeper/_set_result.py
+++ pyleecan/Methods/Simulation/DataKeeper/_set_result.py
@@ -7,16 +7,17 @@ def _set_result(self, value):
     if type(value) is int and value == -1:
         value = list()
     # Load pyleecan/SciDataTool objects
-    for ii, val in enumerate(value):
-        if isinstance(val, dict) and "__class__" in val:
-            try:
-                class_obj = import_class(
-                    "pyleecan.Classes", val.get("__class__"), "result_ref"
-                )
-            except:
-                class_obj = import_class(
-                    "SciDataTool.Classes", val.get("__class__"), "result_ref"
-                )
-            value[ii] = class_obj(init_dict=val)
+    if isinstance(value, list):
+        for ii, val in enumerate(value):
+            if isinstance(val, dict) and "__class__" in val:
+                try:
+                    class_obj = import_class(
+                        "pyleecan.Classes", val.get("__class__"), "result_ref"
+                    )
+                except:
+                    class_obj = import_class(
+                        "SciDataTool.Classes", val.get("__class__"), "result_ref"
+                    )
+                value[ii] = class_obj(init_dict=val)
     check_var("result", value, "list")
     self._result = value
