3dee5590f82ca400013966f3afbefb7eaca1a64d
Pierre Bonneel <pierre.bonneel@eomys.com>
2021-04-08 11:43:23 +0200

    [BC] Convert keys to list

--- pyleecan/Methods/Output/XOutput/get_symbol_list.py
+++ pyleecan/Methods/Output/XOutput/get_symbol_list.py
@@ -2,7 +2,7 @@ def get_symbol_list(self):
     """
     Return the list of the symobl of the DataKeeper and ParamExplorer
     """
-    symbol_list = self.keys()  # DataKeeper or OptiObjective
+    symbol_list = list(self.keys())  # DataKeeper or OptiObjective
     symbol_list.extend([pe.symbol for pe in self.paramexplorer_list])
 
     return symbol_list
