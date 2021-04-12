Model errors fixed such as µ0 instead of µ in comp_magnetrosctrictive_tensor or a 1/2 factor in element_loop. Unit test on normal computation added.

Tests/Methods/Simulation/test_ForceTensor_compforce.py
@@ -17,6 +17,7 @@ from pyleecan.Classes.MeshMat import MeshMat
 from pyleecan.Classes.CellMat import CellMat
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 @pytest.mark.skip
@@ -176,6 +177,98 @@ def test_comp_magnetostrictive_tensor_1cell():
     return True
 
 
+def test_comp_normal_to_edge():
+
+    x_normal = []
+    y_normal = []
+    x_nodes = []
+    y_nodes = []
+
+    ## Mesh object
+    mesh = MeshMat()
+    mesh.cell["triangle3"] = CellMat(nb_node_per_cell=3)
+    mesh.node = NodeMat()
+
+    mesh.node.add_node(np.array([0.33, 0]))
+    mesh.node.add_node(np.array([-1, 1]))
+    mesh.node.add_node(np.array([1, 1.22]))
+
+    nodes_test = np.array([0, 1, 2])
+    mesh.add_cell(nodes_test, "triangle3")
+
+    indice = [0]
+
+    ## Normal comp
+
+    dim = 2
+
+    for key in mesh.cell:
+
+
+        nb_node_per_cell = mesh.cell[
+            key
+        ].nb_node_per_cell  # Number of nodes per element
+
+        mesh_cell_key = mesh.cell[key]
+        connect = mesh.cell[key].get_connectivity()  # Each row of connect is an element
+        nb_elem = len(connect)
+
+        nb_node = mesh.node.nb_node  # Total nodes number
+
+
+        # Loop on element (elt)
+        for elt_indice, elt_number in enumerate(indice):
+
+            node_number = mesh_cell_key.get_connectivity(
+                elt_number
+            )  # elt nodes numbers, can differ from indices
+            vertice = mesh.get_vertice(elt_number)[key]  # elt nodes coordonates
+
+            # Triangle orientation, needed for normal orientation. 1 if trigo oriented, -1 otherwise
+            orientation_sign = np.sign(
+                np.cross(vertice[1] - vertice[0], vertice[2] - vertice[0])
+            )
+
+            for n in range(nb_node_per_cell):
+
+                edge_vector = (
+                    vertice[(n + 1) % nb_node_per_cell] - vertice[n % nb_node_per_cell]
+                )  # coordonées du vecteur nn+1
+
+
+                L = np.linalg.norm(edge_vector)
+                # Normalized normal vector n, that has to be directed outside the element (i.e. normal ^ edge same sign as the orientation)
+                normal_to_edge_unoriented = (
+                    np.array((edge_vector[1], -edge_vector[0])) / L
+                )
+                normal_to_edge = (
+                    normal_to_edge_unoriented
+                    * np.sign(np.cross(normal_to_edge_unoriented, edge_vector))
+                    * orientation_sign
+                )
+                normal_to_edge.reshape(dim, 1)
+
+                x_normal.append(normal_to_edge[0]+(vertice[n][0]+vertice[(n + 1) % nb_node_per_cell][0])/2)
+                x_nodes.append(vertice[n][0])
+                y_normal.append(normal_to_edge[1]+(vertice[n][1]+vertice[(n + 1) % nb_node_per_cell][1])/2)
+                y_nodes.append(vertice[n][1])
+                print(np.linalg.norm(normal_to_edge))
+                #plt.plot(edge_vector[0],edge_vector[1],'b')
+                #plt.plot(normal_to_edge[0],normal_to_edge[1],'r')
+    
+
+    lim = 4
+    plt.plot(x_normal,y_normal,'or')
+    plt.plot(x_nodes,y_nodes,'ob')
+    plt.plot([0],[0],'o',color='black')
+    plt.axis("square")
+
+    plt.xlim([-lim,lim])
+    plt.ylim([-lim,lim])
+    plt.show()
+
+
+
 if __name__ == "__main__":
 
-    test_element_loop()
+    test_comp_normal_to_edge()

pyleecan/Methods/Simulation/ForceTensor/comp_magnetrosctrictive_tensor.py
@@ -42,25 +42,27 @@ def comp_magnetrosctrictive_tensor(
     a22 = polynomial_coeffs[1][1]
     a24 = polynomial_coeffs[1][2]
 
-    M_norm = np.linalg.norm(M, axis=(0, 1))  # M matrices are in the first two axes
-    mu_times_Mnorm_squared = np.multiply(mu, M_norm) ** 2
+    mu_0 = 4*np.pi*1e-7
 
-    alpha1 = a10 + a12 * mu_times_Mnorm_squared + a14 * mu * mu_times_Mnorm_squared ** 2
-    alpha2 = a20 + a22 * mu_times_Mnorm_squared + a24 * mu * mu_times_Mnorm_squared ** 2
+    M_norm = np.linalg.norm(M, axis=(0, 1))  # M matrices are in the first two axes
+    mu_times_Mnorm_squared = np.multiply(mu_0, M_norm) ** 2
+
+    alpha1 = a10 + a12 * mu_times_Mnorm_squared + a14 * mu_times_Mnorm_squared ** 2
+    alpha2 = a20 + a22 * mu_times_Mnorm_squared + a24 * mu_times_Mnorm_squared ** 2
 
     magnetostric_tensor = np.zeros((2, 2, Nt_tot))
 
     # Iteration over time step
     for ti in range(Nt_tot):
-        mu_ti = mu[ti]
+        
 
         M_times_M = np.dot(M[:, :, ti], np.transpose(M[:, :, ti]))
 
         M_norm_squared = np.linalg.norm(M[:, :, ti]) ** 2
         I = np.eye(2, 2)
 
-        first_member = -alpha1[ti] * mu_ti * M_times_M
-        second_member = -alpha2[ti] * mu_ti * M_norm_squared * I
+        first_member = -alpha1[ti] * mu_0 * M_times_M
+        second_member = -alpha2[ti] * mu_0 * M_norm_squared * I
 
         magnetostric_tensor[:, :, ti] = first_member + second_member
 

pyleecan/Methods/Simulation/ForceTensor/element_loop.py
@@ -95,22 +95,26 @@ def element_loop(
                 np.cross(vertice[1] - vertice[0], vertice[2] - vertice[0])
             )
 
+            node_indice_set = set(())
+
             # Loop on edges
             for n in range(nb_node_per_cell):
 
                 # Get current node + next node indices (both needed since pression will be computed on edges because of Green Ostrogradski)
-                node_indice = np.where(mesh.node.indice == node_number[n])
+                node_indice = np.where(mesh.node.indice == node_number[n])[0][0]
 
                 next_node_indice = np.where(
                     mesh.node.indice == node_number[(n + 1) % nb_node_per_cell]
-                )
+                )[0][0]
+
+                node_indice_set.add(node_indice)
 
                 # Edge cooordonates
                 edge_vector = (
                     vertice[(n + 1) % nb_node_per_cell] - vertice[n % nb_node_per_cell]
                 )  # coordonées du vecteur nn+1
 
-                # Volume ratio (Green Ostrogradski), with a conventional 1/2 for a share between 2 nodes
+                # Volume ratio (Green Ostrogradski)
                 L = np.linalg.norm(edge_vector)
                 Ve0 = L / 2
 
@@ -131,8 +135,11 @@ def element_loop(
                 )  # [[1],[0]] means sum product over rows for normal (which is vertical) and over rows for tme
 
                 # Total edge force contribution, to be added to the 2 nodes that made the edge
-                fe = Ve0 * edge_force
+                fe = -Ve0 * edge_force / 2
                 f[node_indice, :, :] = f[node_indice, :, :] + fe
                 f[next_node_indice, :, :] = f[next_node_indice, :, :] + fe
+         
+
+        
 
     return f, connect