3aacab5f9eac8501b2d883ffc81ccb9277caf70f
Pierre Bonneel <pierre.bonneel@eomys.com>
2021-05-31 23:17:21 +0200

    [CC]

--- pyleecan/Methods/Machine/Machine/set_pole_pair_number.py
+++ pyleecan/Methods/Machine/Machine/set_pole_pair_number.py
@@ -9,17 +9,11 @@ def set_pole_pair_number(self, p):
         Pole pair number of the machine
     """
 
-    # Get list of laminations
-    lam_list = self.get_lam_list()
-
-    # Get pole pair number for all laminations
-    for i, lam in enumerate(lam_list):
-
-        if hasattr(lam, "set_pole_pair_number"):
-            # Set the pole pair number of lam
+    # Set pole pair number for all laminations
+    for lam in self.get_lam_list():
+        try:
             lam.set_pole_pair_number(p)
-
-        else:
+        except:
             raise Exception(
                 "ERROR, cannot enforce pole pair number to lamination " + str(i + 1)
             )
