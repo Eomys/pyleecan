525b6ea38bf05510cdab4e8ea2c6750dceb1ecbb
Pierre Bonneel <pierre.bonneel@eomys.com>
2021-08-02 17:16:20 +0200

    [CC] Test structure

--- Tests/Validation/Electrical/test_skin_effect.py
+++ Tests/Validation/Electrical/test_skin_effect.py
@@ -34,57 +34,60 @@ from pyleecan.definitions import DATA_DIR
 # @pytest.mark.IPMSM
 # @pytest.mark.periodicity
 # @pytest.mark.SingleOP
-# def test_skin_effect():
-"""Validation of the PMSM Electrical Equivalent Circuit with the Prius machine
-Compute Torque from EEC results and compare with Yang et al, 2013
-"""
+@pytest.skip(reason="Not finished yet")
+def test_skin_effect():
+    """Validation of the PMSM Electrical Equivalent Circuit with the Prius machine
+    Compute Torque from EEC results and compare with Yang et al, 2013
+    """
 
-Toyota_Prius = load(join(DATA_DIR, "Machine", "Toyota_Prius.json"))
-simu = Simu1(name="test_skin_effect", machine=Toyota_Prius)
+    Toyota_Prius = load(join(DATA_DIR, "Machine", "Toyota_Prius.json"))
+    simu = Simu1(name="test_skin_effect", machine=Toyota_Prius)
 
-# Definition of the input
-simu.input = InputElec(N0=2000, Nt_tot=10, Na_tot=2048)
-simu.input.set_Id_Iq(I0=250 / sqrt(2), Phi0=60 * pi / 180)
+    # Definition of the input
+    simu.input = InputElec(N0=2000, Nt_tot=10, Na_tot=2048)
+    simu.input.set_Id_Iq(I0=250 / sqrt(2), Phi0=60 * pi / 180)
 
-# # Define second simu for FEMM comparison
-# simu2 = simu.copy()
-# simu2.name = "test_EEC_PMSM_FEMM"
+    # # Define second simu for FEMM comparison
+    # simu2 = simu.copy()
+    # simu2.name = "test_EEC_PMSM_FEMM"
 
-# Definition of the electrical simulation (FEMM)
-simu.elec = Electrical(type_skin_effect=1)
-simu.elec.eec = EEC_PMSM(
-    indmag=IndMagFEMM(is_periodicity_a=True, Nt_tot=10),
-    fluxlink=FluxLinkFEMM(is_periodicity_a=True, Nt_tot=10),
-)
+    # Definition of the electrical simulation (FEMM)
+    simu.elec = Electrical(type_skin_effect=1)
+    simu.elec.eec = EEC_PMSM(
+        indmag=IndMagFEMM(is_periodicity_a=True, Nt_tot=10),
+        fluxlink=FluxLinkFEMM(is_periodicity_a=True, Nt_tot=10),
+    )
 
-simu.mag = None
-simu.force = None
-simu.struct = None
+    simu.mag = None
+    simu.force = None
+    simu.struct = None
 
-out = Output(simu=simu)
-#%%
-simu.run()
+    out = Output(simu=simu)
+    #%%
+    simu.run()
 
-# # Definition of the magnetic simulation (FEMM)
-# simu2.mag = MagFEMM(
-#     type_BH_stator=0,
-#     type_BH_rotor=0,
-#     is_periodicity_a=True,
-#     nb_worker=cpu_count(),
-# )
+    # # Definition of the magnetic simulation (FEMM)
+    # simu2.mag = MagFEMM(
+    #     type_BH_stator=0,
+    #     type_BH_rotor=0,
+    #     is_periodicity_a=True,
+    #     nb_worker=cpu_count(),
+    # )
 
-# out2 = Output(simu=simu2)
-# simu2.run()
+    # out2 = Output(simu=simu2)
+    # simu2.run()
 
-# Plot 3-phase current function of time
-# out.elec.get_Is().plot_2D_Data(
-#     "time",
-#     "phase",
-#     save_path=join(save_path, "EEC_FEMM_IPMSM_currents.png"),
-#     is_show_fig=False,
-#     **dict_2D
-# )
+    # Plot 3-phase current function of time
+    # out.elec.get_Is().plot_2D_Data(
+    #     "time",
+    #     "phase",
+    #     save_path=join(save_path, "EEC_FEMM_IPMSM_currents.png"),
+    #     is_show_fig=False,
+    #     **dict_2D
+    # )
 
-# from Yang et al, 2013
-# assert_almost_equal(out.elec.Tem_av_ref, 81.81, decimal=1)
-# assert_almost_equal(out2.mag.Tem_av, 81.70, decimal=1)
+    # from Yang et al, 2013
+    # assert_almost_equal(out.elec.Tem_av_ref, 81.81, decimal=1)
+    # assert_almost_equal(out2.mag.Tem_av, 81.70, decimal=1)
+if __name__ == "__main__":
+    test_skin_effect()
