4c384d6ae98419617af550ca2a7bd6a83ce9c2c8
Pierre Bonneel <pierre.bonneel@gmail.com>
2021-09-08 23:53:51 +0200

    [CC] Better error message for get_pole_pair_number

--- Tests/Methods/Machine/test_get_lam_methods.py
+++ Tests/Methods/Machine/test_get_lam_methods.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 import pytest
 
+from pyleecan.Classes.MachineSCIM import MachineSCIM
 from pyleecan.Classes.MachineUD import MachineUD
-from pyleecan.Classes.Lamination import Lamination
+from pyleecan.Classes.LamSlotWind import LamSlotWind
 
 # init some laminations and a machine
-rotor_0 = Lamination(Rint=0.1, Rext=0.2, is_stator=False)
-stator_0 = Lamination(Rint=0.3, Rext=0.4, is_stator=True)
-stator_1 = Lamination(Rint=0.5, Rext=0.6, is_stator=True)
-rotor_1 = Lamination(Rint=0.7, Rext=0.8, is_stator=False)
+rotor_0 = LamSlotWind(Rint=0.1, Rext=0.2, is_stator=False)
+stator_0 = LamSlotWind(Rint=0.3, Rext=0.4, is_stator=True)
+stator_1 = LamSlotWind(Rint=0.5, Rext=0.6, is_stator=True)
+rotor_1 = LamSlotWind(Rint=0.7, Rext=0.8, is_stator=False)
 
 machine = MachineUD()
 machine.lam_list = [rotor_1, stator_1, rotor_0, stator_0]
@@ -74,3 +75,36 @@ class Test_get_lam_methods(object):
         assert machine.get_lam_index("rotor") is None  # method is case sensitive
         assert machine.get_lam_index("xyz") is None
         assert machine.get_lam_index("Rotor_") is None  # this is also invalid
+
+    def test_get_pole_pair(self):
+        """Check that the pole pair is check and returned"""
+        mach = MachineSCIM()
+        mach.rotor.winding.p = 2
+        mach.rotor.is_stator = False
+        mach.stator.winding.p = 2
+        mach.stator.is_stator = True
+
+        # Correct check
+        assert mach.get_pole_pair_number() == 2
+
+        # Error two laminations
+        mach.stator.winding.p = 3
+        with pytest.raises(Exception) as e:
+            mach.get_pole_pair_number()
+        assert "ERROR, Stator has a different pole pair number than Rotor" == str(
+            e.value
+        )
+
+        # Error 4 laminations
+        stator_0.winding.p = 56
+        with pytest.raises(Exception) as e:
+            machine.get_pole_pair_number()
+        assert "ERROR, Stator-0 has a different pole pair number than Rotor-1" == str(
+            e.value
+        )
+
+
+if __name__ == "__main__":
+    a = Test_get_lam_methods()
+    a.test_get_pole_pair()
+    print("Done")
