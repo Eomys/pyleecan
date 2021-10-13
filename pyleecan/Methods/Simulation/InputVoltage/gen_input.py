07fed825543840bd2641a267b3a95449392cf4fd
helene-t <helene.toubin@eomys.com>
2021-10-13 15:52:20 +0200

    [CC] Us symbol

--- pyleecan/Methods/Simulation/InputVoltage/gen_input.py
+++ pyleecan/Methods/Simulation/InputVoltage/gen_input.py
@@ -139,7 +139,7 @@ def gen_input(self):
         )
         Uabc_data = DataTime(
             name="Stator voltage",
-            symbol="U_{abc}",
+            symbol="U_s",
             unit="V",
             axes=[Time, Phase],
             values=Uabc,
