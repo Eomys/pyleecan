8cf079797f334d9d287b1de877ae87ebf72457c0
helene-t <helene.toubin@eomys.com>
2022-03-01 16:12:21 +0100

    [CO] improve VarLoadCurrent methods

--- pyleecan/Methods/Simulation/VarLoadCurrent/get_input_list.py
+++ pyleecan/Methods/Simulation/VarLoadCurrent/get_input_list.py
@@ -1,7 +1,6 @@
+from ....Classes.InputVoltage import InputVoltage
 from ....Classes.Simulation import Simulation
-from ....Classes.InputCurrent import InputCurrent
 from ....Classes.OPdq import OPdq
-from ....Classes.OPslip import OPslip
 
 
 def get_input_list(self):
@@ -10,8 +9,10 @@ def get_input_list(self):
     # Check that the object has the correct type
     assert isinstance(self.parent, Simulation)
     ref_simu = self.parent
-    assert isinstance(ref_simu.input, InputCurrent)
-    N_simu = self.OP_matrix.shape[0]
+    assert isinstance(ref_simu.input, InputVoltage)
+
+    OP_matrix = self.get_OP_matrix()
+    N_simu = OP_matrix.shape[0]
 
     # Generate initial input_list
     ref_input = ref_simu.input.copy()
@@ -22,31 +23,22 @@ def get_input_list(self):
         Nt_tot = len(ref_input.time.get_data())
     else:
         Nt_tot = ref_input.Nt_tot
-    if ref_input.Nrev is None:
-        Nrev = 1
-    else:
-        Nrev = ref_input.Nrev
+    Nrev = ref_input.Nrev
     # Update OP according to OP_matrix
-    if self.parent.machine.is_synchronous():
-        OPclass = OPdq
-    else:
-        OPclass = OPslip
     for ii in range(N_simu):
-        input_list[ii].OP = OPclass()
-        input_list[ii].OP.N0 = self.OP_matrix[ii, 0]
+        input_list[ii].OP = OPdq(Ud_ref=ref_input.OP.Ud_ref, Uq_ref=ref_input.OP.Uq_ref)
+        input_list[ii].OP.N0 = OP_matrix[ii, 0]
         # Edit time vector
         input_list[ii].time = None
         input_list[ii].Nt_tot = Nt_tot
         input_list[ii].Nrev = Nrev
         if self.type_OP_matrix == 0:  # I0, Phi0
-            input_list[ii].set_Id_Iq(
-                I0=self.OP_matrix[ii, 1], Phi0=self.OP_matrix[ii, 2]
-            )
+            input_list[ii].set_Id_Iq(I0=OP_matrix[ii, 1], Phi0=OP_matrix[ii, 2])
         else:  # Id/Iq
-            input_list[ii].OP.Id_ref = self.OP_matrix[ii, 1]
-            input_list[ii].OP.Iq_ref = self.OP_matrix[ii, 2]
-        if self.is_torque:
-            input_list[ii].OP.Tem_av_ref = self.OP_matrix[ii, 3]
+            input_list[ii].OP.Id_ref = OP_matrix[ii, 1]
+            input_list[ii].OP.Iq_ref = OP_matrix[ii, 2]
+        if self.OP_matrix.shape[1] > 3:
+            input_list[ii].OP.Tem_av_ref = OP_matrix[ii, 3]
         if self.is_power and self.OP_matrix.shape[1] > 4:
             input_list[ii].OP.Pem_av_ref = self.OP_matrix[ii, 4]
 
