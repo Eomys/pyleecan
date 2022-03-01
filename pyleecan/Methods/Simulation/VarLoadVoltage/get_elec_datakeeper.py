7c98c39bd60555fba96d237e6c0f21a4ae09cc8b
helene-t <helene.toubin@eomys.com>
2022-03-01 16:14:07 +0100

    [CO] improve VarLoadVoltage methods

--- pyleecan/Methods/Simulation/VarLoadVoltage/get_elec_datakeeper.py
+++ pyleecan/Methods/Simulation/VarLoadVoltage/get_elec_datakeeper.py
@@ -1,69 +1,44 @@
-from numpy import abs as np_abs, angle, pi
+from numpy import isnan
 
-from ....Classes.DataKeeper import DataKeeper
-from ....Classes.VarLoad import VarLoad
+from ....Functions.Simulation.VarSimu.get_elec_datakeeper_dict import (
+    get_elec_datakeeper_dict,
+)
 
 
 def get_elec_datakeeper(self, symbol_list, is_multi=False):
-    """
-    Generate DataKeepers to store by default results from electric module
-
+    """Generate DataKeepers to store by default results from electric module
     Parameters
     ----------
-    self: VarLoadCurrent
-        A VarLoadCurrent object
+    self: VarLoadVoltage
+        A VarLoadVoltage object
     symbol_list : list
         List of the existing datakeeper (to avoid duplicate)
     is_multi : bool
         True for multi-simulation of multi-simulation
-
     Returns
     -------
     dk_list: list
         list of DataKeeper
     """
-    dk_list = VarLoad.get_elec_datakeeper(self, symbol_list)
-    if self.type_OP_matrix == 0:  # I0 and Phi0
-        if not is_multi and "I0" not in symbol_list:
-            # Save I0
-            dk_list.append(
-                DataKeeper(
-                    name="I0",
-                    symbol="I0",
-                    unit="A",
-                    keeper="lambda output: np.abs(output.elec.Id_ref + 1j * output.elec.Iq_ref)",
-                )
-            )
-        # Save Phi0
-        if not is_multi and "Phi0" not in symbol_list:
-            dk_list.append(
-                DataKeeper(
-                    name="Phi0",
-                    symbol="Phi0",
-                    unit="",
-                    keeper="lambda output: np.angle(output.elec.Id_ref + 1j * output.elec.Iq_ref) % (2 * np.pi)",
-                )
-            )
-    # Keep torque
-    if not is_multi and self.OP_matrix.shape[1] > 3 and "Tem_av_ref" not in symbol_list:
-        dk_list.append(
-            DataKeeper(
-                name="Reference Average Torque",
-                symbol="Tem_av_ref",
-                unit="N.m",
-                keeper="lambda output: output.elec.Tem_av_ref",
-            )
-        )
 
-    # TODO Save power
-    # if self.is_power:
-    #     datakeeper_list.append(
-    #         DataKeeper(
-    #             name="Power",
-    #             symbol="Tem",
-    #             unit="N.m",
-    #             keeper="lambda output: output.simu.mag.power",
-    #         ),
-    #     )
+    dk_dict = get_elec_datakeeper_dict()
+    dk_list = []
+
+    if not is_multi:
+        if self.type_OP_matrix == 0:  # U0 and Phi0
+            quantity_list = ["U0", "UPhi0", "Ud", "Uq"]
+        elif self.type_OP_matrix == 1:  # Id and Iq
+            quantity_list = ["Ud", "Uq"]
+        elif self.type_OP_matrix == 2:  # Id and Iq
+            quantity_list = ["U0", "Ud", "Uq", "slip"]
+
+        if self.OP_matrix.shape[1] > 3 and not isnan(self.OP_matrix[0, 3]):
+            quantity_list.append("Tem_av_ref")
+
+    # Save Id
+    if not is_multi:
+        for key in quantity_list:
+            if key not in symbol_list:
+                dk_list.append(dk_dict[key])
 
     return dk_list
