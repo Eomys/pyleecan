8ea55f7283d140b501cd51d3417654b95eb25a99
helene-t <helene.toubin@eomys.com>
2022-03-02 09:09:33 +0100

    [CO] OPdq initialization

--- pyleecan/Methods/Simulation/InputCurrent/set_OP_from_array.py
+++ pyleecan/Methods/Simulation/InputCurrent/set_OP_from_array.py
@@ -5,7 +5,6 @@ from ....Classes.OPdq import OPdq
 
 def set_OP_from_array(self, OP_matrix, type_OP_matrix=1, index=0):
     """Extract the Operating Point from an OP_matrix
-
     Parameters
     ----------
     self : InputCurrent
@@ -24,14 +23,13 @@ def set_OP_from_array(self, OP_matrix, type_OP_matrix=1, index=0):
     assert index < OP_matrix.shape[0]
     assert type_OP_matrix in [0, 1]
 
+    if self.OP is None:
+        self.OP = OPdq()
+
     if type_OP_matrix == 1:
-        if self.OP is None:
-            self.OP = OPdq()
         self.OP.Id_ref = OP_matrix[index, 1]
         self.OP.Iq_ref = OP_matrix[index, 2]
     else:
-        if self.OP is None:
-            self.OP = OPdq()
         self.set_Id_Iq(I0=OP_matrix[index, 1], Phi0=OP_matrix[index, 2])
     self.OP.N0 = OP_matrix[index, 0]
     if OP_matrix.shape[1] > 3:
