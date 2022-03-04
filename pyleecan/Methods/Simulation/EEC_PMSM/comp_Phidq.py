f6ee9fd32620abfc903f24f8bcd923587771f786
EmileDvs <58598539+EmileDvs@users.noreply.github.com>
2022-03-04 11:27:59 +0100

    [BC] Correct PWM workflow

--- pyleecan/Methods/Simulation/EEC_PMSM/comp_Phidq.py
+++ pyleecan/Methods/Simulation/EEC_PMSM/comp_Phidq.py
@@ -7,16 +7,30 @@ def comp_Phidq(self):
         an EEC_PMSM object
 
     """
+    Phid, Phiq = None, None
+    Idq_dict = self.OP.get_Id_Iq()
 
-    Phi_dqh_mean = self.indmag.comp_inductance(
-        machine=self.get_machine_from_parent(), OP_ref=self.OP
-    )
+    if Idq_dict["Id"] is not None and Idq_dict["Iq"] is not None:
 
-    Phid = Phi_dqh_mean[0]  # * self.Xke_skinS
-    Phiq = Phi_dqh_mean[1]  # * self.Xke_skinS
-    if Phid.size == 1:
-        Phid = float(Phid)
-        Phiq = float(Phiq)
+        if self.Ld is not None and self.Phid_mag is not None:
+            # Calculate Phid from available data
+            Phid = self.Ld * Idq_dict["Id"] + self.Phid_mag
+
+        if self.Lq is not None and self.Phiq_mag is not None:
+            # Calculate Phiq from available data
+            Phiq = self.Lq * Idq_dict["Iq"] + self.Phiq_mag
+
+        if Phid is None or Phiq is None:
+            # Calculate Phid and Phiq for the given Id/Iq
+            Phi_dqh_mean = self.indmag.comp_inductance(
+                machine=self.get_machine_from_parent(), OP_ref=self.OP
+            )
+
+        if Phid is None:
+            Phid = Phi_dqh_mean[0]  # * self.Xke_skinS
+
+        if Phiq is None:
+            Phiq = Phi_dqh_mean[1]  # * self.Xke_skinS
 
     self.Phid = Phid
     self.Phiq = Phiq
