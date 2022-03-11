e9d54cee5da7c52c9e6b5c95189c343bdf4dadf9
BaptisteMadery14 <baptiste.madery@eomys.com>
2022-03-11 16:17:31 +0100

    [NF] Disabling comp_surface_opening in comp_surface if slot.type_close == 2 (=> better computation of the mass)

--- pyleecan/Methods/Slot/SlotW29/comp_surface.py
+++ pyleecan/Methods/Slot/SlotW29/comp_surface.py
@@ -16,4 +16,7 @@ def comp_surface(self):
         Slot total surface [m**2]
 
     """
-    return self.comp_surface_active() + self.comp_surface_opening()
+    if self.type_close == 1:
+        return self.comp_surface_active() + self.comp_surface_opening()
+    elif self.type_close == 2:
+        return self.comp_surface_active()
