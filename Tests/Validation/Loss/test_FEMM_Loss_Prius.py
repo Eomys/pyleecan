42e0ae0acc01ef2ea8e810e498743e10ccf8c0c3
EmileDvs <58598539+EmileDvs@users.noreply.github.com>
2022-03-14 16:14:13 +0100

    [WiP] Validation case for loss calculation

--- Tests/Validation/Loss/test_FEMM_Loss_Prius.py
+++ Tests/Validation/Loss/test_FEMM_Loss_Prius.py
@@ -15,6 +15,85 @@ from pyleecan.definitions import DATA_DIR
 is_show_fig = False
 
 
+def test_FEMM_Loss_SPMSM():
+    """Test to calculate losses in SPMSM using LossFEMM model"""
+
+    machine = load(join(DATA_DIR, "Machine", "SPMSM_18s16p_loss.json"))
+
+    # machine.plot()
+
+    # machine.comp_periodicity_spatial()
+
+    Ch = 143  # hysteresis loss coefficient [W/(m^3*T^2*Hz)]
+    Ce = 0.530  # eddy current loss coefficients [W/(m^3*T^2*Hz^2)]
+    Cprox = 1  # sigma_w * cond.Hwire * cond.Wwire
+
+    simu = Simu1(name="test_FEMM_Loss_Prius", machine=machine)
+
+    simu.input = InputCurrent(
+        Nt_tot=20 * 16,
+        Na_tot=1000 * 2,
+        OP=OPdq(N0=4000, Id_ref=0, Iq_ref=2),
+        is_periodicity_t=True,
+        is_periodicity_a=True,
+    )
+
+    simu.mag = MagFEMM(
+        is_periodicity_a=True,
+        is_periodicity_t=True,
+        nb_worker=4,
+        is_get_meshsolution=True,
+    )
+
+    # simu.loss = LossFEMM(Ce=Ce, Cp=Cprox, Ch=Ch, is_get_meshsolution=True, Tsta=100)
+
+    out = simu.run()
+
+    freqs = out.loss.axes_dict["freqs"].get_values()
+
+    assert_almost_equal(
+        out.loss.Pstator, out.loss.get_loss_group("stator core", freqs)[0]
+    )
+    assert_almost_equal(
+        out.loss.Protor, out.loss.get_loss_group("rotor core", freqs)[0]
+    )
+    assert_almost_equal(
+        out.loss.Pprox, out.loss.get_loss_group("stator winding", freqs)[0]
+    )
+    assert_almost_equal(
+        out.loss.Pmagnet, out.loss.get_loss_group("rotor magnets", freqs)[0]
+    )
+
+    ovl_loss = out.loss.get_loss_overall()
+
+    if is_show_fig:
+        out.loss.meshsolution.plot_contour(
+            "freqs=sum",
+            label="Loss",
+            group_names=[
+                "stator core",
+                "stator winding",
+                "rotor core",
+                "rotor magnets",
+            ],
+            # clim=[2e4, 2e7],
+        )
+
+    # out.loss.meshsolution.plot_contour(
+    #     "freqs=sum",
+    #     label="Loss",
+    #     group_names=["stator core", "stator winding"],
+    #     # clim=[2e4, 2e7],
+    # )
+
+    # out.loss.meshsolution.plot_contour(
+    #     "freqs=sum",
+    #     label="Loss",
+    #     group_names=["rotor core", "rotor magnets"],
+    #     # clim=[2e4, 2e7],
+    # )
+
+
 def test_FEMM_Loss_Prius():
     """Test to calculate losses in Toyota_Prius using LossFEMM model"""
 
@@ -92,4 +171,7 @@ def test_FEMM_Loss_Prius():
 
 # To run it without pytest
 if __name__ == "__main__":
-    out = test_FEMM_Loss_Prius()
+
+    out = test_FEMM_Loss_SPMSM()
+
+    # out = test_FEMM_Loss_Prius()
