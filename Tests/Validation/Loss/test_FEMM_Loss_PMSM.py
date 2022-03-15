[BC] Correct loss calculation [CO] Return magnetic vector potential on elements to ease magnet loss calculation

Tests/Validation/Loss/test_FEMM_Loss_PMSM.py
@@ -1,5 +1,6 @@
 from os.path import join
 
+import numpy as np
 from numpy.testing import assert_almost_equal
 
 from pyleecan.Classes.InputCurrent import InputCurrent
@@ -27,9 +28,9 @@ def test_FEMM_Loss_SPMSM():
     simu = Simu1(name="test_FEMM_Loss_SPMSM", machine=machine)
 
     simu.input = InputCurrent(
-        Nt_tot=4 * 16,
+        Nt_tot=8 * 16,
         Na_tot=1000 * 2,
-        OP=OPdq(N0=4000, Id_ref=0, Iq_ref=2),
+        OP=OPdq(N0=4000, Id_ref=0, Iq_ref=np.sqrt(2)),
         is_periodicity_t=True,
         is_periodicity_a=True,
     )
@@ -39,9 +40,13 @@ def test_FEMM_Loss_SPMSM():
         is_periodicity_t=True,
         nb_worker=4,
         is_get_meshsolution=True,
+        FEMM_dict_enforced={
+            "mesh": {"meshsize_airgap": 0.00014, "elementsize_airgap": 0.00014}
+        },
+        is_close_femm=False,
     )
 
-    simu.loss = LossFEMM(Ce=Ce, Cp=Cprox, Ch=Ch, is_get_meshsolution=True, Tsta=100)
+    simu.loss = LossFEMM(Ce=Ce, Cp=Cprox, Ch=Ch, is_get_meshsolution=True, Tsta=120)
 
     out = simu.run()
 
@@ -102,7 +107,7 @@ def test_FEMM_Loss_Prius():
     simu = Simu1(name="test_FEMM_Loss_Prius", machine=machine)
 
     simu.input = InputCurrent(
-        Nt_tot=20 * 8,
+        Nt_tot=80 * 8,
         Na_tot=200 * 8,
         OP=OPdq(N0=1000, Id_ref=-100, Iq_ref=200),
         is_periodicity_t=True,
@@ -168,6 +173,6 @@ def test_FEMM_Loss_Prius():
 # To run it without pytest
 if __name__ == "__main__":
 
-    out = test_FEMM_Loss_SPMSM()
+    # out = test_FEMM_Loss_SPMSM()
 
-    # out = test_FEMM_Loss_Prius()
+    out = test_FEMM_Loss_Prius()