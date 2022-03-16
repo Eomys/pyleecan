8e251bb3c5bd9bbb1c1159c37e97c9ab3ba98f44
BaptisteMadery14 <baptiste.madery@eomys.com>
2022-03-16 16:45:02 +0100

    [BC] : Fixing "if else" in comp_surface for SlotW classes (depending on slot.type_close)

--- pyleecan/Methods/Slot/SlotW26/comp_surface.py
+++ pyleecan/Methods/Slot/SlotW26/comp_surface.py
@@ -18,7 +18,7 @@ def comp_surface(self):
         Slot total surface [m**2]
 
     """
-    if self.type_close == 1:
-        return self.comp_surface_active() + self.comp_surface_opening()
-    elif self.type_close == 2:
+    if self.type_close == 2:
         return self.comp_surface_active()
+    else:
+        return self.comp_surface_active() + self.comp_surface_opening()
