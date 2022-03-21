5161818261f479bb4f82854757f9f8bcc90547be
EmileDvs <58598539+EmileDvs@users.noreply.github.com>
2022-03-21 18:50:18 +0100

    [WiP] Rework for skin effect calculation

---
+++ pyleecan/Methods/Machine/CondType11/comp_width_wire.py
@@ -0,0 +1,16 @@
+def comp_width_wire(self):
+    """Return wire width
+
+    Parameters
+    ----------
+    self : CondType11
+        A CondType11 object
+
+    Returns
+    -------
+    W: float
+        Width of the wire [m]
+
+    """
+
+    return self.Wwire
