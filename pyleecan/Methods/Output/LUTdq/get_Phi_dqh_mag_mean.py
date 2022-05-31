dc749f46de802508b8062b2ad3f9c39244d2c8d5
EmileDvs <58598539+EmileDvs@users.noreply.github.com>
2022-05-31 18:29:07 +0200

    [CC] Various code cleaning [CO] Store calcualte eec parameters in LUTdq [BC] Make comp_skin_effect_inductance robust to freq=0

--- pyleecan/Methods/Output/LUTdq/get_Phi_dqh_mag_mean.py
+++ pyleecan/Methods/Output/LUTdq/get_Phi_dqh_mag_mean.py
@@ -1,3 +1,9 @@
+from numpy import array
+
+from ....Classes.EEC_PMSM import EEC_PMSM
+from ....Classes.Electrical import Electrical
+
+
 def get_Phi_dqh_mag_mean(self):
     """Get the mean magnets flux linkage in DQH frame
 
@@ -12,15 +18,30 @@ def get_Phi_dqh_mag_mean(self):
         mean magnets flux linkage in dqh frame (3,)
     """
 
-    # Get stator winding flux due to magnets
-    Phi_dqh_mag = self.get_Phi_dqh_mag()
+    eec = self.get_eec()
 
-    if Phi_dqh_mag is not None:
-        # Get mean value
-        Phi_dqh_mag_mean = Phi_dqh_mag.get_along("time=mean", "phase")[
-            Phi_dqh_mag.symbol
-        ]
+    if eec is not None and eec.Phid_mag is not None and eec.Phiq_mag is not None:
+        Phi_dqh_mag_mean = array([eec.Phid_mag, eec.Phiq_mag, 0])
     else:
-        Phi_dqh_mag_mean = None
+
+        # Get stator winding flux due to magnets
+        Phi_dqh_mag = self.get_Phi_dqh_mag()
+
+        if Phi_dqh_mag is not None:
+            # Get mean value
+            Phi_dqh_mag_mean = Phi_dqh_mag.get_along("time=mean", "phase")[
+                Phi_dqh_mag.symbol
+            ]
+            # Store in eec for further use
+            if eec is None:
+                eec = EEC_PMSM()
+                if self.simu.elec is None:
+                    self.simu.elec = Electrical()
+                self.simu.elec.eec = eec
+            eec.Phid_mag = float(Phi_dqh_mag_mean[0])
+            eec.Phiq_mag = float(Phi_dqh_mag_mean[1])
+
+        else:
+            Phi_dqh_mag_mean = None
 
     return Phi_dqh_mag_mean
--- pyleecan/Methods/Output/LUTdq/get_Phi_dqh_mean.py
+++ pyleecan/Methods/Output/LUTdq/get_Phi_dqh_mean.py
@@ -1,4 +1,6 @@
-from numpy import zeros
+from numpy import zeros, array
+
+from ....Classes.EEC_PMSM import EEC_PMSM
 
 from ....Functions.Electrical.dqh_transformation import n2dqh_DataTime
 
@@ -28,23 +30,37 @@ def get_Phi_dqh_mean(self):
         phase_dir = self.output_list[0].elec.phase_dir
 
         for ii in range(N_OP):
-            # Integrate stator winding flux contained in LUT over z
-            Phi_wind = (
-                self.output_list[ii]
-                .mag.Phi_wind_slice[stator_label]
-                .get_data_along("time", "phase", "z=integrate")
-            )
+            eec_i = self.output_list[ii].elec.eec
 
-            # dqh transform
-            Phi_dqh = n2dqh_DataTime(
-                Phi_wind,
-                is_dqh_rms=True,
-                phase_dir=phase_dir,
-            )
-            # mean over time axis
-            Phi_dqh_mean[ii, :] = Phi_dqh.get_along("time=mean", "phase")[
-                Phi_dqh.symbol
-            ]
+            if eec_i is not None and eec_i.Phid is not None and eec_i.Phiq is not None:
+                # Get values from eec
+                Phi_dqh_mean[ii, :] = array([eec_i.Phid, eec_i.Phiq, 0])
+
+            else:
+                # Integrate stator winding flux contained in LUT over z
+                Phi_wind = (
+                    self.output_list[ii]
+                    .mag.Phi_wind_slice[stator_label]
+                    .get_data_along("time", "phase", "z=integrate")
+                )
+
+                # dqh transform
+                Phi_dqh = n2dqh_DataTime(
+                    Phi_wind,
+                    is_dqh_rms=True,
+                    phase_dir=phase_dir,
+                )
+                # mean over time axis
+                Phi_dqh_mean[ii, :] = Phi_dqh.get_along("time=mean", "phase")[
+                    Phi_dqh.symbol
+                ]
+
+                # Store values in eec for further use
+                if eec_i is None:
+                    eec_i = EEC_PMSM()
+                    self.output_list[ii].elec.eec = eec_i
+                eec_i.Phid = float(Phi_dqh_mean[ii, 0])
+                eec_i.Phiq = float(Phi_dqh_mean[ii, 1])
 
         # Store for next call
         self.Phi_dqh_mean = Phi_dqh_mean
