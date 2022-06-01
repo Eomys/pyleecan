6420ffce486ea97b134f45156c2646949ce2f077
Pierre Bonneel <pierre.bonneel@gmail.com>
2022-06-01 10:27:53 +0200

    [WIP] Adding widgets for Notches

---
+++ pyleecan/GUI/Dialog/DMachineSetup/DNotch/DNotchTab.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+
+
+from PySide2.QtCore import Signal
+from PySide2.QtWidgets import QMessageBox, QWidget, QSizePolicy
+from logging import getLogger
+from .....loggers import GUI_LOG_NAME
+from .....Classes.HoleM50 import HoleM50
+from .....Classes.Material import Material
+from .....GUI.Dialog.DMachineSetup.SMHoleMag.Ui_SMHoleMag import Ui_SMHoleMag
+from .....GUI.Dialog.DMachineSetup.SMHoleMag.WHoleMag.WHoleMag import WHoleMag
+from .....Methods.Slot.Slot import SlotCheckError
+from .....Functions.Plot.set_plot_gui_icon import set_plot_gui_icon
+
+
+class SMHoleMag(Ui_SMHoleMag, QWidget):
+    """Step to set several Holes"""
+
+    # Signal to DMachineSetup to know that the save popup is needed
+    saveNeeded = Signal()
+    # Information for DMachineSetup
+    step_name = "Hole"
+
+    def __init__(self, machine, material_dict, is_stator=False):
+        """Initialize the widget according to machine
+
+        Parameters
+        ----------
+        self : SMHoleMag
+            A SMHoleMag widget
+        machine : Machine
+            current machine to edit
+        material_dict: dict
+            Materials dictionary (library + machine)
+        is_stator : bool
+            To adapt the GUI to set either the stator or the rotor
+        """
+        # Build the interface according to the .ui file
+        QWidget.__init__(self)
+        self.setupUi(self)
+
+        # Saving arguments
+        self.machine = machine
+        self.material_dict = material_dict
+        self.is_stator = is_stator
+
+        # Get the correct object to set
+        if self.is_stator:
+            self.obj = machine.stator
+        else:
+            self.obj = machine.rotor
+        if self.obj.hole is None:
+            self.obj.hole = list()
+
+        # If the hole is not set, initialize it with a HoleM50
+        if len(self.obj.hole) == 0:
+            self.obj.hole.append(HoleM50())
+            if self.machine.type_machine == 5:  # SyRM
+                self.obj.hole[0].remove_magnet()
+            self.obj.hole[0]._set_None()
+            self.obj.hole[0].Zh = machine.stator.winding.p * 2  # Default value
+        self.set_hole_pitch(self.obj.hole[0].Zh)
+
+        # Update all the hole tab
+        # (the current hole types will be initialized)
+        # print(type(self.obj.hole[0]).__name__)
+        self.tab_hole.clear()
+        for hole in self.obj.hole:
+            self.s_add(hole)
+        self.tab_hole.setCurrentIndex(0)
+
+        # Set Help URL
+        self.b_help.hide()
+
+        # Connect the slot
+        self.b_add.clicked.connect(self.s_add)
+        self.b_remove.clicked.connect(self.s_remove)
+
+        self.b_plot.clicked.connect(self.s_plot)
+
+    def emit_save(self):
+        """Send a saveNeeded signal to the DMachineSetup"""
+        self.saveNeeded.emit()
+
+    def set_hole_pitch(self, Zh):
+        """Update out_hole_pitch with the correct value
+
+        Parameters
+        ----------
+        self : SMHoleMag
+            A SMHoleMag object
+        Zh : int
+            The current value of Zh
+        """
+        Zh_txt = self.tr("Slot pitch = 360 / 2p = ")
+        if Zh in [None, 0]:
+            self.out_hole_pitch.setText(Zh_txt + "?")
+        else:
+            hole_pitch = 360.0 / Zh
+            self.out_hole_pitch.setText(Zh_txt + "%.4g" % (hole_pitch) + " °")
+
+    def s_add(self, hole=False):
+        """Signal to add a new hole
+
+        Parameters
+        ----------
+        self : SMHoleMag
+            a SMHoleMag object
+        hole : HoleMag
+            hole to initialize in the new page
+            if None create a new empty HoleM50
+        """
+        # Adapt the GUI according to the machine type
+        if self.machine.type_machine == 5:  # SyRM
+            is_mag = False
+        else:
+            is_mag = True
+        # Create a new hole if needed
+        if type(hole) is bool:
+            self.obj.hole.append(HoleM50())
+            hole = self.obj.hole[-1]
+            hole._set_None()
+            hole_index = len(self.obj.hole) - 1
+            if self.machine.type_machine == 5:
+                hole.remove_magnet()
+            else:
+                hole.set_magnet_by_id(0, self.obj.hole[0].get_magnet_by_id(0))
+            hole.Zh = self.obj.hole[0].Zh
+        else:
+            hole_index = self.obj.hole.index(hole)
+        tab = WHoleMag(
+            self, is_mag=is_mag, index=hole_index, material_dict=self.material_dict
+        )
+        tab.saveNeeded.connect(self.emit_save)
+        self.tab_hole.addTab(tab, "Hole " + str(hole_index + 1))
+
+    def s_remove(self):
+        """Signal to remove the last hole
+
+        Parameters
+        ----------
+        self : SMHoleMag
+            a SMHoleMag object
+        """
+        if len(self.obj.hole) > 1:
+            self.tab_hole.removeTab(len(self.obj.hole) - 1)
+            self.obj.hole.pop(-1)
+
+    def s_plot(self):
+        """Try to plot the lamination
+
+        Parameters
+        ----------
+        self : SMHoleMag
+            a SMHoleMag object
+        """
+        # Update p
+        for hole in self.obj.hole:
+            hole.Zh = self.machine.stator.winding.p * 2
+        self.set_hole_pitch(self.obj.hole[0].Zh)
+
+        # We have to make sure the hole is right before trying to plot it
+        error = self.check(self.obj)
+        if error:  # Error => Display it
+            err_msg = "Error in Hole definition:\n" + error
+            getLogger(GUI_LOG_NAME).debug(err_msg)
+            QMessageBox().critical(self, self.tr("Error"), err_msg)
+        else:  # No error => Plot the lamination
+            try:
+                self.obj.plot(is_show_fig=True)
+                set_plot_gui_icon()
+            except Exception as e:
+                err_msg = "Error while plotting Lamination in Hole definition:\n" + str(
+                    e
+                )
+                getLogger(GUI_LOG_NAME).error(err_msg)
+                QMessageBox().critical(self, self.tr("Error"), err_msg)
+
+    @staticmethod
+    def check(lamination):
+        """Check that the current machine have all the needed field set
+
+        Parameters
+        ----------
+        lamination : Lamination
+            Lamination to check
+
+        Returns
+        -------
+        error : str
+            Error message (return None if no error)
+        """
+
+        # Check that everything is set
+        for hole in lamination.hole:
+            try:
+                hole.check()
+            except SlotCheckError as error:
+                return str(error)
---
+++ pyleecan/GUI/Dialog/DMachineSetup/DNotch/WNotch/WNotch.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+
+
+from PySide2.QtCore import Signal
+from PySide2.QtWidgets import QMessageBox, QWidget
+
+from ......GUI.Dialog.DMachineSetup.SMHoleMag.PHoleM50.PHoleM50 import PHoleM50
+from ......GUI.Dialog.DMachineSetup.SMHoleMag.PHoleM51.PHoleM51 import PHoleM51
+from ......GUI.Dialog.DMachineSetup.SMHoleMag.PHoleM52.PHoleM52 import PHoleM52
+from ......GUI.Dialog.DMachineSetup.SMHoleMag.PHoleM53.PHoleM53 import PHoleM53
+from ......GUI.Dialog.DMachineSetup.SMHoleMag.PHoleM54.PHoleM54 import PHoleM54
+from ......GUI.Dialog.DMachineSetup.SMHoleMag.PHoleM57.PHoleM57 import PHoleM57
+from ......GUI.Dialog.DMachineSetup.SMHoleMag.PHoleM58.PHoleM58 import PHoleM58
+from ......GUI.Dialog.DMachineSetup.SMHoleMag.PHoleMUD.PHoleMUD import PHoleMUD
+from ......GUI.Dialog.DMachineSetup.SMHoleMag.WHoleMag.Ui_WHoleMag import Ui_WHoleMag
+
+
+class WHoleMag(Ui_WHoleMag, QWidget):
+    """Widget to Setup a single Hole in a list"""
+
+    # Signal to DMachineSetup to know that the save popup is needed
+    saveNeeded = Signal()
+
+    def __init__(self, parent, is_mag, index, material_dict):
+        """Initialize the GUI according to lamination
+
+        Parameters
+        ----------
+        self : WHoleMag
+            A WHoleMag object
+        parent :
+            A parent object containing the lamination (LamHole) to edit
+        is_mag : bool
+            False: no magnet in the Hole (for the SyRM)
+        index : int
+            Index of the hole to edit
+        material_dict: dict
+            Materials dictionary (library + machine)
+        """
+
+        # Build the interface according to the .ui file
+        QWidget.__init__(self)
+        self.setupUi(self)
+
+        self.is_stator = False
+        self.obj = parent.obj
+        self.index = index
+        self.is_mag = is_mag
+        self.parent = parent
+        self.material_dict = material_dict
+
+        # Adapt the GUI to the current machine
+        if is_mag:  # IPMSM
+            self.wid_list = [
+                PHoleM50,
+                PHoleM51,
+                PHoleM52,
+                PHoleM53,
+                PHoleM57,
+                PHoleM58,
+                PHoleMUD,
+            ]
+        else:  # SyRM
+            self.wid_list = [
+                PHoleM50,
+                PHoleM51,
+                PHoleM52,
+                PHoleM53,
+                PHoleM54,
+                PHoleM57,
+                PHoleM58,
+                PHoleMUD,
+            ]
+        self.type_list = [wid.hole_type for wid in self.wid_list]
+        self.name_list = [wid.hole_name for wid in self.wid_list]
+
+        # Avoid erase all the parameters when navigating though the holes
+        self.previous_hole = dict()
+        for hole_type in self.type_list:
+            self.previous_hole[hole_type] = None
+
+        # Fill the combobox with the available hole
+        self.c_hole_type.clear()
+        for hole in self.name_list:
+            self.c_hole_type.addItem(hole)
+        self.c_hole_type.setCurrentIndex(
+            self.type_list.index(type(self.obj.hole[index]))
+        )
+
+        # Regenerate the pages with the new values
+        self.w_hole.setParent(None)
+        self.w_hole = self.wid_list[self.c_hole_type.currentIndex()](
+            hole=self.obj.hole[index], material_dict=self.material_dict
+        )
+        # Refresh the GUI
+        self.main_layout.removeWidget(self.w_hole)
+        self.main_layout.insertWidget(1, self.w_hole)
+
+        # Connect the slot
+        self.c_hole_type.currentIndexChanged.connect(self.set_hole_type)
+
+    def emit_save(self):
+        """Send a saveNeeded signal to the DMachineSetup"""
+        self.saveNeeded.emit()
+
+    def set_hole_type(self, c_index):
+        """Initialize self.obj with the hole corresponding to index
+
+        Parameters
+        ----------
+        self : WHoleMag
+            A WHoleMag object
+        c_index : int
+            Index of the selected hole type in the combobox
+        """
+
+        # Save the hole
+        hole = self.obj.hole[self.index]
+        self.previous_hole[type(hole)] = hole
+
+        # Call the corresponding constructor
+        Zh = hole.Zh
+        if self.previous_hole[self.type_list[c_index]] is None:
+            # No previous hole of this type
+            self.obj.hole[self.index] = self.type_list[c_index]()
+            self.obj.hole[self.index]._set_None()  # No default value
+            self.obj.hole[self.index].Zh = Zh
+            if self.is_mag and self.obj.hole[self.index].has_magnet():  # IPMSM
+                magnet = hole.get_magnet_by_id(0)
+                self.obj.hole[self.index].set_magnet_by_id(0, magnet)
+            elif self.obj.hole[self.index].has_magnet():  # SyRM
+                self.obj.hole[self.index].remove_magnet()
+        else:  # Load the previous hole of this type
+            self.obj.hole[self.index] = self.previous_hole[self.type_list[c_index]]
+
+        # Update the GUI
+        self.w_hole.setParent(None)
+        self.w_hole = self.wid_list[c_index](
+            hole=self.obj.hole[self.index], material_dict=self.material_dict
+        )
+        self.w_hole.saveNeeded.connect(self.emit_save)
+        # Refresh the GUI
+        self.main_layout.removeWidget(self.w_hole)
+        self.main_layout.insertWidget(1, self.w_hole)
+
+        # Notify the machine GUI that the machine has changed
+        self.saveNeeded.emit()
+
+    def check(self):
+        """Check that the current machine have all the needed field set
+
+        Parameters
+        ----------
+        self : WHoleMag
+            A WHoleMag widget
+
+        Returns
+        -------
+        error : str
+            Error message (return None if no error)
+        """
+
+        return self.w_hole.check()
