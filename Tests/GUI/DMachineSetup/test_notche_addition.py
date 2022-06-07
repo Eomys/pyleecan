a390ee4b30a901eb3573f662355de79fb3da2c6f
BaptisteMadery14 <baptiste.madery@eomys.com>
2022-06-07 10:49:35 +0200

    [NF] Finishing test_notch_addition by simulating the addition of a notch on the rotor + checking that the machine is correctly saved. Fixing bug where we would load Test\Data\Material, instead loading pyleecan\Data\Material

--- Tests/GUI/DMachineSetup/test_notche_addition.py
+++ Tests/GUI/DMachineSetup/test_notche_addition.py
@@ -1,19 +1,25 @@
 from PySide2 import QtWidgets
 from os.path import join, isfile
+from os import remove
 import mock
 import sys
 from numpy import pi
+from numpy.testing import assert_almost_equal
+
+from pyleecan.definitions import DATA_DIR
+from pyleecan.Functions.load import load_matlib
 from pyleecan.GUI.Dialog.DMachineSetup.DMachineSetup import DMachineSetup
 from pyleecan.GUI.Dialog.DMachineSetup.DNotchTab.DNotchTab import DNotchTab
 from pyleecan.GUI.Dialog.DMachineSetup.DNotchTab.WNotch.WNotch import WNotch
 from pyleecan.GUI.Dialog.DMachineSetup.SMSlot.PMSlot10.PMSlot10 import PMSlot10
+from pyleecan.GUI.Dialog.DMachineSetup.SMSlot.PMSlot11.PMSlot11 import PMSlot11
 from pyleecan.GUI.Dialog.DMachineSetup.SMSlot.WSlotCirc.WSlotCirc import WSlotCirc
-from pyleecan.Functions.load import load_matlib
+from pyleecan.GUI.Dialog.DMachineSetup.SMachineType.SMachineType import SMachineType
 from pyleecan.GUI.Dialog.DMachineSetup.SLamShape.SLamShape import SLamShape
-from pyleecan.definitions import DATA_DIR
-from Tests import TEST_DATA_DIR
+from Tests import save_gui_path as save_path
 
-matlib_path = join(TEST_DATA_DIR, "Material")
+matlib_path = join(DATA_DIR, "Material")
+machine_name = "Toyota_Prius"
 
 
 class TestNotcheAddition(object):
@@ -33,7 +39,7 @@ class TestNotcheAddition(object):
         # MatLib widget
         material_dict = load_matlib(matlib_path=matlib_path)
         self.widget = DMachineSetup(
-            material_dict=material_dict, machine_path=join(TEST_DATA_DIR, "Machine")
+            material_dict=material_dict, machine_path=join(DATA_DIR, "Machine")
         )
 
     @classmethod
@@ -41,12 +47,12 @@ class TestNotcheAddition(object):
         """Exit the app after the test"""
         cls.app.quit()
 
-    def test_adding_notch(self):
+    def test_notch_addition(self):
         """Checking that the UI allow the definition and the addition to a machine"""
 
         # Loading Prius machine
         return_value = (
-            join(DATA_DIR, "Machine", "Toyota_Prius.json"),
+            join(DATA_DIR, "Machine", machine_name + ".json"),
             "Json (*.json)",
         )
         with mock.patch(
@@ -132,22 +138,80 @@ class TestNotcheAddition(object):
         notche_wid.b_plot.clicked.emit()
         self.widget.w_step.notches_win.b_plot.clicked.emit()
 
+        self.widget.w_step.notches_win.b_ok.clicked.emit()
+
         # Clicking on OK then selecting rotor lamination tab
         self.widget.nav_step.setCurrentRow(7)
+
         # Enabling notch on rotor
+        assert isinstance(self.widget.w_step, SLamShape)
+        assert not self.widget.w_step.g_notches.isChecked()
+
+        self.widget.w_step.g_notches.setChecked(True)
+
+        assert self.widget.w_step.b_notch.isEnabled()
+        self.widget.w_step.b_notch.clicked.emit()
+
+        assert isinstance(self.widget.w_step.notches_win, DNotchTab)
 
         # Adding first notch polar
+        assert self.widget.w_step.notches_win.tab_notch.count() == 1
 
-        # checking preview/plot function
+        notche_wid = self.widget.w_step.notches_win.tab_notch.currentWidget()
+        assert isinstance(notche_wid, WNotch)
+
+        assert notche_wid.c_notch_type.currentIndex() == 0
+        notche_wid.c_notch_type.setCurrentIndex(1)
+
+        Zs = 8
+        notche_wid.si_Zs.setValue(Zs)
+        notche_wid.si_Zs.editingFinished.emit()
+        assert notche_wid.si_Zs.value() == Zs
+
+        H0 = 2e-3
+        W0 = pi / 24
+        assert isinstance(notche_wid.w_notch, PMSlot11)
+        notche_wid.w_notch.lf_H0.setValue(H0)
+        notche_wid.w_notch.lf_W0.setValue(W0)
+        assert notche_wid.w_notch.lf_H0.value() == H0
+        notche_wid.w_notch.lf_H0.editingFinished.emit()
+        assert_almost_equal(notche_wid.w_notch.lf_W0.value(), W0)
+        notche_wid.w_notch.lf_W0.editingFinished.emit()
+        # Checking plot/preview function
+        notche_wid.b_plot.clicked.emit()
+        self.widget.w_step.notches_win.b_plot.clicked.emit()
 
         # Clicking on OK then saving the machine
+        self.widget.w_step.notches_win.b_ok.clicked.emit()
 
         # Making sure that the machine was updated
+        file_path = join(save_path, machine_name + ".json")
+
+        # Check that the file didn't already exist
+        if isfile(file_path):
+            remove(file_path)
+        assert not isfile(file_path)
+
+        return_value = (file_path, "Json (*.json)")
+        with mock.patch(
+            "PySide2.QtWidgets.QFileDialog.getSaveFileName", return_value=return_value
+        ):
+            # To trigger the slot
+            self.widget.b_save.clicked.emit()
+
+        # Check that the file now exist => delete for next test
+        assert isfile(file_path)
+        remove(file_path)
+        assert not isfile(file_path)
+        # Check that the GUI have been updated
+        self.widget.nav_step.setCurrentRow(0)
+        assert type(self.widget.w_step) == SMachineType
+        assert self.widget.w_step.le_name.text() == machine_name
 
 
 if __name__ == "__main__":
     a = TestNotcheAddition()
     a.setup_class()
     a.setup_method()
-    a.test_adding_notch()
+    a.test_notch_addition()
     print("Done")
