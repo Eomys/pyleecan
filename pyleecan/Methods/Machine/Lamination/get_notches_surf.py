115e4fb174fb72afde7801469cc38c53456f94e1
Pierre Bonneel <pierre.bonneel@gmail.com>
2022-06-14 15:53:00 +0200

    [BC] Revert modifications

--- pyleecan/Methods/Machine/Lamination/get_notches_surf.py
+++ pyleecan/Methods/Machine/Lamination/get_notches_surf.py
@@ -1,5 +1,6 @@
 from ....Functions.Geometry.transform_hole_surf import transform_hole_surf
 from ....Functions.labels import NOTCH_LAB, YSNR_LAB, YSNL_LAB
+from ....Classes.NotchEvenDist import NotchEvenDist
 from ....Functions.Geometry.merge_notch_list import merge_notch_list
 from ....Classes.Segment import Segment
 from ....Classes.SurfLine import SurfLine
@@ -31,57 +32,30 @@ def get_notches_surf(self, sym):
     notch_list = [notch for notch in self.notch if notch.notch_shape.is_bore]
     if len(notch_list) == 0:
         return list()
-    # Get description of first notch
-    notch_desc_list = notch_list[0].get_notch_desc_list(sym=sym)
-    # If more than one notch, we need to merge and order the description
-    for ii in range(len(notch_list) - 1):
-        notch_desc_list = merge_notch_list(
-            notch_desc_list, notch_list[ii + 1].get_notch_desc_list(sym=sym)
-        )
-
-    # TODO Add yoke notches surfaces (issue with BC on yoke line)
-    Rbo = self.get_Rbo()
 
     surf_list = list()
-    for ii, notch_desc in enumerate(notch_desc_list):
-        # Add closing Line
-        Zbegin = notch_desc["lines"][-1].get_end()
-        Zend = notch_desc["lines"][0].get_begin()
-        notch_desc["lines"].append(Segment(Zbegin, Zend))
-
-        # create a surface
-        surf = SurfLine(line_list=notch_desc["lines"], label="Slot")
-        surf.comp_point_ref(is_set=True)
-
-        surf.label = self.get_label() + "_" + NOTCH_LAB + "_R" + str(ii) + "-T0-S0"
-        surf_list.append(surf)
-
-    # Label definition
-    BC_prop_right = self.get_label() + "_" + YSNR_LAB
-    BC_prop_left = self.get_label() + "_" + YSNL_LAB
-
-    # split surfaces for symmetry if needed (code from transform_hole_surf.py)
-    cut_list = list()
-    for surf in surf_list:
-        # Cut Ox axis
-        top, _ = surf.split_line(
-            0,
-            1.2 * Rbo,
-            is_join=True,
-            prop_dict_join={BOUNDARY_PROP_LAB: BC_prop_right},
-        )
-        if top is not None and sym > 2:
-            # Cut O-"sym angle" axis
-            _, bot = top.split_line(
-                0,
-                1.2 * Rbo * exp(1j * 2 * pi / sym),
-                is_join=True,
-                prop_dict_join={BOUNDARY_PROP_LAB: BC_prop_left},
+    for ii, notch in enumerate(notch_list):
+        # Method used for symetry (for now) so only NotchEvenDist
+        assert isinstance(notch, NotchEvenDist)
+        # Get the original surface
+        Nsurf = notch.notch_shape.get_surface()
+        Nsurf.label = self.get_label() + "_" + NOTCH_LAB + "_R" + str(ii) + "-T0-S0"
+        Nsurf.rotate(angle=notch.alpha)
+        # Label definition
+        BC_prop_right = self.get_label() + "_" + YSNR_LAB
+        BC_prop_left = self.get_label() + "_" + YSNL_LAB
+        # Generate all the surfaces (handle cut on sym axis)
+        surf_list.extend(
+            transform_hole_surf(
+                hole_surf_list=[Nsurf],
+                Zh=notch.notch_shape.Zs,
+                sym=sym,
+                alpha=0,
+                delta=0,
+                is_split=True,
+                BC_prop_right=BC_prop_right,
+                BC_prop_left=BC_prop_left,
             )
-            if bot is not None:
-                cut_list.append(bot)
-        elif top is not None:  # Half the machine => Only one cut required
-            cut_list.append(top)
-    surf_list = cut_list
+        )
 
     return surf_list
