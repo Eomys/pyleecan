2e554aa715337ff6e736e00f64209ed53319cac2
Pierre Bonneel <pierre.bonneel@gmail.com>
2022-06-17 16:10:54 +0200

    [BC] Update sym for notch and bore

--- pyleecan/Methods/Machine/LamHoleNS/comp_periodicity_spatial.py
+++ pyleecan/Methods/Machine/LamHoleNS/comp_periodicity_spatial.py
@@ -17,10 +17,30 @@ def comp_periodicity_spatial(self):
         True if an spatial anti-periodicity is possible after the periodicities
     """
 
-    per_a = self.get_pole_pair_number()
+    p = self.get_pole_pair_number()
+
+    nb_list = [p]
+    # account for notches and bore
+    if self.notch is not None:
+        for notch in self.notch:
+            nb_list.append(notch.comp_periodicity_spatial()[0])
+
+    if self.bore:
+        nb_list.append(self.bore.comp_periodicity_spatial()[0])
+    if self.yoke:
+        nb_list.append(self.yoke.comp_periodicity_spatial()[0])
+
+    # compute the gcd of the list
+    if len(nb_list) == 1:
+        per_a = nb_list[0]
+    else:
+        per_a = gcd(nb_list[0], nb_list[1])
+        for ii in range(2, len(nb_list)):
+            per_a = gcd(per_a, nb_list[ii])
     is_antiper_a = False
 
     # Account for duct periodicity
     per_a, is_antiper_a = self.comp_periodicity_duct_spatial(per_a, is_antiper_a)
+    per_a = int(per_a)
 
     return per_a, is_antiper_a
