6c4029521e7e1193503cdbbbf87c6e14c8142cc6
Pierre Bonneel <pierre.bonneel@gmail.com>
2022-09-01 23:43:32 +0200

    [BC] plot mmf

--- pyleecan/GUI/Dialog/DMachineSetup/SPreview/WMachineTable/WMachineTable.py
+++ pyleecan/GUI/Dialog/DMachineSetup/SPreview/WMachineTable/WMachineTable.py
@@ -101,7 +101,7 @@ class WMachineTable(Ui_WMachineTable, QWidget):
         """Plot the unit mmf of the stator"""
         try:
             if self.machine is not None:
-                self.machine.stator.plot_mmf_unit(is_create_appli=False)
+                self.machine.stator.plot_mmf_unit()
                 set_plot_gui_icon()
         except Exception as e:
             err_msg = "Error while plotting Stator mmf unit:\n" + str(e)
