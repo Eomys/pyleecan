c530c1a1e6a9118c1e10c6f64b446861277342b9
Clemence Mauger <clemence.mauger@eomys.com>
2023-09-13 11:02:18 +0200

    [CC] Correction of reviews

---
+++ pyleecan/Methods/Slot/HoleM60/__init__.py
@@ -0,0 +1,13 @@
+from ....Methods.Slot.Slot import SlotCheckError
+
+
+class S60_NoneError(SlotCheckError):
+    """Raised when a propery of HoleM58 is None"""
+
+    pass
+
+
+class S60_WCheckError(SlotCheckError):
+    """ """
+
+    pass
--- pyleecan/Methods/Slot/HoleM60/check.py
+++ pyleecan/Methods/Slot/HoleM60/check.py
@@ -1,5 +1,5 @@
 from ....Methods.Slot.Slot import SlotCheckError
-from ....Methods.Slot.HoleM58 import *
+from ....Methods.Slot.HoleM60 import *
 
 
 def check(self):
@@ -16,22 +16,22 @@ def check(self):
 
     Raises
     _______
-    S58_WCheckError
+    S60_WCheckError
         You must have W1+H0 <= W2
     """
     # Check that everything is set
     if self.W0 is None:
-        raise S58_NoneError("You must set W0 !")
+        raise S60_NoneError("You must set W0 !")
     elif self.W1 is None:
-        raise S58_NoneError("You must set W1 !")
+        raise S60_NoneError("You must set W1 !")
     elif self.W2 is None:
-        raise S58_NoneError("You must set W2 !")
+        raise S60_NoneError("You must set W2 !")
     elif self.W3 is None:
-        raise S58_NoneError("You must set W3 !")
+        raise S60_NoneError("You must set W3 !")
     elif self.H0 is None:
-        raise S58_NoneError("You must set H0 !")
+        raise S60_NoneError("You must set H0 !")
     elif self.H1 is None:
-        raise S58_NoneError("You must set H1 !")
+        raise S60_NoneError("You must set H1 !")
 
     if (self.W1 + self.H0) > self.W2:
-        raise S58_WCheckError("You must have (W1 + H0) <= W2")
+        raise S60_WCheckError("You must have (W1 + H0) <= W2")
