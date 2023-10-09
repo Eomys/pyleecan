71015be7e005abb1f32a294e8f3f1365e8982fc9
Bonneel Pierre <pierre.bonneel@eomys.com>
2023-10-09 17:12:13 +0200

    [BC] Adding missing label

--- pyleecan/Functions/Plot/get_patch_color_from_label.py
+++ pyleecan/Functions/Plot/get_patch_color_from_label.py
@@ -4,6 +4,7 @@ from ...Functions.labels import (
     ROTOR_LAB,
     KEY_LAB,
     HOLEM_LAB,
+    HOLEV_LAB,
     MAG_LAB,
     LAM_LAB,
     VENT_LAB,
@@ -50,7 +51,7 @@ def get_path_color_from_label(label, label_dict=None):
         return ROTOR_COLOR
     elif HOLEM_LAB in label_dict["surf_type"] or MAG_LAB in label_dict["surf_type"]:
         return MAGNET_COLOR
-    elif VENT_LAB in label_dict["surf_type"]:
+    elif VENT_LAB in label_dict["surf_type"] or HOLEV_LAB in label_dict["surf_type"]:
         return VENT_COLOR
     elif WEDGE_LAB in label_dict["surf_type"]:
         return WEDGE_COLOR
