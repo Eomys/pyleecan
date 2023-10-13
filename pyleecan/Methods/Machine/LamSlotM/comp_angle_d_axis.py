0b2e760c2f39bd146f5fd0b46e6c3bc152002a71
helene-t <helene.toubin@eomys.com>
2023-10-13 09:16:36 +0200

    [BC] handle uneven NS case

--- pyleecan/Methods/Machine/LamSlotM/comp_angle_d_axis.py
+++ pyleecan/Methods/Machine/LamSlotM/comp_angle_d_axis.py
@@ -23,7 +23,9 @@ def comp_angle_d_axis(self):
         if hasattr(self, "magnet") and self.magnet.type_magnetization == 3:
             angle = 0
         elif (
-            self.magnet_north.type_magnetization == 3
+            hasattr(self, "magnet_north")
+            and hasattr(self, "magnet_south")
+            and self.magnet_north.type_magnetization == 3
             and self.magnet_south.type_magnetization == 3
         ):
             angle = 0
