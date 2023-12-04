0e04de5250e22324d960c3e1d6323d448d46e735
CedMrnl <cedric.marinel@eomys.com>
2023-12-04 15:23:32 +0100

    [BC] Correct test to import mesh from unv file

--- Tests/Methods/Import/test_import_mesh_unv.py
+++ Tests/Methods/Import/test_import_mesh_unv.py
@@ -2,14 +2,15 @@
 
 # TODO: debug mesh with mix of elements (not handled in pyuff)
 
-import pytest
 from os import mkdir
-from os.path import isdir, join, basename, splitext
+from os.path import basename, isdir, join, splitext
+
+import pytest
 
 from pyleecan.Classes.ImportMeshMat import ImportMeshMat
 from pyleecan.Classes.MeshSolution import MeshSolution
-from Tests import save_plot_path as save_path
 from pyleecan.definitions import TEST_DIR
+from Tests import save_plot_path as save_path
 
 save_path = join(save_path, "Import")
 if not isdir(save_path):
@@ -60,7 +61,7 @@ def test_import_mesh_unv(unv_file):
     mesh = test_obj.get_data()
 
     meshsol = MeshSolution(dimension=3)
-    meshsol.mesh = [mesh]
+    meshsol.mesh = mesh
     meshsol.plot_mesh(
         save_path=join(save_path, splitext(basename(unv_file["path"]))[0] + ".png"),
         is_show_axes=True,
