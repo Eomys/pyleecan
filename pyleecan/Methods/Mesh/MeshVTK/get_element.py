b51efc7e5d9011646ca0a92cdd4466423c1db900
AdrienLeduque <adrien.leduque@eomys.com>
2024-01-23 15:38:42 +0100

    [WP] Remove meshio dependancies

--- pyleecan/Methods/Mesh/MeshVTK/get_element.py
+++ pyleecan/Methods/Mesh/MeshVTK/get_element.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from meshio import read
-from os import remove
+import numpy as np
+import pyvista as pv
 
 
 def get_element(self, indices=None):
@@ -16,31 +16,38 @@ def get_element(self, indices=None):
 
     Returns
     -------
-    elements: list
-        List of element groups (type + array)
+    dict_connectivity: dict[str, ndarray]
+        Dict of connectivity with element names as keys
+    nb_element: int
+        Number of element in the connectivity
+    dict_index: dict
+        When apply get_element on a MeshMat :
+            Dict of the element element_indices for each ElementMat.
+        Not use here, because it as any meanings for a MeshVTK
+
     """
+    # Extract mesh
+    mesh = self.get_mesh_pv(indices=indices)
 
-    if indices is not None:
-        # Extract submesh
-        mesh = self.get_mesh_pv(indices=indices)
-        # Write submesh in .vtk file
-        mesh.save(self.get_path(name="temp"))
-        # Read mesh file with meshio (pyvista does not provide the correct element format)
-        mesh = read(self.get_path(name="temp"))
-        # Remove the temporary .vtk file
-        remove(self.get_path(name="temp"))
-    else:
-        if self.is_pyvista_mesh:
-            mesh = self.get_mesh_pv()
-            mesh.save(self.get_path(name="mesh"))
-        # Read mesh file with meshio (pyvista does not provide the correct element format)
-        mesh = read(self.get_path(name=self.name, file_format=self.format))
+    pv_to_str = {
+        pv.CellType.TRIANGLE: "triangle",
+        pv.CellType.QUAD: "quad",
+        pv.CellType.QUADRATIC_EDGE: "line3",
+        pv.CellType.BIQUADRATIC_QUAD: "quad9",
+    }
 
-    elements = {}
-    nb_element = 0
-    indice_dict = {}
-    for element in mesh.cells:
-        elements[element.type] = element.data
-        nb_element += element.data.shape[0]
+    # Initialize the element dictionary with the keys
+    elements = {pv_to_str[cell_type]: [] for cell_type in np.unique(mesh.celltypes)}
 
-    return elements, nb_element, indice_dict
+    # Get the connectivity of each element
+    for cell_type, start_offset, end_offset in zip(
+        mesh.celltypes, mesh.offset[:-1], mesh.offset[1:]
+    ):
+        nodes_indice = mesh.cell_connectivity[start_offset:end_offset]
+        elements[pv_to_str[cell_type]].append(nodes_indice)
+
+    # Convert a list of 1D-ndarray into a 2D-ndarray
+    for key, value in elements.items():
+        elements[key] = np.array(value)
+
+    return elements, mesh.n_cells, {}
